143dd9aa3d1d5fa184902e377fb765fd35558f7e
troy <troy00pernia@gmail.com>
2023-10-29 19:45:55 -0400

    add components material UI to Header component

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import { AppBar, Toolbar, Container, Typography } from '@mui/material';
-import NavBar from '../NavBar/NavBar'
+'use client'
+
+import Image from 'next/image'
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+// import { AppBar, Toolbar, Container, Typography, Button, List, ListItem, ListItemIcon } from '@mui/material';
+
+import logoBeraund from '../../public/images/homeimg/homeimgberaud/logoBeraud.png'
+import links from '../NavBar/navbarpaths.json'
+
+
 function Header() {
-
-
   return (
-    <AppBar position="static" >
-      <Toolbar>
-        <Container>
-          <img src="/images/Header/logoBeraud.png" alt="logoBeraud" />
-        </Container>
-      </Toolbar>
-      <NavBar />
+    <AppBar>
+      <Container maxWidth="lg">
+        <Toolbar sx={{borderBottom: 1, borderColor: 'divider'}}>
+          <IconButton>
+            <Image src={logoBeraund} alt={'logoBeraund'} width={130}/>
+          </IconButton>
+        </Toolbar>
+        <Toolbar component='nav' sx={{justifyContent: 'flex-start', gap: 3}} >
+          {
+            links.map(link => (
+              <Link
+                href={link.path}
+                color='#000'
+                key={link.title}
+                sx={{textDecoration: 'none'}}
+              >{link.title}</Link>
+            ))
+          }
+        </Toolbar>
+        <Toolbar component='nav' sx={{justifyContent: 'flex-start', gap: 3}} >
+          {
+            links[0].subNav.map(link => (
+              <Link
+                href={link.path}
+                color='#000'
+                key={link.title}
+                sx={{textDecoration: 'none'}}
+              >{link.title}</Link>
+            ))
+          }
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
