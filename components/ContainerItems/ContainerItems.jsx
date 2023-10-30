e19879eaae2beb86da682e3ef09b4e8ff0c0e57c
troy <troy00pernia@gmail.com>
2023-10-30 08:49:55 -0400

    change component ContainerItems, adding material UI

--- components/ContainerItems/ContainerItems.jsx
+++ components/ContainerItems/ContainerItems.jsx
@@ -1,32 +1,51 @@
-import Image from 'next/image'
+'use client'
+
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+
 import items from './menuStore.json'
 import style from './ContainerItems.module.css'
 
 function ContainerItems () {
   return (
-    <div className={style.ContainerItems}>
+    <Grid item xs={12} md={8.5}>
       <h1 className={style.ContainerItemsTitle}>
         <strong>Pizzas</strong>
       </h1>
-      <ul className={style.listItems}>
+      <Grid container spacing={4}>
         {
-            items.map(item => (
-              <li key={item.name}>
-                <figure>
-                  <Image
-                    src={item.image}
-                    alt={item.name}
-                    fill
-                  />
-                </figure>
-                <label>{item.name}</label>
-                <span>${item.price}</span>
-                <p>{item.text}</p>
-              </li>
-            ))
+          items.slice(0,5).map(item => (
+            <Grid item key={item.name} xs={12} sm={6} md={4}>
+              <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              >  
+                <CardMedia
+                  component='div'
+                  sx={{
+                    pt: '100%'
+                  }}
+                  image={item.image}
+                />
+                <CardContent sx={{ flexGrow: 1}}>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    {item.name}
+                  </Typography>
+                  <Typography>
+                    ${item.price}
+                  </Typography>
+                  <Typography>
+                    {item.text}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
         }
-      </ul>
-    </div>
+      </Grid>
+    </Grid>
   )
 }
 
