350653345e07dbbaf849ee39387591c60af816c1
troy <troy00pernia@gmail.com>
2023-10-30 18:19:57 -0400

    change components/ContainerItems: change key, adding index to (line 20-21)

--- components/ContainerItems/ContainerItems.jsx
+++ components/ContainerItems/ContainerItems.jsx
@@ -17,8 +17,8 @@ function ContainerItems () {
       </h1>
       <Grid container spacing={4}>
         {
-          items.slice(0,5).map(item => (
-            <Grid item key={item.name} xs={12} sm={6} md={4}>
+          items.slice(0,5).map((item, index) => (
+            <Grid item key={item.name + index} xs={12} sm={6} md={4}>
               <Card
                 sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
               >  
