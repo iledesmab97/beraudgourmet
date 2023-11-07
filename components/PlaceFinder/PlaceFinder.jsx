6a21cbaf719b2eba74f8743ff21fe907f3038fe7
troy <troy00pernia@gmail.com>
2023-11-06 21:09:33 -0400

    change components/PlaceFinder: add distance to error input (line 89-94) and function calculateRoute (lne 49-65)

--- components/PlaceFinder/PlaceFinder.jsx
+++ components/PlaceFinder/PlaceFinder.jsx
@@ -3,12 +3,20 @@
 import React, { useState } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+// import { GoogleMap } from '@react-google-maps/api'
 import usePlacesAutocomplete from 'use-places-autocomplete'
 import ItemPlace from './ItemPlace'
+
+const center = {
+  lat: 19.4307,
+  lng: -99.2385
+};
 
 function AutocompleteAddress() {
   const [address, setAddress] = useState('');
   const [selectedSuggestion, setSelectedSuggestion] = useState(null);
+  const [distance, setDistance] = useState('')
+  // const [duration, setDuration] = useState('')
 
   // Configura usePlacesAutocomplete
   const {
@@ -17,7 +25,11 @@ function AutocompleteAddress() {
     suggestions: { status, data },
     setValue,
     clearSuggestions,
-  } = usePlacesAutocomplete();
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      componentRestrictions: { country: 'MX' }
+    }
+  });
 
   function handleInputChange (e) {
     if (!e) return
@@ -31,9 +43,29 @@ function AutocompleteAddress() {
     setSelectedSuggestion(suggestion);
     setValue(suggestion, false); // false para no borrar el valor del campo
     clearSuggestions();
+    calculateRoute(event.target.textContent)
   };
 
+  async function calculateRoute(address) {
+    const directionService = new google.maps.DirectionsService()
+    const results = await directionService.route({
+      origin: center,
+      destination: address,
+      // travelMode: google.maps.TravelMode.DRIVING
+      travelMode: 'DRIVING'
+    })
+    setDistance(results.routes[0].legs[0].distance.text.split('km')[0].trim())
+    console.log('distancia:', results.routes[0].legs[0].distance.text.split('km')[0].trim())
+  }
+
+  function clearRoute() {
+    setDistance(results.routes[0].legs[0].distance.text)
+    setAddress('')
+    selectedSuggestion(null)
+  }
+
   return (
+    <>
     <Autocomplete
       fullWidth
       disablePortal
@@ -42,14 +74,27 @@ function AutocompleteAddress() {
       getOptionLabel={option => option.description ? option.description : option}
       renderOption={
         (props, option) => (
-          <ItemPlace {...props} key={option.description} place={option.description}/>
+          <ItemPlace
+            {...props}
+            place={option.description}
+            key={option.description}
+            // onClick={() => {calculateRoute}}
+          />
       )}
       value={selectedSuggestion}
       onChange={handleSelect}
       inputValue={address}
       onInputChange={handleInputChange}
-      renderInput={(params) => <TextField {...params} label='Place' />}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label='Place'
+          error={distance > 15 ? true : false}
+          helperText={ distance > 15 ? 'Maxima destancia 15 km' : '' } 
+        />)}
     />
+    {/* <GoogleMap center={center} zoom={15} mapContainerStyle={{width: '100%', height: '500px'}}/> */}
+    </>
   );
 }
 
