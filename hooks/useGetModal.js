43f7b9b7de4b29aeb55f5f68b7deab9996a9e68a
troy <troy00pernia@gmail.com>
2023-11-08 12:34:22 -0400

    change hooks/useGetModal: change function handleOpenModal for work without arguments (line 17-18)

--- hooks/useGetModal.js
+++ hooks/useGetModal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 import { useAppSelector, useAppDispatch } from '@/hooks/store'
 import { openModal, closeModal } from '@/stores/modal/slice'
 
@@ -7,8 +7,6 @@ export default function useGetUser({modaltype}) {
     const modal = useAppSelector(state => state.modal)
     const dispatch = useAppDispatch()
 
-    // const [open, setOpen] = useState(false)
-
     const open = useMemo(() => {
         if (!modal || modal !== modaltype) {
             return false
@@ -16,16 +14,8 @@ export default function useGetUser({modaltype}) {
             return true
     }, [modal])
 
-    // useEffect(() => {
-    //     console.log('modal from globalState:', modal)
-    //     if (!modal || modal !== modaltype) {
-    //         return setOpen(false)
-    //     }
-    //     setOpen(true)
-    // }, [modal])
-
-    function handleOpenModal(modal) {
-        dispatch(openModal(modal))
+    function handleOpenModal() {
+        dispatch(openModal(modaltype))
     }
 
     function handleCloseModal() {
