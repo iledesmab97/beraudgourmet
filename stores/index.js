b1541ad3340a27eb1e88d8cc1dde33831aadc35a
troy <troy00pernia@gmail.com>
2023-11-08 12:10:02 -0400

    change stores/index: add to reducer: place and modal (line 11-12)

--- stores/index.js
+++ stores/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit"
 import usersReducer from './users/slice'
 import ordersReducer from './order/slice'
 import placeReducer from './place/slice'
+import modalReducer from './modal/slice'
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
         orders: ordersReducer,
-        place: placeReducer
+        place: placeReducer,
+        modal: modalReducer
     }
 })
