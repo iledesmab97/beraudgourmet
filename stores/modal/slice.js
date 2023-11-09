bf28ed038531f8bd22105f04e6e2b0d35b012713
troy <troy00pernia@gmail.com>
2023-11-09 10:27:48 -0400

    change store/modal/slice: change initialState (line 11-18) and add function to reducer: openModalOrder, closeModalOrder (line 24-58)

--- stores/modal/slice.js
+++ stores/modal/slice.js
@@ -8,21 +8,58 @@ const modal = {
     notifications: ''
 }
 
-const initialState = ''
+const initialState = {
+    order: {
+        open: false
+    },
+    place: {
+        open: false
+    }
+}
 
 export const modalSlice = createSlice({
     name: 'modal',
     initialState,
     reducers: {
-        openModal: (state, action) => {
-            return action.payload
+        openModalPlace: (state, action) => {
+            return {
+                ...state,
+                place: {
+                    ...state.place,
+                    open: true
+                }
+            }
         },
-        closeModal: (state, action) => {
-            return ''
+        closeModalPlace: (state, action) => {
+            return {
+                ...state,
+                place: {
+                    ...state.place,
+                    open: false
+                }
+            }
+        },
+        openModalOrder: (state, action) => {
+            return {
+                ...state,
+                order: {
+                    ...state.order,
+                    open: true
+                }
+            }
+        },
+        closeModalOrder: (state, action) => {
+            return {
+                ...state,
+                order: {
+                    ...state.order,
+                    open: false
+                }
+            }
         }
     }
 })
 
 export default modalSlice.reducer
 
-export const { openModal, closeModal } = modalSlice.actions
+export const { openModalPlace, closeModalPlace, openModalOrder, closeModalOrder } = modalSlice.actions
