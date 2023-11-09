064afa976c981dd2c49a4dca637e193be9cf45f7
troy <troy00pernia@gmail.com>
2023-11-09 10:28:38 -0400

    change hooks/useGetModal: add function handleOpenModalOrder and handleCloseModalOrder (line 10-23)

--- hooks/useGetModal.js
+++ hooks/useGetModal.js
@@ -1,26 +1,27 @@
 import { useMemo } from 'react'
 import { useAppSelector, useAppDispatch } from '@/hooks/store'
-import { openModal, closeModal } from '@/stores/modal/slice'
+import { openModalPlace, closeModalPlace, openModalOrder, closeModalOrder } from '@/stores/modal/slice'
 
-export default function useGetUser({modaltype}) {
+export default function useGetUser({modalType}) {
 
     const modal = useAppSelector(state => state.modal)
     const dispatch = useAppDispatch()
 
-    const open = useMemo(() => {
-        if (!modal || modal !== modaltype) {
-            return false
-        } else if (modal === modaltype)
-            return true
-    }, [modal])
-
-    function handleOpenModal() {
-        dispatch(openModal(modaltype))
+    function handleOpenModalPlace() {
+        dispatch(openModalPlace())
     }
 
-    function handleCloseModal() {
-        dispatch(closeModal())
+    function handleCloseModalPlace() {
+        dispatch(closeModalPlace())
     }
 
-    return {open, handleOpenModal, handleCloseModal}
+    function handleOpenModalOrder() {
+        dispatch(openModalOrder())
+    }
+
+    function handleCloseModalOrder() {
+        dispatch(closeModalOrder())
+    }
+
+    return {open: modal[modalType]?.open, handleOpenModalPlace, handleCloseModalPlace, handleOpenModalOrder, handleCloseModalOrder}
 } 
