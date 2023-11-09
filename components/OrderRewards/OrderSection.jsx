c1ff642b84a294306eb788611595a78b7f8191b8
troy <troy00pernia@gmail.com>
2023-11-09 09:25:24 -0400

    change components/OrderRewards/OrderSection: add hook useGetOrder (line 11)

--- components/OrderRewards/OrderSection.jsx
+++ components/OrderRewards/OrderSection.jsx
@@ -1,21 +1,31 @@
 'use client'
 
+import useGetOrder from '@/hooks/useGetOrders'
+
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 export default function OrderSection () {
-    return (
-        <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-            <Typography
-              variant='title'
-              gutterBottom>
-              Pedido
-            </Typography>
-            <br/>
-            <Typography variant='p'>
-              Su pedído está vacio
-            </Typography>
 
-          </Box>
-    )
+  const {orders} = useGetOrder()
+
+  function showOrder() {
+    console.log(orders)
+  }
+
+  return (
+      <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+          <Typography
+            variant='title'
+            gutterBottom>
+            Pedido
+          </Typography>
+          <br/>
+          <Typography variant='p'>
+            Su pedído está vacio
+          </Typography>
+          <Button variant='contained' onClick={showOrder}>Mostrar pedido</Button>
+        </Box>
+  )
 }
