78b8fa92e885353e1efa4a88ed3a59f58b2ff48c
troy <troy00pernia@gmail.com>
2023-11-10 13:46:03 -0400

    change components/ModalChooseProduct: add ChooseProduct component (line 15-21)

--- components/ModalChooseProduct/ModalChooseProduct.jsx
+++ components/ModalChooseProduct/ModalChooseProduct.jsx
@@ -1,157 +1,24 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, useRef } from 'react'
 import useGetModal from '@/hooks/useGetModal'
-import AboutPizza from './AboutPizza'
-import CustomizePizza from './CustomizePizza'
-import useGetOrders from '@/hooks/useGetOrders';
+import useHandleOder from '@/hooks/useHandleOrder'
+import ChooseProduct from './ChooseProduct'
 
-import Image from 'next/image'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import Grid from '@mui/material/Grid';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Radio from '@mui/material/Radio';
-import FormGroup from '@mui/material/FormGroup';
-import Checkbox from '@mui/material/Checkbox';
-import Table from '@mui/material/Table'
-import TableContainer from '@mui/material/TableContainer'
-import TableBody from '@mui/material/TableBody'
-import TableRow from '@mui/material/TableRow'
-import TableCell from '@mui/material/TableCell'
-import Paper from '@mui/material/Paper'
-import TableHead from '@mui/material/TableHead';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 960,
-  height: 600,
-  bgcolor: 'background.paper',
-  // border: '2px solid #000',
-  boxShadow: 24,
-  // p: 4,
-  borderRadius: 5
-};
-
-const INGREDIENTES = [
-  {
-    name: 'Aceitunas',
-    price: '$25'
-  },
-  {
-    name: 'Champiñon',
-    price: '$25'
-  },
-  {
-    name: 'Jalapeño',
-    price: '$25'
-  },
-  {
-    name: 'Cebolla',
-    price: '$25'
-  },
-  {
-    name: 'Chorizo',
-    price: '$25'
-  },
-  {
-    name: 'Jamón',
-    price: '$25'
-  },
-]
 
 export default function ModalChooseProduct() {
 
-  const {open, product, handleCloseModalOrder} = useGetModal({modalType:'order'})
-  const { orders, handleAddOrder } = useGetOrders()
-
-  const [quantity, setQuantity] = useState(1)
-  const totalPrice = quantity*product.price
-
-  function handleQuantity (event) {
-    const operation = event.target.name
-    if (operation === '+') {
-      setQuantity(prevQuantity => prevQuantity+=1)
-    } else if (operation === '-' && quantity > 1) {
-      setQuantity(prevQuantity => prevQuantity-=1)
-    }
-  }
+  const {open, handleCloseModalOrder} = useGetModal({modalType:'order'})
 
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleCloseModalOrder}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Grid
-            container
-            direction='row'
-            justifyContent='center'
-            alignItems='stretch'
-            spacing={4}
-            sx={{
-              width: '100%',
-              height: '100%',
-              marginTop: 0,
-              marginLeft: 0,
-            }}>
-
-            <AboutPizza product={product} />
-
-            <CustomizePizza product={product}/>
-
-            <Grid
-              container
-              direction='row'
-              justifyContent='space-between'
-              alignItems="center"
-              sx={{
-                height: '15%',
-                py: 1,
-                px: 5
-              }}
-            >
-              <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
-                <Button
-                  size='small'
-                  variant='contained'
-                  name='-'
-                  onClick={handleQuantity}
-                >
-                  -
-                </Button>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  {quantity}
-                </Typography>
-                <Button
-                  size='small'
-                  variant='contained'
-                  name='+'
-                  onClick={handleQuantity}
-                >
-                  +
-                </Button>
-                <Typography id="modal-modal-description" sx={{ ml: 5 }}>
-                  ${totalPrice}
-                </Typography>
-              </Box>
-              <Button variant='contained' onClick={handleAddOrder}>Agregar</Button>
-            </Grid>
-          </Grid>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={open}
+      onClose={handleCloseModalOrder}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <ChooseProduct />
+    </Modal>
   );
 }
