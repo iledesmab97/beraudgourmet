666628b87d180cd0553ce06a3a75cdd8d6594561
troy <troy00pernia@gmail.com>
2023-11-13 10:13:45 -0400

    change hooks/useHandleOrder: add prop "size" to inputs (line 8) and handleSize function (line 47-53). Change functions: handleMass, handleIngredients and handleExtra (line 75-112)

--- hooks/useHandleOrder.js
+++ hooks/useHandleOrder.js
@@ -5,6 +5,7 @@ export default function useHandleOrder({ product }) {
 
     const currentProduct = useRef(product)
     const [inputs, setInputs] = useState({
+        size: '14"',
         quantity: product?.quantity ? product.quantity : 1,
         mass: 'Masa Tradicional',
         ingredients: [],
@@ -19,6 +20,10 @@ export default function useHandleOrder({ product }) {
     function handleCurrentProduct(newProduct) {
         currentProduct.current = newProduct
     }
+
+    useEffect(() => {
+        console.log('inputs:', inputs)
+    }, [inputs])
 
     useEffect(() => {
         if (firstLoad) {
@@ -38,6 +43,14 @@ export default function useHandleOrder({ product }) {
         }
         handleCurrentProduct(newCurrentProduct)
     }, [inputs])
+
+    function handleSize (event) {
+        setInputs(prevInputs => ({
+            ...prevInputs,
+            size: event.target.value
+        }))
+        updateValue.current = {name: 'size'}
+    }
 
     function handleQuantity (event) {
         const operation = event.target.name
@@ -59,21 +72,51 @@ export default function useHandleOrder({ product }) {
     }
 
     function handleMass (event) {
-        // setMass({name: event.target.value})
+        setInputs(prevInput => ({
+            ...prevInput,
+            mass: event.target.value
+        }))
+        updateValue.current = {name: 'mass'}
     }
 
     function handleIngredients (event) {
-
+        const ingredient = event.target.labels[0].textContent
+        const isChecked = event.target.checked
+        const newInput = {...inputs}
+        const index = newInput.ingredients.indexOf(ingredient)
+        if (isChecked) {
+            if (index === -1) return
+            newInput.ingredients.splice(index, 1)
+        } else {
+            if (index !== -1) return
+            newInput.ingredients.push(ingredient)
+        }
+        setInputs(newInput)
+        updateValue.current = {name: 'ingredients'}
     }
 
     function handleExtra (event) {
-
+        const operation = event.target.name
+        const extraName = event.target.value
+        const newInputs = {...inputs}
+        if (operation === '+') {
+            newInputs.extra[extraName] = newInputs.extra[extraName] ? newInputs.extra[extraName] +=1 : 1
+        } else if (operation === '-') {
+            if (newInputs.extra[extraName] && newInputs.extra[extraName] >= 2) {
+                newInputs.extra[extraName] -=1
+            } else if (newInputs.extra[extraName] && newInputs.extra[extraName] === 1) {
+                newInputs.extra[extraName] = undefined
+            } else return
+        } else return
+        setInputs(newInputs)
+        updateValue.current = {name: 'extra'}
     }
 
     return {
         // updateCurrentProduct,
         totalPrice,
         inputs,
+        handleSize,
         handleQuantity,
         handleMass,
         handleIngredients,
