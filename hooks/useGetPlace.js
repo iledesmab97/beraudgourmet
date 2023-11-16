f88914b27298912a5d54d3a112b1489e97d0a5ee
troy <troy00pernia@gmail.com>
2023-11-16 13:38:42 -0400

    change hooks/useGetPlace: add function handleAddPlace and handleRemovePlace (line 7-17)

--- hooks/useGetPlace.js
+++ hooks/useGetPlace.js
@@ -1,8 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { addPlace, removePlace } from '@/stores/place/slice'
 
-export default function useGetOrders () {
+export default function useGetPlace () {
 
     const place = useSelector(state => state.place)
+    const dispatch = useDispatch()
     
-    return {place}
+    function handleAddPlace(newPlace) {
+        dispatch(addPlace(newPlace))
+    }
+
+    function handleRemovePlace() {
+        dispatch(removePlace())
+    }
+
+    return {place, handleAddPlace, handleRemovePlace}
 }
