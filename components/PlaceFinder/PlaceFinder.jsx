733b1428637d5a5c458f59050a50e4b7e981752a
troy <troy00pernia@gmail.com>
2023-11-17 07:14:01 -0400

    change components/PlaceFinder: move de logic of the component to usePlaceFinder hook, and add properties to Autocomplete component (line 54-56)

--- components/PlaceFinder/PlaceFinder.jsx
+++ components/PlaceFinder/PlaceFinder.jsx
@@ -1,41 +1,27 @@
 'use client'
 
 import React, { useEffect, useState, useRef } from 'react';
+import usePlacesAutocomplete from 'use-places-autocomplete'
+import usePlaceFinder from '@/hooks/usePlaceFinder'
+
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 // import { GoogleMap } from '@react-google-maps/api'
-import usePlacesAutocomplete from 'use-places-autocomplete'
 import ItemPlace from './ItemPlace'
 
-const center = {
-  lat: 19.4307,
-  lng: -99.2385
-};
+function PlaceFinder({ changeWithinLimit, withinLimit, handleInputsAddress, inputAddress }) {
 
-function PlaceFinder({ changeWithinLimit, withinLimit }) {
-  const [address, setAddress] = useState('');
-  const [selectedSuggestion, setSelectedSuggestion] = useState(null);
-  const [distance, setDistance] = useState('')
-  
-  const timerRef = useRef()
-
-  // const [duration, setDuration] = useState('')
-
-  // Configura usePlacesAutocomplete
   const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      componentRestrictions: { country: 'MX' }
-    }
-  });
+    address,
+    data,
+    selectedSuggestion,
+    distance,
+    handleSelect,
+    handleInputChange 
+  } = usePlaceFinder()
 
   useEffect(() => {
-    if (!distance) return
+    if (!distance) return changeWithinLimit(null)
     if (distance > 15) {
       changeWithinLimit(false)
     } else {
@@ -43,62 +29,13 @@ function PlaceFinder({ changeWithinLimit, withinLimit }) {
     }
   }, [distance])
 
-  function debounceSetValue (value, time) {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-    }
-
-    timerRef.current = window.setTimeout(() => {
-      setValue(value)
-    }, time);
-  }
-
-  function handleInputChange (e) {
-    if (!e) return
-    debounceSetValue(e.target.value, 500)
-    setAddress(e.target.value)
-  };
-
-  function handleSelect (event) {
-    const suggestion = event.target.textContent
-    setAddress(suggestion);
-    setSelectedSuggestion(suggestion);
-    setValue(suggestion, false); // false para no borrar el valor del campo
-    clearSuggestions();
-    calculateRoute(event.target.textContent)
-  };
-
-  async function calculateRoute(address) {
-    if (!address) return
-    const directionService = new google.maps.DirectionsService()
-    const results = await directionService.route({
-      origin: center,
-      destination: address,
-      // travelMode: google.maps.TravelMode.DRIVING
-      travelMode: 'DRIVING'
-    })
-    let newDistance = results.routes[0].legs[0].distance.text
-    if (newDistance.includes('.')) {
-      newDistance = newDistance.replaceAll('.', '')
-    }
-    if (newDistance.includes(',')) {
-      newDistance = newDistance.replace(',', '.')
-    }
-    setDistance(newDistance.split('km')[0].trim())
-  }
-
-  function clearRoute() {
-    setDistance(results.routes[0].legs[0].distance.text)
-    setAddress('')
-    selectedSuggestion(null)
-  }
-
   return (
     <>
     <Autocomplete
       fullWidth
       disablePortal
       id='autocomplete-PlaceFinder'
+      // noOptionsText={null}
       options={ address ? data : []}
       getOptionLabel={option => option.description ? option.description : option}
       renderOption={
@@ -114,6 +51,9 @@ function PlaceFinder({ changeWithinLimit, withinLimit }) {
       onChange={handleSelect}
       inputValue={address}
       onInputChange={handleInputChange}
+      isOptionEqualToValue={(option, value) => {
+        return option.description === value.description
+      }}
       renderInput={(params) => (
         <TextField
           {...params}
