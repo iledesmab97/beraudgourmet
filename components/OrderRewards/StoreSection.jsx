a98c186a607d4478948011ff9248f2e0406f1128
troy <troy00pernia@gmail.com>
2023-11-21 11:35:29 -0400

    change components/OrderRewards/StoreSection: add components: DateChoose and TimeChoose (line 120, 125)

--- components/OrderRewards/StoreSection.jsx
+++ components/OrderRewards/StoreSection.jsx
@@ -1,38 +1,153 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import useGetModal from '@/hooks/useGetModal'
+import useGetPlace from '@/hooks/useGetPlace'
+import DateChoose from '@/components/DateChoose/DateChoose'
+import TimeChoose from '@/components/TimeChoose/TimeChoose'
 
+import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import Select from '@mui/material/Select'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
 
 export default function StoreSection () {
 
-    const {handleOpenModalPlace} = useGetModal({modalType: 'place'})
+  const { handleOpenModalPlace } = useGetModal({modalType: 'place'})
+  const { place } = useGetPlace()
+  const [isTherePlace, setIsTherePlace] = useState(false)
+  const [whereDelivery, setWhereDelivery] = useState('')
 
-    return (
-        <Box sx={{
+  useEffect(() => {
+    if (Object.keys(place).length) {
+      setIsTherePlace(true)
+      setWhereDelivery(() => {
+        if (place.name) return 'store'
+        return 'home'
+      })
+    }
+  }, [place])
+
+  function handleChange(event) {
+    setWhereDelivery(event.target.value)
+  }
+
+  return (
+      <Grid
+        container
+        spacing={1}
+          sx={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'flex-start'
-          }}>
 
-            <Typography
-              variant='title'
-              gutterBottom>
-              Tienda
-            </Typography>
+          }}
+      >
+        <Grid
+          item
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'baseline'
+          }}
+        >
+          <Typography
+            variant='title'
+            gutterBottom>
+            Tienda
+          </Typography>
+          {
+            isTherePlace
+            ? (
+              <Typography
+                variant='p'
+                sx={{
+                  textDecoration: 'underline',
+                  cursor: 'pointer'
+                }}
+                onClick={handleOpenModalPlace}
+              >
+                cambiar
+              </Typography>
+            ) : null
+          }
+        </Grid>
+        {
+          isTherePlace
+          ? (
+            <>
+              <Grid
+                item
+                sx={{
+                  width: '100%'
+                }}
+              >
+                <Button
+                  fullWidth
+                  variant='contained'
+                >
+                  Modal nuevo
+                </Button>
+              </Grid>
+              <Grid
+                item
+                sx={{
+                  width: '100%'
+                }}
+              >
+                <FormControl fullWidth>
+                  {/* <InputLabel id="demo-simple-select-label">Entrega a domi</InputLabel> */}
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={whereDelivery}
+                    // label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={'home'}>Entrega a domicilio</MenuItem>
+                    <MenuItem value={'store'}>Recoger en tienda</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid
+                item
+              >
+                <DateChoose />
+                {/* <Grid item xs={6}>
+                  <Button
+                    variant='contained'
+                    fullWidth
+                  >
+                    {'Hora'}
+                  </Button>
+                </Grid> */}
+              </Grid>
+              <Grid
+                item
+              >
+                <TimeChoose />
+              </Grid>
+            </>
+          ): (
+            <>
+              <Button
+                variant='contained'
+                color='secondary'
+                sx={{ my:1 }}
+                fullWidth
+                onClick={handleOpenModalPlace}
+              >
+                  Ver la lista de tiendas
+              </Button>
+            </>
+          )
+        }
 
-            <Button
-              variant='contained'
-              color='secondary'
-              sx={{ my:1 }}
-              fullWidth
-              onClick={handleOpenModalPlace}
-            >
-                Ver la lista de tiendas
-            </Button>
-
-          </Box>
-    )
+      </Grid>
+  )
 }
