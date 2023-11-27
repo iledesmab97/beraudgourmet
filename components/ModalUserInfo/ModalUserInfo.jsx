b5852f76874ad28b0cdf9fd61624be4f76fe9b39
troy <troy00pernia@gmail.com>
2023-11-27 19:11:43 -0400

    change components/ModaluserInfo: change Box to Grid component (line 45,68)

--- components/ModalUserInfo/ModalUserInfo.jsx
+++ components/ModalUserInfo/ModalUserInfo.jsx
@@ -1,20 +1,24 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useGetModal from '@/hooks/useGetModal'
 import useGetUser from '@/hooks/useGetUser'
+import useHandleUser from '@/hooks/useHandleUser'
+import UserLoged from '../OrderRewards/UserLoged'
 import { accept } from '@/genericFunctions/modal'
 
 import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 700,
+    width: 400,
     height: 700,
     bgcolor: 'background.paper',
     boxShadow: 24,
@@ -29,22 +33,39 @@ const style = {
 
 function ModalUserInfo() {
 
-    // const [open, setOpen] = useState(false)
     const {open, handleCloseModalUser} = useGetModal({modalType: 'user'})
     const { handleRemoveUser } = useGetUser()
+    const { inputs, handleChange, userLoged, handleChangeNumberPhone} = useHandleUser()
 
     return (
         <Modal
             open={open}
             onClose={handleCloseModalUser}
         >
-            <Box sx={style}>
+            <Grid
+                container
+                sx={style}
+                alignItems={'stretch'}
+            >
+                <Typography
+                    variant='title'
+                    gutterBottom
+                >
+                    Su cuenta
+                </Typography>
+                <UserLoged
+                    userLoged={userLoged}
+                    inputs={inputs}
+                    handleChange={handleChange}
+                    handleChangeNumberPhone={handleChangeNumberPhone}
+                    type={'text'}
+                />
                 <Button
                     onClick={ () => {accept({action: handleRemoveUser}, handleCloseModalUser)}}
                 >
                     Cerrar Cesión
                 </Button>
-            </Box>
+            </Grid>
 
         </Modal>        
     )
