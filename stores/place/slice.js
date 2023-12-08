cf7cf90d3bcdd5feb5704d5d9d7be0635c2108ac
Troy Pernía <troy00pernia@gmail.com>
2023-12-07 21:39:03 -0400

    change store/place: add functions addDeadLine and addTypeDelivery (line 23-44)

--- stores/place/slice.js
+++ stores/place/slice.js
@@ -19,10 +19,33 @@ export const placeSlice = createSlice({
         },
         removePlace: (state, action) => {
             return {}
+        },
+        addDeadLine: (state, action) => {
+            if (!state.deadLine) {
+                return {
+                    ...state,
+                    deadLine: {
+                        [action.payload.property]: action.payload.value
+                    }
+                }
+            }
+            return {
+                ...state,
+                deadLine: {
+                    ...state.deadLine,
+                    [action.payload.property]: action.payload.value
+                }
+            }
+        },
+        addTypeDelivery: (state, action) => {
+            return {
+                ...state,
+                typeDelivery: {name: action.payload.name, totalName: action.payload.totalName}
+            }
         }
     }
 })
 
 export default placeSlice.reducer
 
-export const { addPlace, removePlace} = placeSlice.actions
+export const { addPlace, removePlace, addDeadLine, addTypeDelivery } = placeSlice.actions
