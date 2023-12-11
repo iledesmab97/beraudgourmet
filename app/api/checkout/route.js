a3060c5bb170aa74dbfe5875e747942437432a7b
Troy Pernía <troy00pernia@gmail.com>
2023-12-11 18:53:14 -0400

    change app/api/checkout/route.js: change value in amount (line 10)

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -7,7 +7,7 @@ export async function POST(req, res) {
     const {amount} = await req.json()
     try {
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: amount * 100,
+            amount: Math.round(amount * 100),
             currency: 'mxn',
             automatic_payment_methods: {
                 enabled: true
