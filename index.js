3ae9320805b4cc6b009029f534d581cf906f9023
Troy Pernía <troy00pernia@gmail.com>
2023-12-19 12:03:07 -0400

    rename server/index.js to index.js: remove createServer and parce. add express

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: '.env.local'})
+const server = require('./server/server')
+const next = require('next')
+const { initDB } = require("./server/db")
+
+const dev = process.env.NODE_ENV !== "production"
+const hostname = process.env.SERVER_HOSTNAME
+const port = process.env.SERVER_PORT
+
+async function bootstap() {
+    
+    await initDB()
+    const app = next({ dev, hostname, port })
+    const handle = app.getRequestHandler()
+
+    app.prepare().then(() => {
+
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
+
+        server.listen(port, (error) => {
+            if (error) throw error
+            console.log(`> Server listening at http://${hostname}:${port} as ${dev ? 'development' : process.env.NODE_ENV}`)
+        })
+    })
+}
+
+bootstap()
