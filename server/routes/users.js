736b43da35a8208a5db3ae45dbf0c004b237b5d9
Troy Pernía <troy00pernia@gmail.com>
2023-12-20 12:50:03 -0400

    change server/routes/users: change get request (line 7-13) and add post and delete request (line 15-35)

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,8 +4,35 @@ const {User} = require('../db')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    console.log('En efecto, funciona bien es con express')
-    res.json({message: '¡Hola Mundo!'})
+    try {
+        const allUsers = await User.findAll()
+        res.status(200).json(allUsers)
+    } catch(error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({...req.body})
+        res.status(200).json(newUser)
+    } catch(error) {
+        const {message, parent} = error
+        res.status(400).json({message, parent: parent.message})
+    }
+})
+
+router.delete('/', async (req, res) => {
+    const {id} = req.query
+    try {
+        if (!id) return res.status(300).json({message: 'id can\'t be undefined'})
+        const userToRemove = await User.findByPk(id)
+        if (!userToRemove) return res.status(200).json({message: `user with id:${id} does not exist`})
+        await userToRemove.destroy()
+        res.status(200).json({message: `user with id:${id} had been removed successfully`})
+    } catch(error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 module.exports = router
