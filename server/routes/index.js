e9015ee6871a31ff09d742707aac152f7374890e
Troy Pernía <troy00pernia@gmail.com>
2023-12-20 08:47:43 -0400

    change server/routes/index: add checkoutMiddleware (line 3) and add to new route (line 7)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
 const usersMiddleware = require('./users')
+const checkoutMiddleware = require('./checkout')
 
 const router = Router()
 router.use('/users', usersMiddleware)
+router.use('/checkout', checkoutMiddleware)
 
 module.exports = router
