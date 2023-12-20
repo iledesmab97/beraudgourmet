bdfc2b2e7d45ab435d22c2521119eef8a16c1930
Troy Pernía <troy00pernia@gmail.com>
2023-12-20 11:42:55 -0400

    change server/routes/stores: change request get (line 7-12), and add post and delete (line 20-33)

--- server/routes/stores.js
+++ server/routes/stores.js
@@ -4,17 +4,33 @@ const {Store} = require('../db')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    res.json({message: 'estas son las tiendas disponibles'})
+    try {
+        const allStores = await Store.findAll()
+        res.status(200).json(allStores)
+    } catch(error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 router.post('/', async (req, res) => {
-    const {name, city, address, phoneNumber, openTime, closeTime, pickUpSchedule, deliverySchedule, coordinates} = req.body
     try {
         const newStore = await Store.create({...req.body})
         res.status(200).json(newStore)
     } catch(error) {
-        console.error(error)
-        res.status(400).json({message: error})
+        res.status(400).json({message: error.message})
+    }
+})
+
+router.delete('/', async (req, res) => {
+    const {id} = req.query
+    try {
+        if (!id) return res.status(300).json({message: 'id can\'t be undefined'})
+        const storeToRemove = await Store.findByPk(id)
+        if (!storeToRemove) return res.status(200).json({message: `store with id:${id} does not exist`})
+        await storeToRemove.destroy()
+        res.status(200).json({message: `store with id:${id} had been removed successfully`})
+    } catch(error) {
+        res.status(400).json({message: error.message})
     }
 })
 
