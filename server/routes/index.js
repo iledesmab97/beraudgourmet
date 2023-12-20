6c487249f06f7fe3278e0a8210d61a760cac8aee
Troy Pernía <troy00pernia@gmail.com>
2023-12-20 16:55:40 -0400

    change server/routes/index: add orderMiddleware and use it (line 4,11)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const usersMiddleware = require('./users')
 const storesMiddleware = require('./stores')
+const ordersMiddleware = require('./orders')
 const checkoutMiddleware = require('./checkout')
 
 const router = Router()
 router.use('/checkout', checkoutMiddleware)
 router.use('/users', usersMiddleware)
 router.use('/stores', storesMiddleware)
+router.use('/orders', ordersMiddleware)
 
 module.exports = router
