999b45c6ef99c4674ea4343ada149a1fcaff4973
Troy Pernía <troy00pernia@gmail.com>
2023-12-21 16:20:49 -0400

    change server/routes/users: add condition to get request (line 9-21)

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,21 @@ const {User} = require('../db')
 const router = Router()
 
 router.get('/', async (req, res) => {
+    const {email} = req.query
     try {
+        if (email) {
+            const {email} = req.body
+            const userFinded = await User.findOne({
+                where:{
+                    email
+                }
+            })
+            const userData = userFinded ? {
+                ...userFinded.dataValues,
+                password: '****'
+            } : null
+            return res.status(200).json(userData)
+        }
         const allUsers = await User.findAll()
         res.status(200).json(allUsers)
     } catch(error) {
@@ -33,24 +47,12 @@ router.get('/:idUser', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const {search} = req.query 
     try {
-        if (!search) {
-            const newUser = await User.create({...req.body})
-            return res.status(200).json(newUser)
-        } else {
-            const {email, password} = req.body
-            const userFinded = await User.findOne({
-                where:{
-                    email,
-                    password
-                }
-            })
-            res.status(200).json(userFinded)
-        }
+        const newUser = await User.create({...req.body})
+        return res.status(200).json(newUser) 
     } catch(error) {
         const {message, parent} = error
-        res.status(400).json({message, parent: parent.message})
+        return res.status(400).json({message, parent: parent.message})
     }
 })
 
