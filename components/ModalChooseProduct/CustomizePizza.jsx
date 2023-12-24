19e3261e6fb7424f0025a7c152714c31c1703c08
Troy Pernía <troy00pernia@gmail.com>
2023-12-24 17:50:47 -0400

    change components/ModalChooseProdct/CustomizePizza: add components: ButtonGrouPizza and MasTypePizza (line 5-6, 95, 104)

--- components/ModalChooseProduct/CustomizePizza.jsx
+++ components/ModalChooseProduct/CustomizePizza.jsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from 'react'
 import { scrollToSection, showScrollPosition } from '@/genericFunctions/modal'
+import ButtonGroupPizza from '@/components/ButtonGroupPizza/ButtonGroupPizza'
+import MasaTypesPizza from '@/components/MasaTypesPizza/MasaTypesPizza'
 
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
@@ -57,7 +59,7 @@ export default function CustomizePizza ({ name, ingredientsProduct, customizePiz
         handleIngredientsModal,
         extra,
         handleExtra
-      } = customizePizza
+    } = customizePizza
 
     return (
         <Grid
@@ -90,47 +92,7 @@ export default function CustomizePizza ({ name, ingredientsProduct, customizePiz
                     ELIGE EL TAMAÑO
                 </Typography>
 
-                <ButtonGroup
-                    size='large'
-                    variant='contained'
-                    aria-label="contained large button group"
-                    sx={{
-                        // width: 216
-                        width: 'fit-content'
-                    }}
-                >
-                    <Button
-                        onClick={handleSize}
-                        value={'12"'}
-                        sx={size === '12"'
-                            ? {
-                                backgroundColor: 'rgb(28, 58, 93)'
-                            } : {}}
-                    >
-                        {'12"'}
-                    </Button>
-                    <Button
-                        onClick={handleSize}
-                        value={'14"'}
-                        sx={size === '14"'
-                            ? {
-                                backgroundColor: 'rgb(28, 58, 93)'
-                            } : {}}
-                    >
-                        {'14"'}
-                    </Button>
-                    <Button
-                        onClick={handleSize}
-                        value={'16"'}
-                        sx={size === '16"'
-                            ? {
-                                backgroundColor: 'rgb(28, 58, 93)'
-                            } : {}}
-                    >
-                        {'16"'}
-                    </Button>
-                    {/* <Button onClick={handleSize} value={'18"'}>{'18"'}</Button> */}
-                </ButtonGroup>
+                <ButtonGroupPizza handleClick={handleSize} size={size} />
 
                 <Typography
                     id="modal-subtitle-ELIGE_LA_MASA"
@@ -138,130 +100,8 @@ export default function CustomizePizza ({ name, ingredientsProduct, customizePiz
                     sx={{ mt: 2 }}>
                     ELIGE LA MASA
                 </Typography>
-            
-                <FormControl>
-                    {/* <FormLabel id="demo-radio-buttons-group-label">ELIGE LA MASA</FormLabel> */}
-                    <RadioGroup
-                        aria-labelledby="demo-radio-buttons-group-label"
-                        // defaultValue="Masa Tradicional"
-                        name="radio-buttons-group"
-                        onChange={handleMass}
-                        // sx={{
-                        //     display: 'flex',
-                        //     flexDirection: 'column',
-                        //     gap: 1
-                        // }}
-                    >
-                        <Grid
-                            container
-                            direction='row'
-                            justifyContent="space-between"
-                            columns={{ sm:12, md:24}}
-                        >
-                            {
-                                Object.keys(currentProduct.price[size]).map((typeMass, index) => (
-                                    <Grid
-                                        key={typeMass + index}
-                                        item
-                                        sm={12}
-                                        md={11}
-                                        mt={1}
-                                        // container
-                                        // direction='column'
-                                        // justifyContent='flex-start'
-                                        // alignItems='flex-start'
-                                        sx={ {
-                                            minWidth: '49%',
-                                            borderRadius: '10px',
-                                            '&:hover': {
-                                                backgroundColor: 'rgba(0,0,0,0.1)'
-                                            }
-                                        }}
-                                        // xs={5.5}
-                                    >
-                                        <Box
-                                            sx={{
-                                                component: 'div',
-                                                display: 'flex',
-                                                justifyContent: 'flex-start',
-                                                alignItems: 'center'
-                                            }}
-                                        >
-                                            <FormControlLabel
-                                                // key={typeMass + index}
-                                                value={typeMass}
-                                                control={
-                                                    <Radio
-                                                        checked={mass === typeMass ? true : false}
-                                                    />}
-                                                label={
-                                                    <Typography
-                                                        variant='p'
-                                                        sx={{
-                                                            width: 'inline',
-                                                            fontWeight: 400
-                                                        }}
-                                                    >
-                                                        {typeMass}
-                                                    </Typography>
-                                                }
-                                                sx={ mass === typeMass
-                                                    ? {
-                                                        width: '100%',
-                                                        height: '100%',
-                                                        mt: 1,
-                                                        borderRadius: '10px',
-                                                        margin: 0,
-                                                        px: 2,
-                                                        pl: 0,
-                                                        py: 1,
-                                                        // '&:hover': {
-                                                        //     backgroundColor: 'rgba(0,0,0,0.1)'
-                                                        // }   
-                                                    } : {
-                                                        width: '100%',
-                                                        height: '100%',
-                                                        mt: 1,
-                                                        borderRadius: '10px',
-                                                        margin: 0,
-                                                        // width: 'fit-content',
-                                                        px: 2,
-                                                        pl: 0,
-                                                        py: 1,
-                                                        // '&:hover': {
-                                                        //     backgroundColor: 'rgba(0,0,0,0.1)'
-                                                        // }   
-                                                    }
-                                                }
-                                            />
-                                            <Typography
-                                                id="modal-modal-description"
-                                                // variant='title'
-                                                sx={{
-                                                    width: 'inline',
-                                                    paddingRight: 1
-                                                }}
-                                            >
-                                                ${currentProduct.price[size][typeMass]}
-                                            </Typography>
-                                        </Box>
-                                        <Typography
-                                            variant='miniature'
-                                            component='p'
-                                            sx={{
-                                                // width: '100%'
-                                                // padding: 1,
-                                                paddingTop: 0
-                                            }}
-                                        >
-                                            {masses[typeMass].text}
-                                        </Typography>
-                                    </Grid>
-                                ))    
-                            }
-                        </Grid>
-                    </RadioGroup>
-                </FormControl>
+
+                <MasaTypesPizza listMass={currentProduct.price[size]} mass={mass} handleMass={handleMass} />
             
                 <Typography
                     id="modal-modal-description"
