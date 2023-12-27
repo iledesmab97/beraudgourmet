4846d2f6aaa6868b822efea9caaddf6be7deaae8
Troy Pernía <troy00pernia@gmail.com>
2023-12-27 11:53:50 -0400

    change server/routes/scheduleHours: add new condition to go in in if (line 18)

--- server/routes/scheduleHours.js
+++ server/routes/scheduleHours.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     const { many } = req.query
     try {
-        if (many) {
+        if (many && JSON.parse(many)) {
             const newScheduleHours = await ScheduleHours.bulkCreate(req.body)
             return res.status(200).json(newScheduleHours)
         }
