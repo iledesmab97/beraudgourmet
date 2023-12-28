09396e91852b569e438c3fd58fa805d626660e37
Troy Pernía <troy00pernia@gmail.com>
2023-12-27 22:12:14 -0400

    add stores/products

---
+++ stores/products/slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {}
+
+export const productsSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {
+        addProductsList: (state, action) => {
+            const { type, products } = action.payload
+            if (state[type]) {
+                const productsFitered = products.filter( product => {
+                    return !state[type].some(producIncluded => producIncluded.name === product.name)
+                } )
+                return {
+                    ...state,
+                    [type]: [...state[type], ...productsFitered]
+                }
+            } else {
+                return {
+                    ...state,
+                    [type]: [...products]
+                }
+            }
+        }
+    }
+})
+
+export default productsSlice.reducer
+
+export const { addProductsList } = productsSlice.actions
