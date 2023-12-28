5e86303e9f548a0faa5869aa496d492bdd3d0c8f
Troy Pernía <troy00pernia@gmail.com>
2023-12-27 22:23:52 -0400

    change hooks/useGetProducts: change products definition (line 6)

--- hooks/useGetProducts.js
+++ hooks/useGetProducts.js
@@ -1,9 +1,9 @@
 import { useAppSelector, useAppDispatch } from '@/hooks/store'
 import { addProductsList } from '@/stores/products/slice'
 
-export default function useGetProducts () {
+export default function useGetProducts ({type}) {
 
-    const products = useAppSelector(state => state.products)
+    const products = useAppSelector(state => state.products[type])
     const dispatch = useAppDispatch()
 
     function handleAddProductsList({type, products}) {
