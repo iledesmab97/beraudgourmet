c82ef597086f357937bad8258d2128d877cad8da
Troy Pernía <troy00pernia@gmail.com>
2024-01-02 13:29:39 -0400

    change server/routes/orders: change post request (line 18-114)

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const {Order, KindProduct, OrderPizza, ExtraIngredientsxOrderPizza, ItemsxOrder} = require('../db')
+const {Order, KindProduct, OrderPizza, ExtraIngredientsxOrderPizza, ItemsxOrder, PizzaCharacteristic, PizzaMass, PizzaSize, PizzaExtraIngredient, PizzaIngredient, Pizza } = require('../db')
 
 const router = Router()
 
@@ -14,66 +14,104 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const {storeId, userId, itemsList, cost, applicationDate, deliveryDate } = req.body
-        if (!itemsList || !Array.isArray(itemsList)) throw new Error('itemsList need to be a array')
-        if (!cost || !Number(cost) || Number(cost) < 0) throw new Error('the cost cannot be null, less or equal than zero')
 
-        // Buscar los id de los tipos de productos
-        const listKindProductsName = itemsList.map(item => item.typeItem)
-        const listKindProducts = await Promise.all(listKindProductsName.map(async (name)=> {
-            return await KindProduct.findOne({
-                where: {
-                    name
+        const { userId, storeId, totalCostByItems, commissions, totalCost, applicationDate, deliveryDate, itemsList } = req.body
+
+        // Creo la nueva Orden
+        const newOrder = await Order.create({totalCostByItems, commissions, totalCost, applicationDate, deliveryDate, StoreId: storeId, UserId: userId})
+
+        // Creo las nuevas Ordenes para los articulos de la orden general
+        for (let item of itemsList) {
+            const { name, itemType, quantity } = item
+            switch (name) {
+                case 'pizza': {
+                    const { size, mass, ingredientsOut, extraIngredients, costItemPerUnit, totalCostByItem } = item
+                    const pizzaId = await Pizza.findOne({
+                        attributes: ['id'],
+                        where: {
+                            name: itemType
+                        }
+                    })
+    
+                    const pizzaSizeId = await PizzaSize.findOne({
+                        attributes: ['id'],
+                        where: {
+                            size: size
+                        }
+                    })
+    
+                    const pizzaMassId = await PizzaMass.findOne({
+                        attributes: ['id'],
+                        where: {
+                            name: mass
+                        }
+                    })
+    
+                    const pizzaCharacteristicsId = await PizzaCharacteristic.findOne({
+                        attributes: ['id', 'cost'],
+                        where: {
+                            PizzaMassId: pizzaMassId.id,
+                            PizzaSizeId: pizzaSizeId.id
+                        }
+                    })
+    
+                    const newOrderPizza = await OrderPizza.create({
+                        idCharacteristicsPizza: pizzaCharacteristicsId.id,
+                        quantity,
+                        idPizza: pizzaId.id
+                    })
+    
+                    if (ingredientsOut) {
+                        for (let ingredient of ingredientsOut) {
+                            const [ingredientsSelected, created] = await PizzaIngredient.findOrCreate({
+                                attributes: ['id'],
+                                where: {
+                                    name: ingredient
+                                },
+                                defaults: {}
+                            })
+                            newOrderPizza.addPizzaIngredient([ingredientsSelected.id])
+                        }
+                    }
+    
+                    if (extraIngredients) {
+                        const extraIngredientsListName = extraIngredients.map(ingredient => ingredient.name)
+                        const extraIngredientsList = await PizzaExtraIngredient.findAll({
+                            where: {
+                                name: extraIngredientsListName
+                            }
+                        })
+                        for (let i=0; i < extraIngredients.length; i++) {
+                            const {id, cost} = extraIngredientsList[i]
+                            const {quantity} = extraIngredients[i]
+                            await newOrderPizza.addPizzaExtraIngredient(id, {
+                                through: {
+                                    quantity,
+                                    cost: cost * quantity
+                                }
+                            })
+                        }
+                    }
+
+                    // Añado la información a la tabla de ItemsxOrder
+
+                    const newItemxOrder = await ItemsxOrder.create({
+                        quantity,
+                        costPerUnity: costItemPerUnit,
+                        totalCostByItem,
+                        OrderItemId: newOrderPizza.id
+                    })
+                    await newItemxOrder.setOrder(newOrder.id)
+                    await newItemxOrder.setKindProduct({})
+
                 }
-            })
-        }))
-
-        // Buscar todas las ordenes de pizza creadas y pertenecientes a la orden general
-        const orderPizzaList = itemsList.map(item => item.orderItemId)
-        const itemsOrderList = await OrderPizza.findAll({
-            where: {
-                id: orderPizzaList
-            }
-        })
-
-        const listExtraIngredientsByItemsOrder = await Promise.all(itemsOrderList.map((itemOrder, index) => {
-            const { id } = itemOrder
-            return ExtraIngredientsxOrderPizza.findAll({
-                attributes: ['cost', 'OrderPizzaId'],
-                where: {
-                OrderPizzaId: id
+                default: {
+                    continue
                 }
-            })
-        }))
-
-        const dataByItemsOrder = listExtraIngredientsByItemsOrder.map((extraIngredientByItemOrder, index) => {
-            const { quantity } = itemsList[index]
-            const { id, cost } = itemsOrderList[index]
-            const totalExtraCost = extraIngredientByItemOrder.reduce((acc, curr) => acc + Number(curr.cost), 0)
-            const totalCost = (Number(cost) + totalExtraCost) * quantity
-
-            return {
-                id,
-                quantity,
-                totalCost
             }
-        })
 
-        const newOrder = await Order.create({cost, applicationDate, deliveryDate, StoreId: storeId, UserId: userId})
-
-        await Promise.all(dataByItemsOrder.map( async (itemOrder, index) => {
-            const { quantity, totalCost, id } = itemOrder
-            const idKindProduct = listKindProducts[index].id
-            const newItemOrder = await ItemsxOrder.create({
-                quantity,
-                cost: totalCost,
-                OrderItemId: id,
-            })
-            await newItemOrder.setOrder(newOrder.id)
-            await newItemOrder.setKindProduct(idKindProduct)
-        }))
-
-        console.log('Se han modificado los parametros de la orden exitosamente')
+        }
+        
         return res.status(200).json(newOrder)
     } catch(error) {
         const {message, parent} = error
