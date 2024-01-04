9a6c9c84268e05e95ed3f7c8bced3399fb4bc816
Troy Pernía <troy00pernia@gmail.com>
2024-01-04 15:50:01 -0400

    change server/routes/users: change post request "/" removing condition with "validate" const (line 48-61), add new post request to: "/login" (line 63-99), and "/logout" (line 101-120)

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,7 @@
 const { Router } = require('express')
 const {User} = require('../db')
+const jwt =  require('jsonwebtoken')
+const { serialize, parse } = require('cookie') 
 
 function validateNumber(input) {
     const regularExpresion = /^\d+$/
@@ -43,25 +45,8 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const { validate, many } = req.query
+    const { many } = req.query
     try {
-        if (validate && JSON.parse(validate)) {
-            const {email, password} = req.body
-            const userFinded = await User.findOne({
-                where:{
-                    email,
-                    password
-                }
-            })
-            if (userFinded.password !== password) {
-                return res.status(200).json({message: 'Contraseña incorrecta'})
-            }
-            const userData = userFinded ? {
-                ...userFinded.dataValues,
-                password: '****'
-            } : null
-            return res.status(200).json(userData)
-        }
         if (many && JSON.parse(many)) {
             const usersList = req.body
             const newUserList = await User.bulkCreate(usersList)
@@ -72,6 +57,65 @@ router.post('/', async (req, res) => {
     } catch(error) {
         const {message, parent} = error
         return res.status(400).json({message, parent: parent.message})
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+            const userFinded = await User.findOne({
+                where:{
+                    email,
+                    password
+                }
+            })
+            if (!userFinded) {
+                return res.status(401).json({message: 'Contraseña incorrecta'})
+            }
+            const userData = {
+                ...userFinded.dataValues
+            }
+            delete userData.password
+            const token = jwt.sign({
+                ...userData.dataValues,
+                exp: Math.floor(Date.now() / 1000) + 60*60*24*30
+            }, 'secret')
+
+            const serialized = serialize( 'tokenUser', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'strict',
+                // sameSite: 'none',
+                maxAge: 1000 * 60 * 60 * 24 * 30,
+                path: '/'
+            })
+
+            res.setHeader('Set-Cookie', serialized)
+            return res.status(200).json(userData) 
+    } catch(error) {
+        const {message, parent} = error
+        return res.status(400).json({message, parent: parent?.message})
+    }
+})
+
+router.post('/logout', async (req, res) => {
+    const { tokenUser } = parse(req.headers.cookie)
+    try {
+        if (!tokenUser) return res.status(200).json({message: 'No hay usuario con la sesión activa'})
+        jwt.verify(tokenUser, 'secret')
+        const serialized = serialize( 'tokenUser', null, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            // sameSite: 'none',
+            maxAge: 0,
+            path: '/'
+        })
+        res.setHeader('Set-Cookie', serialized)
+        res.status(200).json({ message: 'Se ha cerrado seción exitosamente'})
+    } catch(error) {
+        const {message, parent} = error
+        return res.status(400).json({message, parent: parent?.message})
     }
 })
 
