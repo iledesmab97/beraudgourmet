ca0cbfe5cffd8e7cba6369bfb773b9503b933040
Troy Pernía <troy00pernia@gmail.com>
2024-01-09 18:45:58 -0400

    change server/middlewares/authjwt: change function verifyToken (line 5-10), isRoot (line 17-37)

--- server/middlewares/authjwt.js
+++ server/middlewares/authjwt.js
@@ -2,14 +2,36 @@ const jwt =  require('jsonwebtoken')
 const { parse } = require('cookie')
 
 async function verifyToken(req, res, next) {
-    const { tokenUser } = parse(req.headers.cookie)
     try {
+        const { tokenUser } = parse(req.headers.cookie)
+
         if (!tokenUser) return res.status(403).json({message: 'No token provided'})
-        jwt.verify(tokenUser, 'secret')
+        const user = jwt.verify(tokenUser, 'secret')
+        req.user = user
         next()
     } catch(error) {
         res.status(400).json({message: 'Unauthorized'})
     }
 }
 
-module.exports = verifyToken
+async function isRoot(req, res, next) {
+    const { user } = req
+    try {
+        if (user.RoleId !== 1) throw new Error('Unauthorized')
+        next()
+    } catch(error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+async function isAdmin(req, res, next) {
+    const { user } = req
+    try {
+        if (user.RoleId > 2) throw new Error('Unauthorized')
+        next()
+    } catch(error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+module.exports = {verifyToken, isRoot, isAdmin}
