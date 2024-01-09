5e40e1e80178f636e2ee1c668c9a6716d6e9db0a
Troy Pernía <troy00pernia@gmail.com>
2024-01-09 18:47:10 -0400

    change server/libs/validateData: add function validateEmail (line 12-14), and unserialize (line 36-44)

--- server/libs/validateData.js
+++ server/libs/validateData.js
@@ -6,8 +6,12 @@ const { NODE_ENV } = process.env
 
 module.exports = {
     validateNumber: (input) => {
-        const regularExpresion = /^\d+$/
-        return regularExpresion.test(input)
+        const numberRegex = /^\d+$/
+        return numberRegex.test(input)
+    },
+    validateEmail: (input) => {
+        const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+        return emailRegex.test(input)
     },
     makeJWT: (userData) => {
         const token = jwt.sign({
@@ -28,5 +32,15 @@ module.exports = {
             token,
             serialized
         }
+    },
+    unserialize: () => {
+        return serialize( 'tokenUser', null, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            // sameSite: 'none',
+            maxAge: 0,
+            path: '/'
+        })
     }
 }
