e33b522c3018e1807ca59e35a3b96c69615362a6
Troy Pernía <troy00pernia@gmail.com>
2024-01-15 19:17:38 -0400

    add app/user-verify

---
+++ app/user-verify/[token]/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useState } from "react"
+import Button from "@mui/material/Button"
+import { verifyEmailUser } from '@/api/userVerifyApi'
+
+function UserTokenVerify({ params }) {
+
+    const [ verified, setVerified ] = useState(false)
+
+    async function verifyEmail() {
+        const response = await verifyEmailUser(params.token)
+        if (response.message) alert(response.message)
+        if (response) setVerified(true)
+    }
+
+    return (
+        <>
+            <Button variant='contained' margin='dense' onClick={verifyEmail}>
+                {verified ? 'Correo verificado' : 'Verificar correo'}
+            </Button>
+        </>
+    )
+}
+
+export default (UserTokenVerify)
