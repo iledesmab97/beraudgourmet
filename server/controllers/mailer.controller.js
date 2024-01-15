6fc6473a3ccb23ca2a727e427004af1d1b62f2f5
Troy Pernía <troy00pernia@gmail.com>
2024-01-15 18:22:06 -0400

    change server/controllers/mailer.controller: change arguments name from user to email (line 5, 9)

--- server/controllers/mailer.controller.js
+++ server/controllers/mailer.controller.js
@@ -2,11 +2,11 @@ const { transporter } = require('../mailer')
 const { GOOGLE_USER } = process.env
 
 module.exports = {
-    emailVerification: async function ({token, user}) {
+    emailVerification: async function ({token, email}) {
         const verificationLink = `http://localhost:3000/user-verify/${token}`
         transporter.sendMail({
             from: `"Verification email" <${GOOGLE_USER}>`, // sender address
-            to: user.email, // list of receivers
+            to: email, // list of receivers
             subject: "Verification email", // Subject line
             // text: "", // plain text body
             html: `<p>Te saludamos desde BeraudGourmet y te damos gracias por darnos la oportunidad de servirte con nuestras más exquisitas pizzas.</p><br/><a href='${verificationLink}'>Haz clic aquí para verificar tu cuenta.</a>`, // html body
