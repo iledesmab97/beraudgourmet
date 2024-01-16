7e446ad70ca96dd1beeae491a4e1b24b2f2b4d56
Troy Pernía <troy00pernia@gmail.com>
2024-01-16 14:08:26 -0400

    change server/routes/users: add routes: "/users/update" and "/users/update/:id" (line 19-20) and import functions updateMyAccount and updateUser (line 4)

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '.env.local'})
 const { Router } = require('express')
 
-const {getAllUsers, getUser, signUp, logIn, logOut, update, remove, verifyUser } = require('../controllers/users.controller')
+const {getAllUsers, getUser, signUp, logIn, logOut, updateMyAccount, updateUser, remove, verifyUser } = require('../controllers/users.controller')
 
 const {verifyToken, isRoot, isAdmin} = require('../middlewares')
 
@@ -16,7 +16,8 @@ router.post('/', signUp)
 router.post('/login', logIn)
 router.post('/logout', verifyToken, logOut)
 
-router.put('/', verifyToken, update)
+router.put('/update', verifyToken, updateMyAccount)
+router.put('/update/:id', [verifyToken, isAdmin], updateUser)
 
 router.delete('/', verifyToken, remove)
 
