6eeb59da3e44f44167b7d1a6ff399a48033a30ab
Troy Pernía <troy00pernia@gmail.com>
2024-01-17 19:00:25 -0400

    add server/controllers/checkout.controller

---
+++ server/controllers/checkout.controller.js
@@ -0,0 +1,26 @@
+const { Stripe } = require('stripe')
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
+
+const controllerCheckout = {
+    requestPayment: async function(req, res) {
+        const { amount } = req.body
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: Math.round(amount * 100),
+                currency: 'mxn',
+                description: 'pizza de queso',
+                automatic_payment_methods: {
+                    enabled: true
+                },
+                capture_method: 'manual'
+            })
+            const {id, status, client_secret} = paymentIntent
+            return res.json({clientSecret: client_secret, id, status})
+        } catch(error) {
+            return res.json({message: error.message})
+        }
+    }
+}
+
+module.exports = controllerCheckout
