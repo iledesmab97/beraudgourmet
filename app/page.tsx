a8d5885d49be72c95a1e1eb822fdde354ae192a4
Troy Pernía <troy00pernia@gmail.com>
2024-01-17 13:34:36 -0400

    change app/page: remove comment code

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,8 @@
 "use client";
-// import About from './about/page';
-// import Header from '@/components/Header/Header'
-// import Footer from '@/components/Footer/Footer'
-// import { Grid } from '@mui/material';
-import { useEffect } from 'react'
-import { getAllStores } from '@/api/storeApi'
-import useGetStoreList from '@/hooks/useGetStoreList'
 
 export default function Home() {
 
-  const { handleAddStoreList } = useGetStoreList()
-
-  useEffect(() => {
-    getAllStores()
-  }, [])
-
   return (
     <h1>Página principal</h1>
-    // <Grid container>
-    //       <Grid item> <Header /></Grid>
-    //       <Grid item> <About /></Grid>
-    //       <Grid item> <Footer /></Grid>
-    // </Grid>
   );
 }
