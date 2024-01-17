f72ef3faec650f977f331edda357605864a07e40
Troy Pernía <troy00pernia@gmail.com>
2024-01-17 13:32:13 -0400

    remove folder api

--- api/userVerifyApi.js
+++
@@ -1,8 +0,0 @@
-export function verifyEmailUser(token) {
-    return fetch(`http://localhost:3000/api/users/verify/${token}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.message) return { message: data.message}
-            return data[0] >= 1 ? true : false
-        })
-}
