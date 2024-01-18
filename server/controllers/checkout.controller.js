a67b0c54df66be83f03c419be1ecccfe9e3e08f6
Troy Pernía <troy00pernia@gmail.com>
2024-01-18 14:38:23 -0400

    change server/controllers/checkout.controller: add new props to paymentIntent (line 12, 16-18)

--- server/controllers/checkout.controller.js
+++ server/controllers/checkout.controller.js
@@ -4,21 +4,23 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
 
 const controllerCheckout = {
     requestPayment: async function(req, res) {
-        const { amount } = req.body
+        const { amount, userId, email, description } = req.body
         try {
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: Math.round(amount * 100),
                 currency: 'mxn',
-                description: 'pizza de queso',
+                description,
                 automatic_payment_methods: {
                     enabled: true
                 },
-                capture_method: 'manual'
+                capture_method: 'manual',
+                // customer: userId,
+                receipt_email: email
             })
             const {id, status, client_secret} = paymentIntent
-            return res.json({clientSecret: client_secret, id, status})
+            return res.status(200).json({clientSecret: client_secret, id, status})
         } catch(error) {
-            return res.json({message: error.message})
+            return res.status(400).json({message: error.message})
         }
     }
 }
