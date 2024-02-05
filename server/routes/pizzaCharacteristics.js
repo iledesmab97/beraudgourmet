3945c703c04934b4554009b47d7339a187b6c6a7
Troy Pernía <troy00pernia@gmail.com>
2024-02-05 11:33:54 +0100

    change server/routes/pizzaCharacteristics: add functions: getPizzaCharacteristics, addPizzaCharacteristics and removePizzaCharacteristics (line 3), and use these functions (line 7, 9, 11)

--- server/routes/pizzaCharacteristics.js
+++ server/routes/pizzaCharacteristics.js
@@ -1,83 +1,13 @@
 const { Router } = require('express')
-const {PizzaCharacteristic, PizzaMass, PizzaSize} = require('../db')
+
+const { getAllPizzaCharacteristics, addPizzaCharacteristics, removePizzaCharacteristics } = require('../controllers/pizzaCharacteristics.controller')
 
 const router = Router()
 
-router.get('/', async (req, res) => {
-    try {
-        const allPizzaCharacteristics = await PizzaCharacteristic.findAll()
-        const allPizzaCharacteristicsWithText = allPizzaCharacteristics.map(async(pizzaCharacteristics) => {
-            const { id, cost, PizzaMassId, PizzaSizeId } = pizzaCharacteristics
-            const massName = await PizzaMass.findOne({
-                attribute: ['name'],
-                where: {
-                    id: PizzaMassId
-                }
-            })
-            const sizeName = await PizzaSize.findOne({
-                attribute: ['size'],
-                where: {
-                    id: PizzaSizeId
-                }
-            })
-            const pizzaCharacteristicsText = {
-                id,
-                cost,
-                pizzaSize: sizeName.size,
-                pizzaMass: massName.name
-            }
-            return pizzaCharacteristicsText
-        })
-        return Promise.all(allPizzaCharacteristicsWithText)
-            .then(result => res.status(200).json(result))
-            .catch(error => {throw new Error({message: error.message})})
-    } catch(error) {
-        res.status(400).json({message: error.message})
-    }
-})
+router.get('/', getAllPizzaCharacteristics)
 
-router.post('/', async (req, res) => {
-    const { many } = req.query
-    try {
-        if (many && JSON.parse(many)) {
-            const listPizzaCharacteristics = req.body
-            const newListPizzaCharacteristics = listPizzaCharacteristics.map(async (pizzaCharacteristics) => {
-                const newPizzaCharacteristics = await PizzaCharacteristic.create(pizzaCharacteristics)
-                const {PizzaMassId, PizzaSizeId} = pizzaCharacteristics
-                if (PizzaMassId && PizzaSizeId) {
-                    await newPizzaCharacteristics.setPizzaMass(PizzaMassId)
-                    await newPizzaCharacteristics.setPizzaSize(PizzaSizeId)
-                }
-                return newPizzaCharacteristics
-            })
-            return Promise.all(newListPizzaCharacteristics)
-                .then(result => res.status(200).json(result))
-                .catch(error => {throw new Error({message: error.message})})
-        }
+router.post('/', addPizzaCharacteristics)
 
-
-        const { sizeId, masaTypeId} = req.body
-        const newPizzaCharacteristic = await PizzaCharacteristic.create({...req.body})
-        await newPizzaCharacteristic.setPizzaSize(sizeId)
-        await newPizzaCharacteristic.setPizzaMass(masaTypeId)
-        res.status(200).json(newPizzaCharacteristic)
-    } catch(error) {
-        const {message, parent} = error
-        res.status(400).json({message, parent: parent.message})
-    }
-})
-
-router.delete('/', async (req, res) => {
-    const {id} = req.query
-    try {
-        if (!id) return res.status(300).json({message: 'id can\'t be undefined'})
-        const pizzaCharacteristicToRemove = await PizzaCharacteristic.findByPk(id)
-        if (!pizzaCharacteristicToRemove) return res.status(200).json({message: `pizzaCharacteristic with id:${id} does not exist`})
-        await pizzaCharacteristicToRemove.destroy()
-        res.status(200).json({message: `pizzaCharacteristic with id:${id} had been removed successfully`})
-    } catch(error) {
-        res.status(400).json({message: error.message})
-    }
-})
+router.delete('/', removePizzaCharacteristics)
 
 module.exports = router
