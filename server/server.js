cf3ee2591e22048728c2d71d989726ba33d6bb24
Troy Pernía <troy00pernia@gmail.com>
2024-02-13 21:30:01 +0100

    change server/server: add express-session and passport and use it (line 2-3, 8-15)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
 const routes = require('./routes/index')
 const morgan = require('morgan')
 
 const server = express()
-
+server.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+server.use(passport.initialize())
+server.use(passport.session())
 server.use(express.json())
 server.use(morgan('dev'))
 
