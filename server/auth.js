970736c5b8ced944efa902e59752f3fdfbcf8c6a
Troy Pernía <troy00pernia@gmail.com>
2024-02-13 21:26:06 +0100

    add server/auth

---
+++ server/auth.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: '.env.local'})
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NEXT_PUBLIC_PATH_BACK, NEXT_PUBLIC_PATH_FRONT } = process.env
+
+passport.use( new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    // callbackURL: `${NEXT_PUBLIC_PATH_BACK}/auth/google/callback`,
+    // callbackURL: `${NEXT_PUBLIC_PATH_BACK}/auth/google/callback`,
+    callbackURL: `http://localhost:3000/api/auth/google/callback`,
+    passReqToCallback: true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
+    return done(null, profile)
+  }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+    done(null, user)
+})
