4bdc92c6bb186fe1d41ad9f3e95bb5b55ef0251c
Troy Pernía <troy00pernia@gmail.com>
2024-02-20 10:12:39 +0100

    change server/controllers/pizzaMasses.controller: add makePizzaMass and addSizesToPizzaMass function (line 12-34, 53-77)

--- server/controllers/pizzaMasses.controller.js
+++ server/controllers/pizzaMasses.controller.js
@@ -1,4 +1,4 @@
-const {PizzaMass} = require('../db')
+const { PizzaMass, PizzaSize } = require('../db')
 
 async function getAllPizzaMasses(req, res) {
     try {
@@ -9,18 +9,70 @@ async function getAllPizzaMasses(req, res) {
     }
 }
 
+async function makePizzaMass(massData) {
+    // Add a mass
+    const { name, text, sizes } = massData
+    const newPizzaMass = await PizzaMass.create({ name, text })
+
+    // Add sizes to mass
+    if (sizes) {
+        const sizesId = findSizesId(sizes)
+
+        await newPizzaMass.addPizzaSizes(sizesId)
+    }
+
+    return newPizzaMass
+}
+
+async function findSizesId(listSizes) {
+    return await PizzaSize.findAll({
+        attributes: ['id'],
+        where: {
+            size: listSizes
+        }
+    }).then(data => data.map( size => size.id ))
+}
+
 async function addPizzaMass(req, res) {
     const {many} = req.query
     try {
         if (many && JSON.parse(many)) {
-            const newPizzaMasss = await PizzaMass.bulkCreate(req.body)
-            return res.status(200).json(newPizzaMasss)
+            const newPizzaMass = await PizzaMass.bulkCreate(req.body)
+            return res.status(200).json(newPizzaMass)
         }
-        const newPizzaMasss = await PizzaMass.create(req.body)
-        return res.status(200).json(newPizzaMasss)
+        const { name, text, sizes } = massData
+        const newPizzaMass = makePizzaMass({ name, text, sizes })
+
+        return res.status(200).json(newPizzaMass)
     } catch(error) {
         const {message, parent} = error
         return res.status(400).json({message: message, parent: parent?.message})
+    }
+}
+
+async function addSizesToPizzaMass(req, res) {
+    const { name, sizes } = req.body
+    
+    try {
+        if (!sizes) throw new Error('Necesitas indicar la lista de tamaños')
+
+        const mass = await PizzaMass.findOne({
+            attributes: ['id'],
+            where: {
+                name
+            }
+        })
+        if (!mass) throw new Error('Esta masa no se encuentra ne la base de datos')
+
+        const sizesId = await findSizesId(sizes)
+
+        if (!sizesId.length) throw new Error('Los tamaños indicados no están en la base de datos')
+
+        await mass.addPizzaSizes(sizesId)
+        res.status(200).json({ message: 'Los tamaños fueron añadidos exitosamente'})
+
+    } catch(error) {
+        res.status(400).json({ message: error.message})
     }
 }
 
@@ -40,5 +92,6 @@ async function removePizzaMass(req, res) {
 module.exports = {
     getAllPizzaMasses,
     addPizzaMass,
+    addSizesToPizzaMass,
     removePizzaMass
 }
