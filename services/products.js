67ac61ce71f92fca34dc2c4b4cc05d55d6295fb4
Troy Pernía <troy00pernia@gmail.com>
2024-02-24 19:07:21 +0100

    add services/products

---
+++ services/products.js
@@ -0,0 +1,101 @@
+const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK
+
+export function fetchPizzas() {
+    return fetch(`${PATH_BACK}/pizzas`)
+      .then(response => response.json())
+      .then(data => {
+        const pizzaList = data.map(pizza => {
+            const { id, name, text, image, ingredients } = pizza
+            const newPizzaData = {
+              id,
+              name,
+              text,
+              image,
+              ingredients
+            }
+            return newPizzaData
+        })
+        return pizzaList
+    })
+}
+
+export function fetchPizzasCharacteristics({type}) {
+    return fetch(`${PATH_BACK}/pizzaCosts`)
+      .then(response => response.json())
+      .then(data => {
+        const pizzaCostsList = data.map(pizzaCost => {
+          const { id, cost, costIVA, pizza, pizzaCharacteristics } = pizzaCost
+          const newPizzaCost = {
+            id,
+            cost,
+            costIVA,
+            pizza,
+            pizzaCharacteristics
+          }
+          return newPizzaCost
+        })
+        if (type === 'object') {
+          const listCostsObject = {}
+          pizzaCostsList.forEach(pizzaCost => {
+            const { costIVA, pizza, pizzaCharacteristics } = pizzaCost
+            const { mass, size } = pizzaCharacteristics
+  
+            if (listCostsObject[pizza]) {
+              // cost per mass
+              const costPerMass = {
+                ...listCostsObject[pizza][size],
+                [mass]: costIVA
+              }
+              // mass per size
+              const massPerSize = {
+                ...listCostsObject[pizza],
+                [size]: costPerMass
+              }
+  
+              listCostsObject[pizza] = massPerSize
+  
+            } else {
+              // cost per mass
+              const costPerMass = {
+                [mass]: costIVA
+              }
+              // mass per size
+              const massPerSize = {
+                [size]: costPerMass
+              }
+  
+              listCostsObject[pizza] = massPerSize
+            }
+          })
+          return listCostsObject
+        }
+        return pizzaCostsList
+    })
+}
+
+export async function fetchExtraIngredients() {
+    return fetch(`${PATH_BACK}/pizzaExtraIngredients`)
+      .then(response => response.json())
+      .then(data => {
+        const extraIngredinetList = {}
+        data.forEach(extraIngredient => {
+          const {id, name, cost} = extraIngredient
+          extraIngredinetList[name] = {
+            id,
+            name,
+            price: cost
+          }
+        })
+        return extraIngredinetList
+    })
+}
+
+export async function fetchingData() {
+    const pizzasList = await fetchPizzas()
+    const pizzaCharacteristicsList = await fetchPizzasCharacteristics({type: 'object'})
+    const totalPizzasList = pizzasList.map(pizza => ({
+      ...pizza,
+      price: pizzaCharacteristicsList[pizza.name]
+    }))
+    return totalPizzasList
+}
