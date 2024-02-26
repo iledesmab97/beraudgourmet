bebd48d9d6e65254677962cf5396b54bcde70a38
Troy Pernía <troy00pernia@gmail.com>
2024-02-26 12:36:57 +0100

    change components/WhatsappButton: import consts: phoneNumber and text1 (line 3) and use theme (line 24)

--- components/WhatsappButton/WhatsappButton.jsx
+++ components/WhatsappButton/WhatsappButton.jsx
@@ -1,15 +1,13 @@
 'use client'
 
-import Image from 'next/image'
+import { phoneNumber, text1 } from '@/utils/contact'
 
+import Image from 'next/image'
 import Box from '@mui/material/Box'
 import Fab from '@mui/material/Fab'
 import Link from '@mui/material/Link'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import whatsappImage from '@/public/images/homeimg/logos/whatsapp.svg'
-
-const phoneNumber = '584120146661'
-const text = 'Hola mundo!'
 
 function WhatsappButton() {
     return (
@@ -23,7 +21,7 @@ function WhatsappButton() {
             }}
         >
             <Link
-                href={`https://wa.me/${phoneNumber}/?text=${text}`}
+                href={`https://wa.me/${phoneNumber}/?text=${text1}`}
                 // href={`https://wa.me/?text=${text}`}
                 target='_blank'
                 sx={{
