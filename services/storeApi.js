6309febb930ac1c27d008c302fdf4698388eab76
Troy Pernía <troy00pernia@gmail.com>
2024-02-26 13:56:21 +0100

    change services/storeApi: import functions: getAllSchedules and inOpen (line 1-2). add function updateStores (line 22-44)

--- services/storeApi.js
+++ services/storeApi.js
@@ -1,3 +1,6 @@
+import { getAllSchedules } from '@/services/scheduleApi'
+import { isOpen } from '@/utils/hours'
+
 const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK
 
 export function getAllStores() {
@@ -14,4 +17,28 @@ export function getAllStores() {
         }))
         return newData
         })
+}
+
+export async function updateStores() {
+    const scheduels = await getAllSchedules()
+    const storesList = await getAllStores()
+    const closeTime = scheduels[0].scheduleHoursList[0].endTime
+    const openTime = scheduels[0].scheduleHoursList[0].startTime
+    const pickUpSchedule = scheduels[1].scheduleHoursList.map(schedule => ({
+      days: schedule.days,
+      hours: `${schedule.startTime} - ${schedule.endTime}`
+    }))
+    const deliverySchedule = scheduels[2].scheduleHoursList.map(schedule => ({
+      days: schedule.days,
+      hours: `${schedule.startTime} - ${schedule.endTime}`
+    }))
+    const storesWithSchedulsList = storesList.map(store => ({
+      ...store,
+      closeTime,
+      openTime,
+      pickUpSchedule,
+      deliverySchedule,
+      open: isOpen({closeTime, openTime})
+    }))
+    return storesWithSchedulsList
 }
