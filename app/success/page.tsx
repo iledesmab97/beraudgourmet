32500c5e97e15c0928d86f80688a953dfbcda537
Troy Pernía <troy00pernia@gmail.com>
2024-02-27 09:00:21 +0100

    change app/success/page.tsx: add Grid and Box components (line 3-7) and use it (line 11-28)

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,8 +1,31 @@
 'use client'
 
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import styles from './page.module.css'
+
 function SuccessPay() {
     return (
-        <h1>Compra exitosa</h1>
+        <Grid
+            container
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+            sx={{
+                // bgcolor: 'green',
+                py: '5%'
+            }}
+        >
+            <Box
+                className={styles.containerPrimaryTitle}
+            >
+                <ShoppingCartIcon/>
+                <h1 className={styles.PrimaryTitle}>Compra exitosa</h1>
+                <h3>Gracias por elegirnos</h3>
+            </Box>
+        </Grid>
     )
 }
 
