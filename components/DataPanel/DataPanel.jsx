d61f3a2fd6b5f3390428a07e22018489d3c45d07
Troy Pernía <troy00pernia@gmail.com>
2024-02-28 12:46:36 +0100

    change components/DataPanel: import components: TabBar and DataTable (line 3-4), and change return (line 14-25)

--- components/DataPanel/DataPanel.jsx
+++ components/DataPanel/DataPanel.jsx
@@ -1,11 +1,28 @@
 'use client'
 
+import TabBar from '@/components/TabBar/TabBar'
+import DataTable from '@/components/DataTable/DataTable'
+
 import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+import styles from './DataPanel.module.css'
 
 function DataPanel() {
     return (
-        <Grid item xs={9}>
-            Contenido
+        <Grid
+            item
+            xs={9}
+            className={styles.DataPanel}
+        >
+            <Typography
+                variant='encabezado'
+            >
+                Historial de Ordenes
+            </Typography>
+            <TabBar />
+            <DataTable />
         </Grid>
     )
 }
