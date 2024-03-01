42cd43a25e1aed6969382432bd90284e8225204a
Troy Pernía <troy00pernia@gmail.com>
2024-03-01 11:40:42 -0400

    change server/models/DeliveryInformation: add prop "address" (line 10-12)

--- server/models/DeliveryInformation.js
+++ server/models/DeliveryInformation.js
@@ -7,6 +7,9 @@ function DeliveryInformation(database) {
             primaryKey: true,
             autoIncrement: true
         },
+        address: {
+            type: DataTypes.STRING,
+        },
         typeResidence: {
             type: DataTypes.STRING,
         },
