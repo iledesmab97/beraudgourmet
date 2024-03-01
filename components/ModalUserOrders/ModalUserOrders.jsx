1532ab4a1f388f2ce8827c6e53620907c96b4638
Troy Pernía <troy00pernia@gmail.com>
2024-03-01 13:02:34 -0400

    change components/ModalUserOrders: import components and function  (line 4-20). add const style (line 25-39). Add states: user and orders (line 44-45) add useEffect hook (line 47-50). change Grid by Box componente (line 57-102)

--- components/ModalUserOrders/ModalUserOrders.jsx
+++ components/ModalUserOrders/ModalUserOrders.jsx
@@ -1,16 +1,55 @@
 'use client'
 
 import Modal from '@mui/material/Modal'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
+
+import { useState, useEffect } from 'react'
 import useGetModal from '@/hooks/useGetModal'
+import useGetUser from '@/hooks/useGetUser'
+
+import { getAllOrders } from '@/services/userApi'
 
 import styles from './ModalUserOrders.module.css'
 
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 700,
+    height: 700,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    borderRadius: 5,
+    p: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItem: 'center'
+}
+
+const tableHeader = [ 'Pedido', 'Precio ($)', 'Estatus' ]
+
 function ModalUserOrders() {
 
     const { open, handleChangeModal } = useGetModal({ modalType: 'userOrders' })
+    const { user } = useGetUser()
+    const [ orders, setOrders ] = useState([])
+
+    useEffect(() => {
+        if (!user.id) return
+        getAllOrders(user.id).then( data => setOrders(data) )
+    }, [open])
 
     return (
         <Modal
@@ -18,10 +57,67 @@ function ModalUserOrders() {
             onClose={() => { handleChangeModal('userOrders', 'user') }}
         >
             <Box
-                className={styles.ModalUserOrdersContainer}
-                sx={{ bgcolor: 'background.paper' }}
+                sx={style}
+                // container
+                // direction='column'
+                // // justifyContent='flex-start'
+                // // spacing={1}
+                // alignItems='stretch'
             >
-                Estoy en el modal de las ordenes de los usuarios
+                <Typography variant='title' sx={{ flexGrow: 1 }} align='center' >Historial de Ordenes</Typography>
+                {/* <Grid
+                    item
+                    // xs={12}
+                    sx={{
+                        display: 'flex',
+                        // flexDirection: 'row',
+                        justifyContent: 'center',
+                        flexGrow: 2
+                    }}
+                >
+                    <Typography variant='title' >Historial de Ordenes</Typography>
+                </Grid> */}
+                <Grid
+                    item
+                    sx={{
+                        flexGrow: 1,
+                        overflowY: 'auto'
+                    }}
+                >
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell >Pedido</TableCell>
+                                    <TableCell
+                                        align='right'
+                                        sx={{
+                                            width: '100px'
+                                        }}
+                                    >
+                                        Precio($)
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{
+                                            width: '120px'
+                                        }}
+                                    >Estatus</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    orders.map( order => (
+                                        <TableRow>
+                                            <TableCell >1 pizza</TableCell>
+                                            <TableCell align='right'>{order.totalCost}</TableCell>
+                                            <TableCell >{order.closed ? 'Entregado' : 'Pendiente'}</TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
             </Box>
         </Modal>
     )
