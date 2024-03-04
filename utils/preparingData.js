5e2ac907c11ef56f9a6f5089745c293ff7ef17cb
Troy Pernía <troy00pernia@gmail.com>
2024-03-04 13:02:25 -0400

    change utils/preparingData: add const regExpIngredientsOut (line 4), add functions descriptionOrder (line 38-46) and extractIngredientsOut (line 48-51)

--- utils/preparingData.js
+++ utils/preparingData.js
@@ -1,6 +1,7 @@
 import { ROLES } from '@/config/user'
 
 const sameProperties = [ 'name', 'email', 'promotion', 'verified']
+const regExpIngredientsOut = /\~(.*?)\~/g
 
 export function userDataFromBackToFront(userBack) {
     const {id, name, email, phoneNumber, promotion, verified, RoleId} = userBack
@@ -32,4 +33,19 @@ export function oneUserDataFromFrontToBack(propertyFront) {
     if (sameProperties.includes(property)) return propertyFront
     else if ( property === 'numberPhone' ) return { ...propertyFront, property: 'phoneNumber' }
     return propertyFront
+}
+
+export function descriptionOrder(order) {
+    const primrayData = order.quantity + ' x ' + order.name + ` (${order.size})`
+    const extraIngredients = `${order.mass}${Object.keys(order.extra).map(ingredient => {
+        return `, ${order.extra[ingredient]}x ${ingredient}`
+        }).join('')
+        }`
+    const ingredientsOut = order.ingredientsModal.map( ingredient => `~${ingredient}~` ).join(', ')
+    return primrayData + (extraIngredients ? ', ' : '') + extraIngredients + (ingredientsOut ? ', ' : '') + ingredientsOut
+}
+
+export function extractIngredientsOut(text) {
+    const resultados = [...text.matchAll(regExpIngredientsOut)]
+    return resultados.map(ingredient => ingredient[1])
 }
