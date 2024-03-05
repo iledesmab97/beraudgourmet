cd33e0824559b7ac406447711c6efcd58256bca6
Troy Pernía <troy00pernia@gmail.com>
2024-03-05 14:49:44 -0400

    change components/DataTable: Change component DataTable, adding states anchorEl and currentorder (line 26-27), const open (line 28), functions handleClick, handleClose and changeStatus (line 30-47). Change TableCell components (line 70-71), and add a IconButton to last TableCell (line 73-81). Add a menu component (line 88-98)

--- components/DataTable/DataTable.jsx
+++ components/DataTable/DataTable.jsx
@@ -6,7 +6,14 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import IconButton from '@mui/material/IconButton'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import Paper from '@mui/material/Paper';
+
+import { useState } from 'react';
+import { updateOrder, getAllOrders } from '@/services/orderApi'
 
 import styles from './DataTable.module.css'
 
@@ -14,37 +21,82 @@ const tableHeaders = {
     orders: [ 'Nombre', 'Teléfono' ,'Método de Pago','Fecha de entrega', 'Tipo', 'Estatus', 'Total ($)', 'Acción' ]
 }
 
-function DataTable({ orders }) {
+function DataTable({ orders, updateOrders }) {
+
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [currentOrder, setCurrentOrder] = useState(null)
+    const open = Boolean(anchorEl)
+
+    function handleClick(event, order) {
+        setAnchorEl(event.currentTarget)
+        setCurrentOrder(order)
+    }
+
+    function handleClose() {
+        setAnchorEl(null)
+    }
+
+    async function changeStatus() {
+        const body = {
+            property: 'closed',
+            value: !currentOrder.closed
+        }
+        const response = await updateOrder(currentOrder.id, body)
+        await handleClose()
+        getAllOrders().then(data => updateOrders(data))
+    }
+
     return (
-        <TableContainer className={styles.DataTable} component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
+        <>
+            <TableContainer className={styles.DataTable} component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            {
+                                tableHeaders.orders.map(column => (
+                                    <TableCell key={column} align='center'>{column}</TableCell>
+                                ))          
+                            }
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
                         {
-                            tableHeaders.orders.map(column => (
-                                <TableCell align='center'>{column}</TableCell>
-                            ))          
+                            orders.map((order) => (
+                                <TableRow key={order.id}>
+                                    <TableCell align='center'>{ order.user.name }</TableCell>
+                                    <TableCell align='center'>{ order.user.phoneNumber }</TableCell>
+                                    <TableCell align='center'>{ order.paymentMethod === 'transfer' ? 'Transferencia' : 'Stripe' }</TableCell>
+                                    <TableCell align='center'>{ order.deliveryDate }</TableCell>
+                                    <TableCell align='center'>{ order.delivery ? 'Delivery' : 'Recoger' }</TableCell>
+                                    <TableCell align='center'>{ order.closed ? 'Entregado' : 'Pendiente' }</TableCell>
+                                    <TableCell align='center'>{ order.totalCost }</TableCell>
+                                    <TableCell align='center'>
+                                        <IconButton
+                                            onClick={(event) => {
+                                                handleClick(event, order)
+                                            }}
+                                        >
+                                            <MoreHorizIcon />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))
                         }
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        orders.map(order => (
-                            <TableRow>
-                                <TableCell align='center'>{order.user.name}</TableCell>
-                                <TableCell align='center'>{order.user.phoneNumber}</TableCell>
-                                <TableCell align='center'>{order.StripeId ? 'Stripe' : 'Transferencia' }</TableCell>
-                                <TableCell align='center'>{order.deliveryDate}</TableCell>
-                                <TableCell align='center'>Delivery</TableCell>
-                                <TableCell align='center'>Pendiente</TableCell>
-                                <TableCell align='center'>{order.totalCost}</TableCell>
-                                <TableCell align='center'>...</TableCell>
-                            </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                <MenuItem
+                    onClick={changeStatus}
+                >
+                    { currentOrder?.closed ? 'Pendiente' : 'Entregado' }
+                </MenuItem>
+            </Menu>
+        </>
     )
 }
 
