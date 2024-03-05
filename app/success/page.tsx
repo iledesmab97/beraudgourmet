90e6accd60d660c0ba5e506c4637ad14904d7c5b
Troy Pernía <troy00pernia@gmail.com>
2024-03-05 11:05:12 -0400

    change app/success/page: import userRouter (line 3) and function saveModal (line 9), and use the hook (line 15) for redirect to "/menu" (line 40) and save in locaStorage the modal to open (line 39). Add a Button component (line 36-44)

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 
+import { useRouter } from 'next/navigation';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import { saveModal } from '@/utils/modal'
 
 import styles from './page.module.css'
 
 function SuccessPay() {
+
+    const router = useRouter()
+
     return (
         <Grid
             container
@@ -22,8 +29,19 @@ function SuccessPay() {
                 className={styles.containerPrimaryTitle}
             >
                 <ShoppingCartIcon/>
-                <h1 className={styles.PrimaryTitle}>Compra exitosa</h1>
-                <h3>Gracias por elegirnos</h3>
+                <Box className={styles.containerTitles}>
+                    <h1 className={styles.PrimaryTitle}>Compra exitosa</h1>
+                    <h3>Gracias por elegirnos</h3>
+                </Box>
+                <Button
+                    variant='contained'
+                    onClick={() => {
+                        saveModal('userOrders')
+                        router.push('/menu')
+                    }}
+                >
+                    Ver historial de compras
+                </Button>
             </Box>
         </Grid>
     )
