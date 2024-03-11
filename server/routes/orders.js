051f05bc4e1ee79fb5589faf11aafb72893a5c6f
Troy Pernía <troy00pernia@gmail.com>
2024-03-11 17:43:32 -0400

    change server/routes/orders: import function uploadImageOrder (line 4), multer and use multer (line 6,8), and add new routes: "/" with PUT request (line 20), "/image/:id" with POST request (line 16)

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,6 +1,11 @@
 const { Router } = require('express')
 const { Order } = require('../db')
-const { getAllOrders, addOrder, removeOrder, changeProperty } = require('../controllers/orders.controller')
+const { getAllOrders, addOrder, removeOrder, changeProperty, changePropertyForAll, changePropertiesOrder, uploadImageOrder } = require('../controllers/orders.controller')
+const {verifyToken, isRoot, isAdmin} = require('../middlewares')
+
+const multer = require('multer')
+
+const upload = multer({ storage: multer.memoryStorage() })
 
 const router = Router()
 
@@ -8,10 +13,11 @@ router.get('/', getAllOrders)
 router.get('/:userId', getAllOrders)
 
 router.post('/', addOrder)
+router.post('/image/:id', [upload.single('file')], uploadImageOrder)
 
 router.delete('/', removeOrder)
 
-router.put('/', changeProperty )
+router.put('/', changePropertyForAll )
 router.put('/:id', changeProperty )
 
 module.exports = router
