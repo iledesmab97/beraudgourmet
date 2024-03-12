3a065e479f875e0e97f0dce41ef7f54150473f83
Troy Pernía <troy00pernia@gmail.com>
2024-03-12 17:37:46 -0400

    change server/routes/scheduleHours: remove functions from every route and import the functions to use from controllers (line 5, 9, 11, 13, 15-17)

--- server/routes/scheduleHours.js
+++ server/routes/scheduleHours.js
@@ -2,66 +2,18 @@ const { Router } = require('express')
 const {ScheduleHours} = require('../db')
 const dayjs = require('dayjs')
 
+const { getAllScheduleHours, addScheduleHours, removeScheduleHour, changeScheduleHourFormatTime, updateScheduleHour } = require('../controllers/scheduleHour.controller')
+
 const router = Router()
 
-router.get('/', async (req, res) => {
-    try {
-        const allScheduleHours = await ScheduleHours.findAll()
-        res.status(200).json(allScheduleHours)
-    } catch(error) {
-        res.status(400).json({message: error.message})
-    }
-})
+router.get('/', getAllScheduleHours)
 
-router.post('/', async (req, res) => {
-    const { many } = req.query
-    try {
-        if (many && JSON.parse(many)) {
-            const newScheduleHours = await ScheduleHours.bulkCreate(req.body)
-            return res.status(200).json(newScheduleHours)
-        }
-        const newScheduleHour = await ScheduleHours.create({...req.body})
-        res.status(200).json(newScheduleHour)
-    } catch(error) {
-        const {message, parent} = error
-        res.status(400).json({message, parent: parent.message})
-    }
-})
+router.post('/', addScheduleHours)
 
-router.delete('/', async (req, res) => {
-    const {id} = req.query
-    try {
-        if (!id) return res.status(300).json({message: 'id can\'t be undefined'})
-        const scheduleHoursToRemove = await ScheduleHours.findByPk(id)
-        if (!scheduleHoursToRemove) return res.status(200).json({message: `scheduleHours with id:${id} does not exist`})
-        await scheduleHoursToRemove.destroy()
-        res.status(200).json({message: `scheduleHours with id:${id} had been removed successfully`})
-    } catch(error) {
-        res.status(400).json({message: error.message})
-    }
-})
+router.delete('/', removeScheduleHour)
 
-router.put('/changeFormat', async (req, res) => {
-    const { prevFormat, newFormat } = req.body
-    try {
-        const allScheduleHours = await ScheduleHours.findAll()
-        for (let timetable of allScheduleHours) {
-            const { id, startTime, endTime } = timetable
-            const newStartTime = dayjs(`${dayjs().format('YYYY')} ${startTime}`, `YYYY ${prevFormat}`).format(newFormat)
-            const newEndTime = dayjs(`${dayjs().format('YYYY')} ${endTime}`, `YYYY ${prevFormat}`).format(newFormat)
-            const newTimetable = await ScheduleHours.update({
-                startTime: newStartTime,
-                endTime: newEndTime
-            }, {
-                where: {
-                    id
-                }
-            })
-        }
-        res.status(200).json({ message: 'The format time had been updated successfully' })
-    } catch(error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+// router.put('/', updateScheduleHour)
+router.put('/:id', updateScheduleHour)
+router.put('/changeFormat', changeScheduleHourFormatTime)
 
 module.exports = router
