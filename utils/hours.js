43d8e4538ef28f7333de5d2e575849f434b972e1
Troy Pernía <troy00pernia@gmail.com>
2024-03-12 11:59:45 -0400

    change utils/hours: add functions dateStringToDate (line 10-20) and howMuchLeft (line 22-31)

--- utils/hours.js
+++ utils/hours.js
@@ -6,3 +6,26 @@ export function isOpen({openTime, closeTime}) {
     const closeTimeDay = dayjs(`${now.format('D')} ${closeTime}`, 'D hh:mm a')
     return now.isAfter(openTimeDay) && now.isBefore(closeTimeDay)
 }
+
+export function dateStringToDate(date) {
+    const dateString = date
+    const [dateWithSlash, time] = dateString.split(' - ')
+    const [day, month, year] = dateWithSlash.split('/')
+    const [hour_min, am_pm] = time.split(" ")
+    let [hour, mine] = hour_min.split(':')
+    hour = am_pm === 'am' ? hour !== '12' ? hour : '00' : hour !== '12' ? String((Number(hour) + 12)) : hour
+
+    const dateA = dayjs(`${year}-${month}-${day} ${hour}:${mine}`, ['YYYY', 'YYYY-MM-DD', 'YYYY-MM-DD HH:mm'])
+    return dateA
+}
+
+export function howMuchLeft(dateString) {
+    const date = dateStringToDate(dateString)
+    const now = dayjs()
+    let comparation = date.isBefore(now)
+    if (!comparation) {
+        return date.isSame(now, 'day') ? 'today' : 'early'
+    } else {
+        return 'late'
+    }
+}
