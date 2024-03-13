59e0856fe6a820d87aa29071dc3be03aeaed3482
Troy Pernía <troy00pernia@gmail.com>
2024-03-13 12:45:07 -0400

    change services/checkoutApi: add function updatePaymentRequest (line 17-30) and chang name function from "sendRequestPayment" to createPaymentRequest (line 1)

--- services/checkoutApi.js
+++ services/checkoutApi.js
@@ -1,4 +1,4 @@
-export function sendRequestPayment({userId, email, amount, description, payInPlace}) {
+export function createPaymentRequest({userId, email, amount, description, payInPlace}) {
     return fetch('api/checkout', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
@@ -11,5 +11,20 @@ export function sendRequestPayment({userId, email, amount, description, payInPla
         })
     })
         .then(res => res.json())
-            .then(data => data)
+        .then(data => data)
+}
+
+export function updatePaymentRequest({amount, stripeId, description, payInPlace}) {
+    return fetch('api/checkout', {
+        method: 'PUT',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            amount,
+            stripeId,
+            description,
+            capture: payInPlace ? 'manual' : 'automatic'
+        })
+    })
+        .then(res => res.json())
+        .then(data => data)
 }
