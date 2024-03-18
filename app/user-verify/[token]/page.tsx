35d590c485eb45ce472ee801780d399256ea8c40
Troy Pernía <troy00pernia@gmail.com>
2024-03-18 12:46:45 -0400

    change app/user-verify/[token]/page: change props (line 7), function verifyEmail (line 12-13), and render (line 19-23)

--- app/user-verify/[token]/page.tsx
+++ app/user-verify/[token]/page.tsx
@@ -4,19 +4,23 @@ import { useState } from "react"
 import Button from "@mui/material/Button"
 import { verifyEmailUser } from '@/services/userApi'
 
-function UserTokenVerify({ params }) {
+function UserTokenVerify({ params }: { params: any }) {
 
     const [ verified, setVerified ] = useState(false)
 
     async function verifyEmail() {
-        const response = await verifyEmailUser(params.token)
-        if (response.message) alert(response.message)
+        const response: any = await verifyEmailUser(params.token)
+        if (response.name) alert(response.name)
         if (response) setVerified(true)
     }
 
     return (
         <>
-            <Button variant='contained' margin='dense' onClick={verifyEmail}>
+            <Button
+                variant='contained'
+                // margin='dense'
+                onClick={verifyEmail}
+            >
                 {verified ? 'Correo verificado' : 'Verificar correo'}
             </Button>
         </>
