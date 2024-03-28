eb4e4145de32f957b6f52165cc234c31474b36d2
Troy Pernía <troy00pernia@gmail.com>
2024-03-27 21:32:00 -0400

    change services/authApi, hooks/userLogedUser, hooks/useHandleUser and app/menu/page: remove comment code and console.logs

--- services/authApi.js
+++ services/authApi.js
@@ -1,6 +1,5 @@
 const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK
 
 export function fetchAuthGoogle() {
-    console.log('voy a empezar con la autenticación')
     window.location.href = `${PATH_BACK}/auth/google`
 }
