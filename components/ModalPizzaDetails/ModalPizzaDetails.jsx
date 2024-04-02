9b0bc3a4f43bfe9303f0348a21072f9326029254
Troy Pernía <troy00pernia@gmail.com>
2024-04-02 13:24:36 -0400

    change components/ModalPizzaDetails: add Images (line 90-107), text of pizza (line 109-114), ingredients (line 116-154), and sizes and masses (line 156-212)

--- components/ModalPizzaDetails/ModalPizzaDetails.jsx
+++ components/ModalPizzaDetails/ModalPizzaDetails.jsx
@@ -1,13 +1,32 @@
 'use client'
 
+import Image from 'next/image'
 import Modal from '@mui/material/Modal'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
+import Input from '@mui/material/Input'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
+
 import OrderData from '@/components/ModalOrderDetails/OrderData'
 import OtherData from '@/components/ModalOrderDetails/OtherData'
 import PriceData from '@/components/ModalOrderDetails/PriceData'
+
+import { useState } from 'react'
 
 // import styles from './ModalOrderDetails.module.css'
 import dayjs from 'dayjs'
@@ -32,6 +51,12 @@ const style = {
 
 function ModalPizzaDetails({ openPizzaDetail, handleOpenPizzaDetail, currentPizza }) {
 
+    const [openColapse, setOpenColapse] = useState(false)
+
+    function handleOpenColapse() {
+        setOpenColapse(prevState => !prevState)
+    }
+
     return (
         <Modal
             open={ openPizzaDetail }
@@ -48,7 +73,7 @@ function ModalPizzaDetails({ openPizzaDetail, handleOpenPizzaDetail, currentPizz
                 >
                     {`Pizza ${currentPizza.name} Nº ${currentPizza.id}`}
                 </Typography>
-                {/* <Box
+                <Box
                     sx={{
                         height: '90%',
                         width: '100%',
@@ -56,12 +81,141 @@ function ModalPizzaDetails({ openPizzaDetail, handleOpenPizzaDetail, currentPizz
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
+                        justifyContent: 'flex-start',
                         gap: '16px',
                         pr: '8px',
                         boxSizing: 'border-box'
                     }}
                 >
-                    <OrderData currentPizza={currentPizza} />
+                    <Box
+                        sx={{
+                            width: '100%',
+                            height: '200px',
+                            minHeight: '200px',
+                            position: 'relative'
+                        }}
+                    >
+                        <Image
+                            src={currentPizza.image}
+                            alt={currentPizza.name}
+                            fill={true}
+                            sizes='auto'
+                            style={{
+                                objectFit: 'contain'
+                            }}
+                        />    
+                    </Box>
+                    <Divider sx={{ width: '100%'}} />
+                    <Input
+                        fullWidth={true}
+                        readOnly={true}
+                        // disabled={true}
+                        value={currentPizza.text}
+                    />
+                    <Divider sx={{ width: '100%'}} />
+                    <Box
+                        sx={{
+                            width: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            // alignItems: 'center'
+                        }}
+                    >
+                        <Typography variant='title' sx={{ alignSelf: 'center' }}>
+                            Ingredientes
+                        </Typography>
+                        <List
+                            // sx={{
+                            //     display: 'flex',
+                            //     flexDirection: 'column',
+                            //     alignItems: 'flex-start'
+                            // }}
+                        >
+                            {
+                                currentPizza.ingredients.map(ingredient => (
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={
+                                                <Input
+                                                    // fullWidth={true}
+                                                    readOnly={true}
+                                                    // disabled={true}
+                                                    value={ingredient}
+                                                />
+                                                // <Typography variant='p'>
+                                                //     {ingredient}
+                                                // </Typography>
+                                            }
+                                        />
+                                    </ListItem>
+                                ))
+                            }       
+                        </List>
+                    </Box>
+                    <Divider sx={{ width: '100%'}} />
+                    <Box>
+                        <Typography variant='title'>
+                            Tamaños y Masas Disponibles
+                        </Typography>
+                        <List>
+                            {
+                                Object.entries(currentPizza.size).map(([size, masses]) => (
+                                    <>
+                                        <ListItem
+                                            onClick={handleOpenColapse}
+                                        >
+                                            <ListItemText>
+                                                <Input
+                                                    readOnly={true}
+                                                    value={size}
+                                                />
+                                            </ListItemText>
+                                            {openColapse ? <ExpandLess /> : <ExpandMore />}
+                                        </ListItem>
+                                        <Collapse in={openColapse} timeout={'auto'}>
+                                            <TableContainer>
+                                                <Table>
+                                                    <TableHead>
+                                                        <TableRow>
+                                                            <TableCell>Masa</TableCell>
+                                                            <TableCell>Costo</TableCell>
+                                                        </TableRow>
+                                                    </TableHead>
+                                                    <TableBody>
+                                                        {
+                                                            Object.entries(masses).map(([mass, cost]) => (
+                                                                <TableRow>
+                                                                    <TableCell>
+                                                                        <Input
+                                                                            readOnly={true}
+                                                                            value={mass}
+                                                                        />
+                                                                    </TableCell>
+                                                                    <TableCell>
+                                                                        <Input
+                                                                            readOnly={true}
+                                                                            value={cost}
+                                                                        />
+                                                                    </TableCell>
+                                                                </TableRow>
+                                                            ))
+                                                        }
+                                                    </TableBody>
+                                                </Table>
+                                            </TableContainer>
+                                        </Collapse>
+                                    </>
+
+                                ))
+                            }
+                        </List>
+                    </Box>
+                    <Divider/>
+                    {/* <h1>hola </h1>
+                    <h1>hola </h1>
+                    <h1>hola </h1>
+                    <h1>hola </h1> */}
+                    {/* <OrderData currentPizza={currentPizza} />
                     <Divider sx={{ width: '100%'}} />
 
                     <PriceData
@@ -75,8 +229,8 @@ function ModalPizzaDetails({ openPizzaDetail, handleOpenPizzaDetail, currentPizz
                         dateEmited={currentPizza.applicationDate}
                         dateToRecive={currentPizza.deliveryDate}
                         deliveryInformation={currentPizza.deliveryInformation}
-                    />   
-                </Box>                 */}
+                    />    */}
+                </Box>                
             </Grid>
         </Modal> 
     )
