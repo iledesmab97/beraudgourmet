f3889b5c296135b3a9383437f0e0a7af73a7c0fd
Troy Pernía <troy00pernia@gmail.com>
2024-04-03 13:42:59 -0400

    add components/InputUpdate

---
+++ components/InputUpdate/InputUpdate.jsx
@@ -0,0 +1,70 @@
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check'
+
+import { useState } from 'react'
+import useGetAlertMessage from '@/hooks/useGetAlertMessage'
+
+function InputUpdate({value, updateProperty, properties, updateState}) {
+    
+    const [myValue, setMyValue] = useState(value)
+    const [edit, setEdit] = useState(false)
+    const { handleUpdateAlertMessage } = useGetAlertMessage()
+
+    function handleChange(event) {
+        setMyValue(event.target.value)
+    }   
+
+    async function handleEdit() {
+        if (edit && myValue !== value) {
+            const { property, id } = properties
+            const response = await updateProperty( id, {property, value: myValue})
+            let text, status
+            if (response.message) {
+                text = response.message
+                status = 'error'
+            } else {
+                text = response
+                status = 'success'
+            }
+            handleUpdateAlertMessage({
+                checked: true,
+                text,
+                status
+            })
+            if (!response.message) updateState({
+                type: 'pizzas',
+                id,
+                property,
+                value: myValue
+            })
+        }
+        setEdit(prevState => !prevState)
+    }
+
+    return (
+        <TextField
+            value={myValue}
+            onChange={handleChange}
+            disabled={!edit}
+            InputProps={{
+                endAdornment: (
+                    <IconButton
+                        onClick={handleEdit}
+                    >
+                        {
+                            edit ? (
+                                <CheckIcon />
+                            ) : (
+                                <EditIcon />
+                            )
+                        }
+                    </IconButton>
+                )
+            }}
+        />
+    )
+}
+
+export default InputUpdate
