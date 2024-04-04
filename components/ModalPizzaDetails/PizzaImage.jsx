69044cd7275fc36fd04dc8f5dc28bc4e65395ff1
Troy Pernía <troy00pernia@gmail.com>
2024-04-04 12:24:01 -0400

    change components/ModalPizzaDetails/PizzaImage: add input for edit image (line 71-88), add button to enable input (line 57-69), change url static (line 47-56), add functions handles for edit url, handle errors (line 18-33), add function urlValid for validated url of input (line 10-14)

--- components/ModalPizzaDetails/PizzaImage.jsx
+++ components/ModalPizzaDetails/PizzaImage.jsx
@@ -1,26 +1,92 @@
+import TextField from '@mui/material/TextField'
 import Image from 'next/image'
 import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
+import EditIcon from '@mui/icons-material/Edit'
+import CheckIcon from '@mui/icons-material/Check'
 
-function PizzaImage({pizza}) {
+import { useState } from 'react'
+
+const regexImage = /^https?:\/\/.*\.(jpeg|jpg|png|gif|bmp)$/i
+
+function urlValid(url) {
+    return regexImage.test(url)
+}
+
+function PizzaImage({ pizza }) {
+
+    const [openInput, setOpenInput] = useState(false)
+    const [url, setUrl] = useState('')
+    const [urlFallback, setUrlFallback] = useState('')
+
+    function handleClick() {
+        setOpenInput(prevState => !prevState)
+    }
+
+    function handleChange(event) {
+        setUrlFallback('')
+        setUrl(event.target.value)
+    }
+
+    function handleError() {
+        setUrlFallback('/icon-image-not-found-free-vector.jpg')
+    }
+
     return (
-        <Box
-            sx={{
-                width: '100%',
-                height: '200px',
-                minHeight: '200px',
-                position: 'relative'
-            }}
-        >
-            <Image
-                src={pizza.image}
-                alt={pizza.name}
-                fill={true}
-                sizes='auto'
-                style={{
-                    objectFit: 'contain'
+        <>
+            <Box
+                sx={{
+                    width: '50%',
+                    height: '200px',
+                    minHeight: '200px',
+                    position: 'relative',
+                    display: 'flex',
+                    justifyContent: 'center'
                 }}
-            />    
-        </Box>
+            >
+                <Image
+                    src={ urlFallback || (urlValid(url) ? url : pizza.image) }
+                    alt={ pizza.name }
+                    fill={true}
+                    sizes='auto'
+                    onError={handleError}
+                    style={{
+                        objectFit: 'contain'
+                    }}
+                />
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        bottom: '0px',
+                        right: '0px'
+                    }}
+                >
+                    <IconButton
+                        onClick={handleClick}
+                    >
+                        <EditIcon />
+                    </IconButton>
+                </Box>
+            </Box>
+            {
+                openInput ? (
+                    <TextField
+                        value={url}
+                        onChange={handleChange}
+                        InputProps={{
+                            endAdornment: (
+                                <IconButton
+                                    onClick={() => {}}
+                                    disabled={urlFallback !== ''}
+                                >
+                                    <CheckIcon />
+                                </IconButton>
+                            )
+                        }}
+                    />
+                ) : null
+            }
+        </>
     )
 }
 
