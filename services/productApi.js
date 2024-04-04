039a89a4fcab8ac8e96752651515ea74050a928b
Troy Pernía <troy00pernia@gmail.com>
2024-04-04 09:03:56 -0400

    change services/productApi: add function removePizza (line 121-135) and add property "credentials: include" to updatePizza (line 107)

--- services/productApi.js
+++ services/productApi.js
@@ -104,8 +104,25 @@ export async function getPizzasWithCosts() {
 export async function updatePizza(id, body) {
   return fetch(`${PATH_BACK}/pizzas/${id}`, {
     method: 'PUT',
+    credentials: 'include',
     headers: { 'Content-type': 'application/json' },
     body: JSON.stringify(body)
+  })
+    .then(response => {
+      return response.json()
+    })
+    .then(response => {
+      if (response.message) throw new Error(response.message)
+      return response
+    })
+    .catch(error => ({message: error.message}))
+}
+
+export async function removePizza(id) {
+  return fetch(`${PATH_BACK}/pizzas/${id}`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: { 'Content-type': 'application/json' }
   })
     .then(response => {
       return response.json()
