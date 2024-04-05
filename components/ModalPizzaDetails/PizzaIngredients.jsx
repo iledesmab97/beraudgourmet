f98fc86f995739f876309b0f45a9362daca3c5b3
Troy Pernía <troy00pernia@gmail.com>
2024-04-05 08:57:19 -0400

    change components/ModalPizzaDetails/PizzaIngredients: change states and functions handles (line 22-39) and render (line 52-104)

--- components/ModalPizzaDetails/PizzaIngredients.jsx
+++ components/ModalPizzaDetails/PizzaIngredients.jsx
@@ -4,10 +4,40 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import Input from '@mui/material/Input'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import EditIcon from '@mui/icons-material/Edit'
+import CheckIcon from '@mui/icons-material/Check'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
-import InputUpdate from '@/components/InputUpdate/InputUpdate'
+import { useState, useEffect } from 'react'
+
+import { getAllIngredients } from '@/services/productApi'
 
 function PizzaIngredients({ ingredients }) {
+
+    const [currentIngredientList, setCurrentIngredientList] = useState(ingredients)
+    const [edit, setEdit] = useState(false)
+    const [allIngredients, setAllIngredients] = useState([])
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        const newCurrentIngredientList = [...currentIngredientList]
+        newCurrentIngredientList[name] = value
+        setCurrentIngredientList(newCurrentIngredientList)
+    }
+
+    function handleEdit() {
+        setEdit(prevState => !prevState)
+    }
+
+    useEffect(() => {
+        getAllIngredients().then(totalListIngredients => setAllIngredients(totalListIngredients.map(ingredient => ingredient.name)))
+    }, [])
+
     return (
         <Box
             sx={{
@@ -19,19 +49,59 @@ function PizzaIngredients({ ingredients }) {
             <Typography variant='title' sx={{ alignSelf: 'center' }}>
                 Ingredientes
             </Typography>
-            <List>
-                {
-                    ingredients.map(ingredient => (
-                        <ListItem key={ingredient}>
-                            <ListItemText
-                                primary={
-                                    <InputUpdate value={ingredient} />
-                                }
-                            />
-                        </ListItem>
-                    ))
-                }       
-            </List>
+            {
+                allIngredients.length ? (
+                    <List
+                        sx={{
+                            width: '50%',
+                            position: 'relative'
+                        }}
+                    >
+                        {
+                            currentIngredientList.map((ingredient, index) => (
+                                <ListItem key={`currentIngredientList:${ingredient}`}>
+                                    <ListItemText
+                                        primary={
+                                            <>
+                                                <InputLabel>{`Ingrediente ${index+1}`}</InputLabel>
+                                                <Select
+                                                    disabled={!edit}
+                                                    name={String(index)}
+                                                    label={`Ingrediente ${index+1}`}
+                                                    value={ingredient}
+                                                    onChange={handleChange}
+                                                >
+                                                    {
+                                                        allIngredients.map(ingredient => (
+                                                            <MenuItem key={`allIngredients:${ingredient}`} value={ingredient} >{ingredient}</MenuItem>
+                                                        ))
+                                                    }
+                                                </Select>
+                                            </>
+                                        }
+                                    />
+                                </ListItem>
+                            ))
+                        }
+                        <IconButton
+                            sx={{
+                                position: 'absolute',
+                                bottom: '0px',
+                                right: '0px'
+                            }}
+                            onClick={handleEdit}
+                        >
+                            {
+                                edit ? (
+                                    <CheckIcon />
+                                ) : (
+                                    <EditIcon />
+                                )
+                            }
+                        </IconButton>
+                    </List>
+                ) : null
+            }
         </Box>
     )
 }
