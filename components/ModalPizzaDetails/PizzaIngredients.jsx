b7439b8db9f3b244ed5b230d850d7ebff4856c16
Troy Pernía <troy00pernia@gmail.com>
2024-04-05 10:44:47 -0400

    change components/ModalPizzaDetails/PizzaIngredients: change function saveIngredients (line 50-75)

--- components/ModalPizzaDetails/PizzaIngredients.jsx
+++ components/ModalPizzaDetails/PizzaIngredients.jsx
@@ -14,15 +14,19 @@ import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 
 import { useState, useEffect } from 'react'
+import useGetAlertMessage from '@/hooks/useGetAlertMessage'
+import useGetProducts from '@/hooks/useGetProducts'
 
 import { getAllIngredients, updatePizza } from '@/services/productApi'
 import { isSameArray } from '@/utils/preparingData'
 
-function PizzaIngredients({ ingredients }) {
+function PizzaIngredients({ ingredients, id }) {
 
     const [currentIngredientList, setCurrentIngredientList] = useState(ingredients)
     const [edit, setEdit] = useState(false)
     const [allIngredients, setAllIngredients] = useState([])
+    const { handleUpdateAlertMessage } = useGetAlertMessage()
+    const { handleUpdateProduct } = useGetProducts({type:'pizzas'})
     
     useEffect(() => {
         getAllIngredients()
@@ -37,15 +41,38 @@ function PizzaIngredients({ ingredients }) {
     }
 
     function handleEdit() {
-        // if (edit && isSameArray(currentIngredientList, ingredients)) {
         if (edit && !isSameArray(currentIngredientList, ingredients)) {
             saveIngredients()
         }
         setEdit(prevState => !prevState)
     }
 
-    function saveIngredients() {
-        console.log('voy a modifivar los ingredientes de la base de datos')
+    async function saveIngredients() {
+        const response = updatePizza( id, {
+            property: 'ingredients',
+            value: currentIngredientList
+        })
+        let text, status
+        if (response.message) {
+            text = response.message
+            status = 'error'
+        } else {
+            text = response
+            status = 'success'
+        }
+        handleUpdateAlertMessage({
+            checked: true,
+            text,
+            status
+        })
+        if (!response.message) {
+            handleUpdateProduct({
+                type: 'pizzas',
+                id: id,
+                property: 'ingredients',
+                value: currentIngredientList
+            })
+        }
     }
 
     return (
