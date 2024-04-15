369c13acb80b4e7b504de321e46d2a149bafbfef
Troy Pernía <troy00pernia@gmail.com>
2024-04-15 10:50:52 -0400

    add hooks/useLocalData

---
+++ hooks/useLocalData.js
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useRef } from 'react'
+import useGetPlace from '@/hooks/useGetPlace'
+
+import { deepEqual } from '@/utils/preparingData'
+
+function useLocalData() {
+    
+    const { place, handleAddPlace } = useGetPlace()
+    const firstTime = useRef(true)
+
+    useEffect(() => {
+        const placeLocalString = getLocalData('place')
+        const placeLocal = JSON.parse(placeLocalString)
+        if (firstTime.current) {
+            firstTime.current = false
+            if (!Object.keys(place).length) {
+                handleAddPlace(placeLocal)
+            }
+        } else {
+            if (!deepEqual(placeLocal, place)) {
+                saveLocalData('place', place)
+            }
+        }
+        
+    }, [place])
+
+    const getLocalData = useCallback((key) => {
+        const dataFromLocal = localStorage.getItem(key)
+        return dataFromLocal
+    }, [])
+
+    const saveLocalData = useCallback((key, value) => {
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [])
+
+    return { getLocalData, saveLocalData }
+}
+
+export default useLocalData
