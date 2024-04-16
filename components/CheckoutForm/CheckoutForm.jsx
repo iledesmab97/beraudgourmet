bf92cc2ecf868d3b4c0811bca898bc55bbb4ed13
Troy Pernía <troy00pernia@gmail.com>
2024-04-16 11:48:51 -0400

    change components/CheckoutForm: import hook useLocalData (line 12) and use it to extract function removeAllLocalData (line 35). change function handleSubmit (line 173) and bayByTransferens (line 199, 202)

--- components/CheckoutForm/CheckoutForm.jsx
+++ components/CheckoutForm/CheckoutForm.jsx
@@ -9,6 +9,7 @@ import { contactUs } from '@/utils/contact'
 import { descriptionOrder } from '@/utils/preparingData'
 import { updatePaymentRequest } from '@/services/checkoutApi'
 import { registerOrder } from '@/services/orderApi'
+import useLocalData from '@/hooks/useLocalData'
 
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
@@ -31,6 +32,7 @@ export default function CheckoutForm({user, place, orders, checkout, payment_met
     const elements = useElements()
     const router = useRouter()
     const firstTime = useRef(true)
+    const { removeAllLocalData } = useLocalData()
 
     const textOrderToWhatsapp = orders.map(order => descriptionOrder(order)).join("; ")
 
@@ -168,6 +170,8 @@ export default function CheckoutForm({user, place, orders, checkout, payment_met
                 paid: !checked,
             })
 
+            removeAllLocalData()
+
             return router.push('/success')
         }
 
@@ -192,8 +196,10 @@ export default function CheckoutForm({user, place, orders, checkout, payment_met
             paymentMethod: 'transfer',
             paid: false,
         })
+        removeAllLocalData()
         contactUs({context: 'transfer', name: user.name, order: textOrderToWhatsapp })
         handleCloseModal('pay')
+        return router.push('/success')
     }
 
     return (
