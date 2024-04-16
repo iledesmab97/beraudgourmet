33c5e57111168c83d53f6c2c2012c6141200dcd8
Troy Pernía <troy00pernia@gmail.com>
2024-04-16 09:51:31 -0400

    change components/ModalPizzaDetails/PizzaIngredients: add state allExtraIngredients (line 15), add function updateExtraIngredient (line 29-44) and function addExtraIngredient (line 46-51). Change function handleIngredients (line 53-67). Add ExtraIngredientsManager component (line 118-121)

--- components/ModalPizzaDetails/PizzaIngredients.jsx
+++ components/ModalPizzaDetails/PizzaIngredients.jsx
@@ -1,23 +1,69 @@
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 
 import ListPizzaIngredients from './ListPizzaIngredients'
 import IngredientsManager from './IngredientsManager'
+import ExtraIngredientsManager from './ExtraIngredientsManager'
 
 import { useState, useEffect } from 'react'
-import { getAllIngredients } from '@/services/productApi'
+import { getAllIngredients, getAllExtraIngredients } from '@/services/productApi'
 
 function PizzaIngredients({ ingredients, id, handleChangeInput, pizzaNew, property, errors, ...props }) {
 
     const [allIngredients, setAllIngredients] = useState([])
+    const [allExtraIngredients, setAllExtraIngredients] = useState([])
 
     useEffect(() => {
         getAllIngredients()
             .then(totalListIngredients => setAllIngredients(totalListIngredients.map(ingredient => ingredient.name)))
+        getAllExtraIngredients()
+            // .then(totalListExtraIngredients => setAllExtraIngredients(totalListExtraIngredients.map(extra => extra.name)))
+            .then(totalListExtraIngredients => setAllExtraIngredients(totalListExtraIngredients))
     }, [])
 
     function handleIngredients(value) {
         setAllIngredients(value)
+    }
+
+    function updateExtraIngredient(data) {
+        const {id, property, value} = data
+        let index
+        const newListExtraIngredinets = [...allExtraIngredients]
+        let [newExtraIngredient] = newListExtraIngredinets.filter((extra, i) => {
+            if (extra.id !== id) return false
+            index = i
+            return true
+        })
+        newExtraIngredient = {
+            ...newExtraIngredient,
+            [property]: value
+        }
+        newListExtraIngredinets[index] = newExtraIngredient
+        setAllExtraIngredients(newListExtraIngredinets)
+    }
+
+    function addExtraIngredient(data) {
+        const { id, name, cost } = data
+        const newListExtraIngredinets = [...allExtraIngredients]
+        newListExtraIngredinets.push({id, name, cost})
+        setAllExtraIngredients(newListExtraIngredinets)
+    }
+
+    function handleExtraIngredients(data, operation) {
+        switch (operation) {
+            case 'update': {
+                updateExtraIngredient(data)
+                break
+            }
+            case 'add': {
+                addExtraIngredient(data)
+                break
+            }
+            default: {
+                break
+            }
+        }
     }
 
     return (
@@ -31,12 +77,15 @@ function PizzaIngredients({ ingredients, id, handleChangeInput, pizzaNew, proper
             <Typography variant='title' sx={{ alignSelf: 'center' }}>
                 Ingredientes
             </Typography>
-            <Box
+            <Grid
+                container
+                alignItems={'flex-start'}
+                justifyContent={'space-between'}
                 sx={{
-                    pr: '10%',
-                    display: 'flex',
-                    alignItems: 'flex-start',
-                    justifyContent: 'space-between'
+                    // pr: '10%',
+                    // display: 'flex',
+                    // alignItems: 'flex-start',
+                    // justifyContent: 'space-between'
                 }}
             >
                 <ListPizzaIngredients
@@ -49,11 +98,29 @@ function PizzaIngredients({ ingredients, id, handleChangeInput, pizzaNew, proper
                     errorsIngredients={errors}
                     {...props}
                 />
-                <IngredientsManager
-                    allIngredients={allIngredients}
-                    handleIngredients={handleIngredients}
-                />
-            </Box>
+                <Grid
+                    container
+                    item
+                    xs={8}
+                    direction={'column'}
+                    alignItems={'center'}
+                    justifyContent={'flex-start'}
+                    sx={{
+                        display:'flex',
+                        height: '100%',
+                        gap: '32px'
+                    }}
+                >
+                    <IngredientsManager
+                        allIngredients={allIngredients}
+                        handleIngredients={handleIngredients}
+                    />
+                    <ExtraIngredientsManager
+                        allExtraIngredients={allExtraIngredients}
+                        handleExtraIngredients={handleExtraIngredients}
+                    />
+                </Grid>
+            </Grid>
         </Box>
     )
 }
