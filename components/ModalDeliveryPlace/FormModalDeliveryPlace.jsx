aff21b682a8e06df0b2eae63a4a7fdaafc696af1
Troy Pernía <troy00pernia@gmail.com>
2024-04-18 14:06:36 -0400

    change components/ModalDeliveryPlace/FormModalDleiveryPlace: change structure of componentn (line 28-277)

--- components/ModalDeliveryPlace/FormModalDeliveryPlace.jsx
+++ components/ModalDeliveryPlace/FormModalDeliveryPlace.jsx
@@ -24,65 +24,92 @@ export default function FormModalDeliveryPlace ({ inputsHome, handleInputsHome, 
         <Grid
             container
             spacing={2}
-            direction='column'
         >
-            <Grid container item alignItems='center'>
-                <Grid item md={3}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant='p'
-                        component="h2"
-                        align='right'
-                        md={3}
-                        sx={{
-                            pr: 3
-                        }}
+            <Grid
+                item
+                sm={3}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end'
+                }}
+            >
+                <Typography
+                    id="modal-modal-title"
+                    variant='p'
+                    component="h2"
+                    align='right'
+                    md={3}
+                    sx={{
+                        pr: 3
+                    }}
+                >
+                    Tipo:
+                </Typography>
+            </Grid>
+            <Grid item sm={9}>
+                <FormControl fullWidth>
+                    <InputLabel id='type-place-label'>Tipo de residencia</InputLabel>
+                    <Select
+                        labelId='type-place'
+                        id='type-place-select'
+                        value={typeLocation.name}
+                        label='Tipo de residencia'
+                        onChange={handleTypeLocation}
                     >
-                        Tipo
-                    </Typography>
-                </Grid>
-                <Grid item md>
-                    <FormControl fullWidth>
-                        <InputLabel id='type-place-label'>Tipo de residencia</InputLabel>
-                        <Select
-                            labelId='type-place'
-                            id='type-place-select'
-                            value={typeLocation.name}
-                            label='Tipo de residencia'
-                            onChange={handleTypeLocation}
-                        >
-                            <MenuItem value='home'>Casa: Dirección residencial</MenuItem>
-                            <MenuItem value='work'>Trabajo: Dirección comercial</MenuItem>
-                            <MenuItem value='building'>Casa: Departament</MenuItem>
-                            <MenuItem value='other'>Hotel</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
+                        <MenuItem value='home'>Casa: Dirección residencial</MenuItem>
+                        <MenuItem value='work'>Trabajo: Dirección comercial</MenuItem>
+                        <MenuItem value='building'>Casa: Departament</MenuItem>
+                        <MenuItem value='other'>Hotel</MenuItem>
+                    </Select>
+                </FormControl>
             </Grid>
 
             {
                 inputsHome.other && Object.keys(inputsHome.other).length
                     ? (
-                        <Grid container item alignItems='center'>
-                            <Grid item md={3}>    
-                                <Typography
-                                    id="modal-modal-title"
-                                    variant='p'
-                                    component="h2"
-                                    align='right'
-                                    sx={{
-                                        pr: 3
-                                    }}
-                                >
-                                    {places[inputsHome.type.name].other.name}
-                                </Typography>
-                            </Grid>
+                        <>
+                        <Grid
+                            item
+                            sm={3}
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'flex-end'
+                            }}
+                        >    
+                            <Typography
+                                id="modal-modal-title"
+                                variant='p'
+                                component="h2"
+                                align='right'
+                                sx={{
+                                    pr: 3
+                                }}
+                            >
+                                {places[inputsHome.type.name].other.name}:
+                            </Typography>
+                        </Grid>
 
-                            {
-                                Object.keys(inputsHome.other).length && (
-                                    <Grid item md>
-                                        {
-                                            Object.keys(inputsHome.other).map((input, index) => (
+                        {
+                            Object.keys(inputsHome.other).length && (
+                                <Grid
+                                    item
+                                    container
+                                    sm={9}
+                                    spacing={1}
+                                >
+                                    {
+                                        Object.keys(inputsHome.other).map((input, index) => (
+                                            <Grid
+                                                item
+                                                sx={{
+                                                    width: {
+                                                        xs: '100%',
+                                                        sm: `${places[inputsHome.type.name].other.inputs[index].width}%`
+                                                    }
+                                                }}
+                                            >
                                                 <TextField
                                                     id='input-unidad'
                                                     variant='outlined'
@@ -91,38 +118,53 @@ export default function FormModalDeliveryPlace ({ inputsHome, handleInputsHome, 
                                                     name={input}
                                                     value={inputsHome.other[input]}
                                                     onChange={handleInputsHome}
-                                                    sx={{
-                                                        width: `${places[inputsHome.type.name].other.inputs[index].width}%`
-                                                    }}
+                                                    fullWidth
                                                 />        
-                                            ))
-                                        }
-                                    </Grid>
-                                )
-                            }
-                        </Grid>
+                                            </Grid>
+                                        ))
+                                    }
+                                </Grid>
+                            )
+                        }
+                        </>
                     )
                     : null
             }
 
-            <Grid container item alignItems='center'>
-                <Grid item md={3}>    
-                    <Typography
-                        id="modal-modal-title"
-                        variant='p'
-                        component="h2"
-                        align='right'
-                        sx={{
-                            pr: 3
-                        }}
-                    >
-                        Calle
-                    </Typography>
-                </Grid>
+            <Grid
+                item
+                sm={3}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end'
+                }}
+            >    
+                <Typography
+                    id="modal-modal-title"
+                    variant='p'
+                    component="h2"
+                    align='right'
+                    sx={{
+                        pr: 3
+                    }}
+                >
+                    Calle:
+                </Typography>
+            </Grid>
 
-                <Grid item md>
-                    {
-                        Object.keys(inputsHome.street).map((input, index) => (
+            <Grid container item sm={9} spacing={1}>
+                {
+                    Object.keys(inputsHome.street).map((input, index) => (
+                        <Grid
+                            item
+                            sx={{
+                                width: {
+                                    xs: '100%',
+                                    sm: `${places[inputsHome.type.name].street[index].width}%`
+                                }
+                            }}
+                        >
                             <TextField
                                 id='input-unidad'
                                 variant='outlined'
@@ -130,42 +172,65 @@ export default function FormModalDeliveryPlace ({ inputsHome, handleInputsHome, 
                                 name={input}
                                 onChange={handleInputsHome}
                                 value={inputsHome.street[input]}
-                                sx={{
-                                    width: `${places[inputsHome.type.name].street[index].width}%`
-                                }}
+                                fullWidth
                                 key={input + index}
                             />
-                        ))
-                    }
-                </Grid>
+                        </Grid>
+                    ))
+                }
             </Grid>
 
-            <Grid container item alignItems='center'>
-                <Grid item md={3}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant='p'
-                        component="h2"
-                        align='right'
-                        sx={{
-                            pr: 3
-                        }}
-                    >
-                        Pueblo/Ciudad
-                    </Typography>
-                </Grid>
+            <Grid
+                item
+                sm={3}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end'
+                }}
+            >
+                <Typography
+                    id="modal-modal-title"
+                    variant='p'
+                    component="h2"
+                    align='right'
+                    sx={{
+                        pr: 3
+                    }}
+                >
+                    Pueblo/Ciudad:
+                </Typography>
+            </Grid>
 
-                <Grid item md>
+            <Grid container item sm={9} spacing={1}>
+                <Grid
+                    item
+                    sx={{
+                        width: {
+                            xs: '100%',
+                            sm: '70%'
+                        }
+                    }}
+                >
                     <TextField
                         id='input-unidad'
                         variant='outlined'
+                        placeholder={typeLocation.city.city}
                         name='city'
                         onChange={handleInputsHome}
                         value={inputsHome.city}
-                        sx={{
-                            width: '70%'
-                        }}
+                        fullWidth
                     />
+                </Grid>
+                <Grid
+                    item
+                    sx={{
+                        width: {
+                            xs: '100%',
+                            sm: '30%'
+                        }
+                    }}
+                >
                     <TextField
                         id='input-unidad'
                         variant='outlined'
@@ -173,40 +238,44 @@ export default function FormModalDeliveryPlace ({ inputsHome, handleInputsHome, 
                         name='postalCode'
                         onChange={handleInputsHome}
                         value={inputsHome.postalCode}
-                        sx={{
-                            width: '30%'
-                        }}
+                        fullWidth
                     />
                 </Grid>
             </Grid>
 
-            <Grid container item alignItems='center'>
-                <Grid item md={3}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant='p'
-                        component="h2"
-                        align='right'
-                        sx={{
-                            pr: 3
-                        }}
-                    >
-                        Nota
-                    </Typography>
-                </Grid>
-
-                <Grid item md>
-                    <TextArea
-                        minRows={3}
-                        maxRows={3}
-                        // placeholder='Minimun 3 rows'
-                        aria-label='minimum height'
-                        onChange={handleInputsHome}
-                        value={inputsHome.note}
-                        name='note'
-                    />
-                </Grid>
+            <Grid
+                item
+                sm={3}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end'
+                }}
+            >
+                <Typography
+                    id="modal-modal-title"
+                    variant='p'
+                    component="h2"
+                    align='right'
+                    sx={{
+                        pr: 3
+                    }}
+                >
+                    Nota:
+                </Typography>
             </Grid>
+
+            <Grid item sm={9} xs={12}>
+                <TextArea
+                    minRows={3}
+                    maxRows={3}
+                    aria-label='minimum height'
+                    onChange={handleInputsHome}
+                    value={inputsHome.note}
+                    name='note'
+                />
+            </Grid>
+
             <Button
                 variant='contained'
                 onClick={() => {
