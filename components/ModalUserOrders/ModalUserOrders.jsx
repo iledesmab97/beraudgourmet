cd3c18f4a442d0cadeb667c67fb21bca788fa5c0
Troy Pernía <troy00pernia@gmail.com>
2024-04-19 12:45:05 -0400

    change components/ModalUserOrders: change render component to OrdersTablet and OrdersList components (line 89). add const theme and isLargeScreen (line 59-60)

--- components/ModalUserOrders/ModalUserOrders.jsx
+++ components/ModalUserOrders/ModalUserOrders.jsx
@@ -1,19 +1,18 @@
 'use client'
 
+import OrdersTablet from './OrdersTablet'
+import OrdersList from './OrdersList'
+
 import Modal from '@mui/material/Modal'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 
 import { useState, useEffect } from 'react'
 import useGetModal from '@/hooks/useGetModal'
 import useGetUser from '@/hooks/useGetUser'
+import { useTheme } from '@mui/material/styles'
+import { useMediaQuery } from '@mui/material'
 
 import { getAllOrders } from '@/services/orderApi'
 import { extractIngredientsOut } from '@/utils/preparingData'
@@ -51,6 +50,8 @@ function ModalUserOrders() {
     const { open, handleChangeModal } = useGetModal({ modalType: 'userOrders' })
     const { user } = useGetUser()
     const [ orders, setOrders ] = useState([])
+    const theme = useTheme()
+    const isLargeScreen = useMediaQuery(theme.breakpoints.up('sm'))
 
     useEffect(() => {
         if (!user.id) return
@@ -78,61 +79,9 @@ function ModalUserOrders() {
                 >
                     Historial de Ordenes
                 </Typography>
-                <Grid
-                    item
-                    sx={{
-                        flexGrow: 1,
-                        overflowY: 'auto'
-                    }}
-                >
-                    <TableContainer>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell >Pedido</TableCell>
-                                    <TableCell >Emición</TableCell>
-                                    <TableCell >Entrega</TableCell>
-                                    <TableCell
-                                        align='right'
-                                    >
-                                        Precio($)
-                                    </TableCell>
-                                    <TableCell
-                                    >
-                                        Estatus
-                                    </TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {
-                                    orders.map( order => (
-                                        <TableRow key={order.id}>
-                                            <TableCell
-                                                dangerouslySetInnerHTML={{
-                                                    __html: order.itemsxOrder.map(item => {
-                                                        const ingredinetsOut = extractIngredientsOut(item.description)
-                                                        if (!ingredinetsOut.length) return item.description
-                                                        const index = item.description.indexOf(', ~')
-                                                        return (
-                                                            item.description.slice(0, index) + ingredinetsOut.map( ingredient => `, <span style="text-decoration: line-through">${ingredient}</span>` ).join('')
-                                                        )
-                                                    }).join('; ')
-                                                }}/>
-                                            <TableCell align='right'>{order.applicationDate}</TableCell>
-                                            <TableCell align='right'>{order.deliveryDate}</TableCell>
-                                            <TableCell align='right'>{order.totalCost}</TableCell>
-                                            <TableCell
-                                                sx={ order.closed ? {color: 'green'} : {color: 'red'} }
-                                            >
-                                                {order.closed ? 'Entregado' : 'Pendiente'}
-                                            </TableCell>
-                                        </TableRow>
-                                    ))
-                                }
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Grid>
+                {
+                    isLargeScreen ? <OrdersTablet orders={orders} /> : <OrdersList orders={orders} />
+                }
             </Box>
         </Modal>
     )
