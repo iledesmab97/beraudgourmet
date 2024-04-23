478ebe2e47a6681dc1eb8d6a7406790d47162f02
Troy Pernía <troy00pernia@gmail.com>
2024-04-23 07:59:25 -0400

    change utils/contact: change name of function from greet to makeOrder (line 6,11)

--- utils/contact.js
+++ utils/contact.js
@@ -3,12 +3,12 @@ const PHONE_NUMBER = process.env.NEXT_PUBLIC_PHONE_NUMBER
 // export const phoneNumber = '+525552954221'
 export const phoneNumber = PHONE_NUMBER
 export const text1 = 'Quieres comunicarte con nosotros para dejarnos alguna sugerencia'
-export function greet(name) {
+export function makeOrder(name) {
     return `Saludos, mi nombre es ${name} y me gustaría hacer un pedido de: `
 }
 
 const typeText = {
-    transfer: greet
+    transfer: makeOrder
 }
 
 export function contactUs({context, name, order}) {
