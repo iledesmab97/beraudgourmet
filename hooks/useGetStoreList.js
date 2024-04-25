3950901cd33de2609aef36cf84d958f3c10c3034
Troy Pernía <troy00pernia@gmail.com>
2024-04-25 08:12:51 -0400

    change hooks/useGetStoreList: add comments to sortStores function (line 5,7,9,11,13,17), add function listStores (line 27-41), add function handleUpdateStoreList (line 53-65)

--- hooks/useGetStoreList.js
+++ hooks/useGetStoreList.js
@@ -2,13 +2,19 @@ import { useAppSelector, useAppDispatch } from '@/hooks/store'
 import { addStoreList } from '@/stores/storeList/slice'
 
 function sortStores(storeListToAdd, storelist = {}) {
+    // Creo una copia del storeList actual
     const newStoreList = {...storelist}
+    // Itero sobre cada elemento de la lista de stores que deseo ordenar
     storeListToAdd.forEach(store => {
+        // Extraigo la propiedad city de cada store de la lista
         const { city } = store
+        // Evaluo si la ciudad ya está agregada al objeto newStoreList. 
         if (newStoreList[city]) {
+            // Si ya lo está, creo una copia del array contenido en la propidad stores
             const cityStoresList = newStoreList[city].stores
             newStoreList[city].stores = [...cityStoresList, store]
         } else {
+            //De lo contrario agrego la ciuedad como propiedad y como valor un objeto con las propiedades: name (nombre de la ciudad) y stores (array de stores en la ciudad) al que le agrego el store sobre el que estoy iterando.
             newStoreList[city] = {
                 name: city,
                 stores: [store]
@@ -16,6 +22,22 @@ function sortStores(storeListToAdd, storelist = {}) {
         }
     })
     return newStoreList
+}
+
+function listStores(storeList) {
+    const arrayStoreList = []
+    if (Object.keys(storeList).length) {
+        for (let city in storeList) {
+            storeList[city].stores.forEach(store => {
+                const newStore = {
+                    ...store,
+                    city
+                }
+                arrayStoreList.push(newStore)
+            })
+        }
+    }
+    return arrayStoreList
 }
 
 export default function useGetStoreList () {
@@ -27,6 +49,20 @@ export default function useGetStoreList () {
         const newStoresListSorted = sortStores(newStoresList, storeList)
         dispatch(addStoreList(newStoresListSorted))
     }
+
+    function handleUpdateStoreList({ id, property, value }) {
+        const arrayStoreList = listStores(storeList)
+        const newArrayStoreList = arrayStoreList.map(store => {
+            if (store.id !== id) return store
+            const newStore = {
+                ...store,
+                [property]: value
+            }
+            return newStore
+        })
+        const newStoresListSorted = sortStores(newArrayStoreList)
+        dispatch(addStoreList(newStoresListSorted))
+    }
     
-    return { storeList, handleAddStoreList }
+    return { storeList, handleAddStoreList, handleUpdateStoreList }
 }
