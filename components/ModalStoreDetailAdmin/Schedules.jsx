e5a286c1124e0ad6d65556a933a49215118930a0
Troy Pernía <troy00pernia@gmail.com>
2024-04-30 11:09:41 -0400

    change components/ModalStoreDetailAdmin/Schedules: add state scheduleList (line 33), change editing and selectedScheduleIndex (line 36,37). change useEffect (line 45-47), change handleChangeSchedule (line 60-62). add function addNewScheduleHours (line 145-160), change render of TableCell component (line 258-283). Add IconButton component (line 315-324)

--- components/ModalStoreDetailAdmin/Schedules.jsx
+++ components/ModalStoreDetailAdmin/Schedules.jsx
@@ -11,13 +11,17 @@ import TableRow from '@mui/material/TableRow'
 import TableCell from '@mui/material/TableCell'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
+import { TimePicker } from '@mui/x-date-pickers/TimePicker'
+
 
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import AddIcon from '@mui/icons-material/Add';
 
 import InputUpdate from '@/components/InputUpdate/InputUpdate'
 import TimePickerViewRenderers from '@/components/TimeChoose/TimeChoose'
+import InputTime from '@/components/InputTime/InputTime'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import useGetAlertMessage from '@/hooks/useGetAlertMessage'
 
 import { weekDaysES } from '@/services/storeApi'
@@ -26,10 +30,11 @@ import { updateSchedulesHoursOfSchedules } from '@/services/scheduleApi'
 function Schedules({ store, updateScheduleHoursStoreState }) {
 
     const [selectValue, setSelectValue] = useState('work')
+    const [scheduleList, setScheduleList] = useState(store[selectValue + 'Schedule'][selectValue + 'Schedule'])
     const [anchorElMenu, setAnchorElMenu] = useState(null)
     const openMenu = Boolean(anchorElMenu)
-    const [editing, setEditing] = useState(store[selectValue + 'Schedule'][selectValue + 'Schedule'].map(() => false))
-    const [selectedScheduleIndex, setSelectedScheduleIndex] = useState(null)
+    const [editing, setEditing] = useState(scheduleList.map(() => false))
+    const selectedScheduleIndex = useRef(null)
     const [inputEditing, setInputEditing] = useState(null)
     const { handleUpdateAlertMessage } = useGetAlertMessage()
 
@@ -37,9 +42,9 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
         const trueIndex = editing.indexOf(true)
         if (trueIndex === -1) return
         setInputEditing(() => {
-            const id = store[selectValue + 'Schedule'][selectValue + 'Schedule'][trueIndex].id
-            const [startDay, endDay] = store[selectValue + 'Schedule'][selectValue + 'Schedule'][trueIndex].days.split('-')
-            const [startTime, endTime] = store[selectValue + 'Schedule'][selectValue + 'Schedule'][trueIndex].hours.split(' - ')
+            const id = scheduleList[trueIndex].id
+            const [startDay, endDay] = scheduleList[trueIndex].days.split('-')
+            const [startTime, endTime] = scheduleList[trueIndex].hours.split(' - ')
             return {
                 id,
                 startDay,
@@ -52,7 +57,9 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
 
     function handleChangeShedule(value) {
         setSelectValue(value)
-        setEditing(store[value + 'Schedule'][value + 'Schedule'].map(() => false))
+        const newScheduleList = store[value + 'Schedule'][value + 'Schedule']
+        setScheduleList(newScheduleList)
+        setEditing(newScheduleList.map(() => false))
     }
 
     function handleCloseMenu() {
@@ -61,7 +68,7 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
 
     function handleMenuOpening(event, index) {
         setAnchorElMenu(event.currentTarget)
-        setSelectedScheduleIndex(index)
+        selectedScheduleIndex.current = index
     }
 
     function handleEdit(edit) {
@@ -69,12 +76,12 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
             setEditing(prevState => {
                 const newState = [...prevState]
                 return newState.map((edit, index) => {
-                    if (index === selectedScheduleIndex) return true
+                    if (index === selectedScheduleIndex.current) return true
                     return false
                 })
             })
         } else {
-            setSelectedScheduleIndex(null)
+            selectedScheduleIndex.current = null
             setEditing(prevState => {
                 return [...prevState].map(() => false)
             })
@@ -90,8 +97,7 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
     }
 
     async function updateSchedule() {
-        console.log('haciendo actualización...')
-        const newScheduleHours = [...store[selectValue + 'Schedule'][selectValue + 'Schedule']].map(scheduleHour => ({
+        const newScheduleHours = [...scheduleList].map(scheduleHour => ({
             id: scheduleHour.id,
             day: scheduleHour.days,
             startTime: scheduleHour.hours.split(' - ')[0],
@@ -136,6 +142,23 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
         return alert(response.message)
     }
 
+    function addNewScheduleHours() {
+        const newScheduleList = [...scheduleList]
+        let lastId = 0
+        newScheduleList.forEach(schedule => {
+            if (String(schedule.id).includes('new')) {
+                lastId = lastId > Number(String(schedule.id).split('new')[1]) ? lastId : Number(String(schedule.id).split('new')[1])
+            }
+        })
+        newScheduleList.push({
+            id: `new${lastId + 1}`,
+            days: 'Lunes-Domingo',
+            hours: '08:00 am - 08:00 pm'
+        })
+        setScheduleList(newScheduleList)
+        setEditing(newScheduleList.map(() => false))
+    }
+
     return (
         <Grid container spacing={3}>
             <Grid item>
@@ -165,6 +188,9 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
                 <Grid
                     item
                     xs={12}
+                    sx={{
+                        position: 'relative'
+                    }}
                 >
                     <TableContainer>
                         <Table>
@@ -174,12 +200,16 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
                                     <TableCell>Día Fin</TableCell>
                                     <TableCell>Hora Inicio</TableCell>
                                     <TableCell>Hora Fin</TableCell>
-                                    <TableCell>Acción</TableCell>
+                                    <TableCell
+                                        sx={{
+                                            width: '75px'
+                                        }}
+                                    >Acción</TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
                                 {
-                                    store[selectValue + 'Schedule'][selectValue + 'Schedule'].map((schedule, index) => (
+                                    scheduleList.map((schedule, index) => (
                                         <TableRow key={schedule.id}>
                                             {
                                                 editing[index] && inputEditing ? (
@@ -224,8 +254,34 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
                                                                 </Select>
                                                             </FormControl>
                                                         </TableCell>
-                                                        <TableCell>{schedule.hours.split(' - ')[0]}</TableCell>
-                                                        <TableCell>{schedule.hours.split(' - ')[1]}</TableCell>
+                                                        <TableCell>
+                                                            <InputTime
+                                                                time={inputEditing.startTime}
+                                                                label="Hora de apertura"
+                                                                onChangeTime={handleChangeDay}
+                                                                property={'startTime'}
+                                                                maxTime={inputEditing.endTime}
+                                                                sx={{
+                                                                    '&.MuiTextField-root': {
+                                                                        minWidth: '100px'
+                                                                    }
+                                                                }}
+                                                            />
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            <InputTime
+                                                                time={inputEditing.endTime}
+                                                                label="Hora de cierre"
+                                                                onChangeTime={handleChangeDay}
+                                                                property={'endTime'}
+                                                                minTime={inputEditing.startTime}
+                                                                sx={{
+                                                                    '&.MuiTextField-root': {
+                                                                        minWidth: '100px'
+                                                                    }
+                                                                }}
+                                                            />
+                                                        </TableCell>
                                                         <TableCell>
                                                             <IconButton
                                                                 onClick={(event) => handleMenuOpening(event, index)}
@@ -256,6 +312,16 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
                             </TableBody>
                         </Table>
                     </TableContainer>
+                    <IconButton
+                        onClick={addNewScheduleHours}
+                        sx={{
+                            position: 'absolute',
+                            top: '100%',
+                            right: '0px'
+                        }}
+                    >
+                        <AddIcon />
+                    </IconButton>
                 </Grid>
             </Grid>
             <Menu
@@ -264,7 +330,7 @@ function Schedules({ store, updateScheduleHoursStoreState }) {
                 onClose={handleCloseMenu}
             >
                 {
-                    editing[selectedScheduleIndex] ? (
+                    editing[selectedScheduleIndex.current] ? (
                         <MenuItem
                             onClick={updateSchedule}
                         >
