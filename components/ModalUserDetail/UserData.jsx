2186b51e779855071684bf851730d3a784de3bad
Troy Pernía <troy00pernia@gmail.com>
2024-05-03 13:33:57 -0400

    change components/components/ModalUserDetail/UserData: add props: errors, updateUserTable and handleChangeUser (line 26) , add states editingNumberPhone and loading (line 28-29), add function handleChangeNumberPhone (line 32-34), add function handleEditing (line 36-69), add InputUpdate component (line 103-108), add InputPhoneNumber component (line 119-143)

--- components/ModalUserDetail/UserData.jsx
+++ components/ModalUserDetail/UserData.jsx
@@ -2,8 +2,16 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+
+import CheckIcon from '@mui/icons-material/Check'
+import EditIcon from '@mui/icons-material/Edit'
 
 import InputUpdate from '@/components/InputUpdate/InputUpdate'
+import InputPhoneNumber from '@/components/InputPhoneNumber/InputPhoneNumber'
+
+import { useState } from 'react'
+import useGetAlertMessage from '@/hooks/useGetAlertMessage'
 
 import { updateAccount } from '@/services/userApi'
 
@@ -13,9 +21,52 @@ const RolsES = {
     admin: 'Administrador'
 }
 
-function UserData({ user, updateUser }) {
 
-    console.log('user:', user)
+
+function UserData({ user, errors, updateUserTable, handleChangeUser }) {
+
+    const [editingNumberPhone, setEditingNumberPhone] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const { handleUpdateAlertMessage } = useGetAlertMessage()
+
+    function handleChangeNumberPhone(newNumberPhone) {
+        handleChangeUser({property: 'phoneNumber', value: newNumberPhone})
+    }
+
+    async function handleEditing() {
+        if (loading) return
+        setLoading(true)
+
+        if (editingNumberPhone) {
+            if (Object.keys(errors).length) return
+            setEditingNumberPhone(prevState => !prevState)
+            const response = await updateAccount( user.id, {property: 'phoneNumber', value: user.phoneNumber})
+            let text, status
+            if (response.message) {
+                text = response.message
+                status = 'error'
+            } else {
+                text = response
+                status = 'success'
+            }
+            handleUpdateAlertMessage({
+                checked: true,
+                text,
+                status
+            })
+            if (!response.message) {
+                updateUserTable({
+                    id: user.id,
+                    property: 'phoneNumber',
+                    value: user.phoneNumber
+                })
+                console.log('Información guardada con exito')
+            }
+        } else {
+            setEditingNumberPhone(prevState => !prevState)
+        }
+        setLoading(false)
+    }
 
     return (
         <Grid
@@ -37,7 +88,7 @@ function UserData({ user, updateUser }) {
                         value={user.name}
                         updateProperty={updateAccount}
                         properties={{ property: 'name', id: user.id}}
-                        updateState={updateUser}
+                        updateState={updateUserTable}
                     />
                 </Grid>
             </Grid>
@@ -49,7 +100,12 @@ function UserData({ user, updateUser }) {
                     <Typography>Email:</Typography>
                 </Grid>
                 <Grid item xs={5}>
-                    <Typography align='right'>{user.email}</Typography>
+                    <InputUpdate
+                        value={user.email}
+                        updateProperty={updateAccount}
+                        properties={{ property: 'email', id: user.id}}
+                        updateState={updateUserTable}
+                    />
                 </Grid>
             </Grid>
             <Grid item xs={12}>
@@ -60,7 +116,31 @@ function UserData({ user, updateUser }) {
                     <Typography >Teléfono:</Typography>
                 </Grid>
                 <Grid item xs={5}>
-                    <Typography align='right'>{user.phoneNumber}</Typography>
+                    <InputPhoneNumber
+                        numberPhone={user.phoneNumber}
+                        errorsNumberPhone={errors.phoneNumber}
+                        userLoged={true}
+                        type={'text'}
+                        handleChangeNumberPhone={handleChangeNumberPhone}
+                        InputProps={{
+                            endAdornment: (
+                                <IconButton
+                                    name='numberPhone'
+                                    position='end'
+                                    onClick={handleEditing}
+                                    disabled={errors.numberPhone || loading ? true : false}    
+                                >
+                                    {
+                                        editingNumberPhone
+                                        ? <CheckIcon />
+                                        : <EditIcon />
+                                    }
+                                </IconButton>
+                            ),
+                            // readOnly: !editingNumberPhone ? true : false,
+                            disabled: !editingNumberPhone
+                        }}
+                    />
                 </Grid>
             </Grid>
             <Grid item xs={12}>
