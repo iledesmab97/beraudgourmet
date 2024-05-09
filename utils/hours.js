abffe85553465dc085d91e2c31d87d4d551b02c0
Troy Pernía <troy00pernia@gmail.com>
2024-05-09 16:19:59 -0400

    change utils/hours: add const typeDelivery (line 6-9), change function dateStringToDate (line 24-30), add functions: getTimeLimitTodaySchedue and dateInRange (line 67-90)

--- utils/hours.js
+++ utils/hours.js
@@ -3,6 +3,11 @@ import dayjs from 'dayjs'
 export const weekDaysEN = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 export const weekDaysES = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
 
+const typeDelivery = {
+    store: 'pickupSchedule',
+    home: 'deliverySchedule'
+}
+
 export function isOpen({openTime, closeTime}) {
     const now = dayjs()
     const openTimeDay = dayjs(`${now.format('D')} ${openTime}`, 'D hh:mm a')
@@ -16,8 +21,13 @@ export function dateStringToDate(date) {
     const [day, month, year] = dateWithSlash.split('/')
     const [hour_min, am_pm] = time.split(" ")
     let [hour, mine] = hour_min.split(':')
-    
-    const dateA = dayjs(`${year}-${month}-${day} ${hour}:${mine}`, ['YYYY', 'YYYY-MM-DD', 'YYYY-MM-DD HH:mm'])
+    if (hour === '12') {
+        hour = am_pm === 'am' ? '00' : hour
+    } else {
+        hour = am_pm === 'am' ? hour : String(Number(hour) + 12)
+    }
+    const totalDateString = `${year}-${month}-${day} ${hour}:${mine}:00`
+    const dateA = dayjs(totalDateString, ['YYYY', 'YYYY-MM-DD', 'YYYY-MM-DD HH:mm:ss'], true)
     return dateA
 }
 
@@ -52,4 +62,29 @@ export function todaysScheduleIs(scheduleList, orderDay) {
         const indexEnd = weekDaysES.indexOf(schedule.days.split('-')[1])
         return indexOrderDay >= indexStart && indexOrderDay <= indexEnd
     })
+}
+
+export function getTimeLimitTodaySchedue(place) {
+    if (!place.deadLine) return {
+        minHour: dayjs(),
+        maxHour: dayjs()
+    }
+    const scheduleList = place.closerStore[typeDelivery[place.typeDelivery.name]][typeDelivery[place.typeDelivery.name]]
+    const orderDay = place.deadLine.date.realDate
+    const scheduleOfDay = todaysScheduleIs(scheduleList, orderDay)
+    const minHour = scheduleOfDay.hours.split(' - ')[0]
+    const maxHour = scheduleOfDay.hours.split(' - ')[1]
+    return {
+        minHour: timeStringToObject(minHour),
+        maxHour: timeStringToObject(maxHour)
+    }
+}
+
+export function dateInRange({minHour, maxHour, currentDay}) {
+    const currentDateObject = typeof currentDay === 'string' ? dateStringToDate(currentDay) : currentDay
+    const minTimeObject = typeof minHour === 'string' ? timeStringToObject(minDate) : minHour
+    const minDateObject = minTimeObject.date(currentDateObject.format('D')).month(Number(currentDateObject.format('M')) - 1).year(currentDateObject.format('YYYY'))
+    const maxTimeObject = typeof maxHour === 'string' ? timeStringToObject(maxDate) : maxHour
+    const maxDateObject = maxTimeObject.date(currentDateObject.format('D')).month(Number(currentDateObject.format('M')) - 1).year(currentDateObject.format('YYYY'))
+    return currentDateObject.isAfter(minDateObject) && currentDateObject.isBefore(maxDateObject)
 }
