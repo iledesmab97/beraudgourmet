bd930329dc4232cff7709b7bb0ad50d0846fd540
Troy Pernía <troy00pernia@gmail.com>
2024-05-10 13:44:41 -0400

    change components/WhatsappButton: add condition to render the component

--- components/WhatsappButton/WhatsappButton.jsx
+++ components/WhatsappButton/WhatsappButton.jsx
@@ -9,52 +9,102 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 
 import whatsappImage from '@/public/images/homeimg/logos/whatsapp.svg'
 
+import { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation'
 import useGetOrders from '@/hooks/useGetOrders'
+import { useMediaQuery } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
 
 import { phoneNumber, text1 } from '@/utils/contact'
 
 function WhatsappButton() {
 
+    const theme = useTheme()
+    const matches = useMediaQuery(theme.breakpoints.down('md'))
     const pathname = usePathname()
     const { orders } = useGetOrders()
+    const [totalMatches, setTotalMatches] = useState('null')
 
-    if (pathname === '/admin' || (orders.length)) return null
+    useEffect(() => {
+        setTotalMatches(String(matches))
+      }, [matches])
 
     return (
-        <Fab
-            sx={{
-                position: 'fixed',
-                bottom: '20px',
-                right: '20px',
-                backgroundColor: '#25D366'
-            }}
-        >
-            <Link
-                href={`https://wa.me/${phoneNumber}/?text=${text1}`}
-                // href={`https://wa.me/?text=${text}`}
-                target='_blank'
-                sx={{
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}
-            >
-                <Image
-                    src={whatsappImage}
-                    alt='whatsappLogo'
-                    style={{
-                        width: '100%',
-                        height: 'auto',
-                        objectFit: 'contain',
-                        aspectRatio: 1
-                    }}   
-                />
-                {/* <WhatsAppIcon/> */}
-            </Link>
-        </Fab>
+        <>
+            {
+                totalMatches === 'true' ? (
+                    pathname !== '/admin' && (!orders.length) ? (
+                        <Fab
+                            sx={{
+                                position: 'fixed',
+                                bottom: '20px',
+                                right: '20px',
+                                backgroundColor: '#25D366'
+                            }}
+                        >
+                            <Link
+                                href={`https://wa.me/${phoneNumber}/?text=${text1}`}
+                                // href={`https://wa.me/?text=${text}`}
+                                target='_blank'
+                                sx={{
+                                    width: '100%',
+                                    height: '100%',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                <Image
+                                    src={whatsappImage}
+                                    alt='whatsappLogo'
+                                    style={{
+                                        width: '100%',
+                                        height: 'auto',
+                                        objectFit: 'contain',
+                                        aspectRatio: 1
+                                    }}   
+                                />
+                                {/* <WhatsAppIcon/> */}
+                            </Link>
+                        </Fab>
+                    ) : null
+                ) : (
+                    <Fab
+                        sx={{
+                            position: 'fixed',
+                            bottom: '20px',
+                            right: '20px',
+                            backgroundColor: '#25D366'
+                        }}
+                    >
+                        <Link
+                            href={`https://wa.me/${phoneNumber}/?text=${text1}`}
+                            // href={`https://wa.me/?text=${text}`}
+                            target='_blank'
+                            sx={{
+                                width: '100%',
+                                height: '100%',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <Image
+                                src={whatsappImage}
+                                alt='whatsappLogo'
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                    objectFit: 'contain',
+                                    aspectRatio: 1
+                                }}   
+                            />
+                            {/* <WhatsAppIcon/> */}
+                        </Link>
+                    </Fab>
+                )
+            }
+        </>
     )
 }
 
