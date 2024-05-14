9a66a097ea121713c7735d2892db2486c4868883
Troy Pernía <troy00pernia@gmail.com>
2024-05-14 11:18:28 -0400

    change components/ModalUserDetail/MakeOrder: add state extraIngredientsList (line 22-30), add useEffect to update extraIngredientsList state (Line 37-46), add handleChange ExtraIngredientsSelected function (line 58-61), add Grid component to show the extraIngredients (line 106-140)

--- components/ModalUserDetail/MakeOrder.jsx
+++ components/ModalUserDetail/MakeOrder.jsx
@@ -4,21 +4,46 @@ import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
 
 import useGetProducts from '@/hooks/useGetProducts'
+import useGetExtraIngredients from '@/hooks/useGetExtraIngredients'
 import { useState, useEffect } from 'react'
 
 function MakeOrder() {
 
     const { products } = useGetProducts({ type: 'pizzas'})
+    const { extraIngredients } = useGetExtraIngredients()
     const [ pizzas, setPizzas ] = useState( products ? products : [])
     const [pizzaSelected, setPizzaSelected] = useState(null)
     const [ingredinetsOut, setIngredientsOut] = useState([])
+    const [extraIngredientsList, setExtraIngredients] = useState(() => {
+        if (Object.keys(extraIngredients).length) return []
+        const newList = []
+        for (let extra in extraIngredients) {
+            newList.push(extraIngredients[extra])
+        }
+        return newList
+    })
+    const [extraIngredientsSelected, setExtraIngredientsSelected] = useState([])
 
     useEffect(() => {
         if (!products) return
         setPizzas(products)
     }, [products])
+
+    useEffect(() => {
+        setExtraIngredients(() => {
+            if (!Object.keys(extraIngredients).length) return []
+            const newList = []
+            for (let extra in extraIngredients) {
+                newList.push(extraIngredients[extra])
+            }
+            return newList
+        })
+    }, [extraIngredients])
 
     function handleChangeSelectPizza(event) {
         const pizza = pizzas.find(pizza => pizza.name === event.target.value)
@@ -28,6 +53,11 @@ function MakeOrder() {
     function handleChangePizzaIngredients(event) {
         const { value } = event.target
         setIngredientsOut(value)
+    }
+
+    function handleChangeExtraIngredientsSelected(event) {
+        const { value } = event.target
+        setExtraIngredientsSelected(value)
     }
 
     return (
@@ -73,8 +103,41 @@ function MakeOrder() {
                 ) : null
             }
             
-            <Grid item>
-                <Typography>Ingredientes extra</Typography>
+            <Grid container item xs={12} spacing={2}>
+                <Grid item xs={12}>
+                    <FormControl fullWidth>
+                        <InputLabel>Ingredientes extra</InputLabel>
+                        <Select
+                            multiple
+                            value={ extraIngredientsSelected }
+                            label='Ingredientes extra'
+                            onChange={handleChangeExtraIngredientsSelected}
+                        >
+                            {
+                                extraIngredientsList.map(extraIngredient => (
+                                    <MenuItem key={extraIngredient.name} value={extraIngredient.name} >{extraIngredient.name}</MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
+                </Grid>
+                {
+                    extraIngredientsSelected.length ? (
+                        <Grid item xs={5}>
+                            <List>
+                                {
+                                    extraIngredientsSelected.map(extraIngredient => (
+                                        <ListItem key={`extraIngredientSelected(${extraIngredient})`}>
+                                            <ListItemText
+                                                primary={extraIngredient}
+                                            />
+                                        </ListItem>
+                                    ))
+                                }
+                            </List>
+                        </Grid>
+                    ) : null
+                }
             </Grid>
             <Grid item>
                 <Typography>Cantidad</Typography>
