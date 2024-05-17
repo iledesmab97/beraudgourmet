e5f25091eb38eb66d949fe44f64276eb0a22359f
Troy Pernía <troy00pernia@gmail.com>
2024-05-17 12:14:27 -0400

    change components/WhatsappButton: change conditions to show the component

--- components/WhatsappButton/WhatsappButton.jsx
+++ components/WhatsappButton/WhatsappButton.jsx
@@ -5,8 +5,6 @@ import Box from '@mui/material/Box'
 import Fab from '@mui/material/Fab'
 import Link from '@mui/material/Link'
 
-import WhatsAppIcon from '@mui/icons-material/WhatsApp'
-
 import whatsappImage from '@/public/images/homeimg/logos/whatsapp.svg'
 
 import { useState, useEffect } from 'react'
@@ -27,13 +25,13 @@ function WhatsappButton() {
 
     useEffect(() => {
         setTotalMatches(String(matches))
-      }, [matches])
+    }, [matches])
 
     return (
         <>
             {
-                totalMatches === 'true' ? (
-                    pathname !== '/admin' && (!orders.length) ? (
+                pathname !== '/admin' ? (
+                    totalMatches === 'true' && (orders.length) ? null : (
                         <Fab
                             sx={{
                                 position: 'fixed',
@@ -64,45 +62,10 @@ function WhatsappButton() {
                                         aspectRatio: 1
                                     }}   
                                 />
-                                {/* <WhatsAppIcon/> */}
                             </Link>
                         </Fab>
-                    ) : null
-                ) : (
-                    <Fab
-                        sx={{
-                            position: 'fixed',
-                            bottom: '20px',
-                            right: '20px',
-                            backgroundColor: '#25D366'
-                        }}
-                    >
-                        <Link
-                            href={`https://wa.me/${phoneNumber}/?text=${text1}`}
-                            // href={`https://wa.me/?text=${text}`}
-                            target='_blank'
-                            sx={{
-                                width: '100%',
-                                height: '100%',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center'
-                            }}
-                        >
-                            <Image
-                                src={whatsappImage}
-                                alt='whatsappLogo'
-                                style={{
-                                    width: '100%',
-                                    height: 'auto',
-                                    objectFit: 'contain',
-                                    aspectRatio: 1
-                                }}   
-                            />
-                            {/* <WhatsAppIcon/> */}
-                        </Link>
-                    </Fab>
-                )
+                    )
+                ) : null
             }
         </>
     )
