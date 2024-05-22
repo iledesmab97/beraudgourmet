4d6044df70dedb04b1d204d461c84d840536b003
Troy Pernía <troy00pernia@gmail.com>
2024-05-22 11:20:47 -0400

    change components/AlertchangePassword: change function changePassword (line 36

--- components/AlertChangePassword/AlertChangePassword.jsx
+++ components/AlertChangePassword/AlertChangePassword.jsx
@@ -4,14 +4,10 @@ import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
-import TextField from '@mui/material/TextField'
-import InputAdornment from '@mui/material/InputAdornment'
-
-import MailOutlineIcon from '@mui/icons-material/MailOutline'
 
 import { useState, useEffect } from 'react'
 import useGetAlertDialogMessage from '@/hooks/useGetAlertDialogMessage'
-import useDebounce from '@/hooks/useDebounce'
+import { useRouter } from 'next/navigation';
 
 import { forgetPassword } from '@/services/userApi'
 
@@ -20,50 +16,24 @@ function AlertChangePassword({ params }) {
     const { alertDialogMessage, closeAlertDialogMessage } = useGetAlertDialogMessage({ type: 'changePassword' })
     const [open, setOpen] = useState(alertDialogMessage ? alertDialogMessage.open : false)
     const [loading, setLoading] = useState(false)
-
-    // useEffect(() => {
-    //     console.log('alertDialogMessage:', alertDialogMessage)
-    // }, [alertDialogMessage])
+    const router = useRouter()
 
     useEffect(() => {
         setOpen(alertDialogMessage.open)
     }, [alertDialogMessage.open])
 
-    // useEffect(() => {
-    //     debounceSetValue(() => {
-    //         const newError = validation(email)
-    //         setErrors(newError)
-    //         if (!newError) {
-    //             searchUser(email)
-    //                 .then(data => {
-    //                     if (!data) setErrors('Correo no registrado')
-    //             })
-    //         }
-    //     }, 500)
-    // }, [email])
-
-    // function handleChange(event) {
-    //     const newEmail = event.target.value
-    //     setEmail(newEmail)
-    // }
-
     async function changePassword() {
-        console.log('Validando datos...')
+        console.log('Cambiando contraseña...')
         setLoading(true)
-        // if (errors) {
-        //     console.log('Datos inválidos')
-        //     return setLoading(false)
-        // }
-        // console.log('Eviando correo...')
         const response = await forgetPassword({ token: params })
-        console.log('response:', response)
         if (response.message) {
             setLoading(false)
             return alert(response.message)
         }
-        console.log('Solicitud enviada exitosamente')
+        console.log('Contraseña cambiada exitosamente')
         setLoading(false)
         closeAlertDialogMessage()
+        router.push('/pizzas')
     }
 
     return (
