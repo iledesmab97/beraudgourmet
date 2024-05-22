5f1c5c32fc8558ed2da8e0d2b5072b2314f593e6
Troy Pernía <troy00pernia@gmail.com>
2024-05-22 11:21:23 -0400

    change app/user-changepassword/[token]/page: remove code comment

--- app/user-change-password/[token]/page.jsx
+++ app/user-change-password/[token]/page.jsx
@@ -4,27 +4,16 @@ import Button from "@mui/material/Button"
 
 import AlertChangePassword from '@/components/AlertChangePassword/AlertChangePassword'
 
-import { useState } from "react"
 import useGetAlertDialogMessage from '@/hooks/useGetAlertDialogMessage'
-
-
-// import { verifyEmailUser } from '@/services/userApi'
 
 function UserChangePasswordVerify({ params }) {
 
     const { openAlertDialogMessage } = useGetAlertDialogMessage({ type: 'changePassword' })
 
-    // async function verifyEmail() {
-    //     const response: any = await verifyEmailUser(params.token)
-    //     if (response.name) alert(response.name)
-    //     if (response) setVerified(true)
-    // }
-
     return (
         <>
             <Button
                 variant='contained'
-                // margin='dense'
                 onClick={openAlertDialogMessage}
             >
                 Solicitar cambio de contraseña
