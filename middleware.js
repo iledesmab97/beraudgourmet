108519369c55162582248ed1f8af6114b4c11107
Troy Pernía <troy00pernia@gmail.com>
2024-05-24 11:16:27 -0400

    change middleware: add const tokenUser (line 7) and pass to fetchwhoAmI (line 11)

--- middleware.js
+++ middleware.js
@@ -4,17 +4,12 @@ import { fetchwhoAmI } from '@/services/userApi'
 
 
 export async function middleware(request) {
-    // const tokenUser = request.cookies.get('tokenUser')
+    const tokenUser = request.cookies.get('tokenUser')
     const { pathname } = request.nextUrl
-
     if ( pathname.includes('/admin') ) {
-        const user = await fetchwhoAmI()
-        // console.log('respuesta del fetchwhoAmI desde el middleware:', user)
-        // if (!tokenUser) return NextResponse.error()
-        if (!user) return NextResponse.error()
         try {
-            // const { payload } = await jwtVerify(tokenUser.value, new TextEncoder().encode('secret'))
-            // if ( payload.RoleId > 2 ) return NextResponse.error()
+            const user = await fetchwhoAmI(tokenUser)
+            if (user.message) return NextResponse.error()
             if ( user.RoleId > 2 ) return NextResponse.error()
             return NextResponse.next()
         } catch(error) {
