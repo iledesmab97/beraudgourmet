17b96e1cf03a2db1608e57ac318ecd2ba99b4bee
Troy Pernía <troy00pernia@gmail.com>
2024-05-24 15:51:25 -0400

    change middleware: reset

--- middleware.js
+++ middleware.js
@@ -3,22 +3,44 @@ import { jwtVerify } from 'jose'
 import { fetchwhoAmI, whatHappen } from '@/services/userApi'
 import { notFound } from 'next/navigation'
 
+// export async function middleware(request) {
+//     // const tokenUser = request.cookies.get('tokenUser')
+//     const { pathname } = request.nextUrl
+//     if ( pathname.includes('/admin') ) {
+//         let user
+//         try {
+//             user = await fetchwhoAmI(tokenUser)
+//             // if (user.message) return NextResponse.error()
+//             // if ( user.RoleId > 2 ) return NextResponse.error()
+//         } catch(error) {
+//             return NextResponse.redirect(new URL('/pizzas', request.url))
+//         }
+//         if ( !user || user.message || user.RoleId > 2 ) {
+//             // return notFound()
+//             // return NextResponse.redirect(new URL('/not-found', request.url))
+//             await whatHappen({user, message: user.message, role: user.RoleId})
+//         }
+//     }
+
+//     return NextResponse.next()
+// }
+
 export async function middleware(request) {
-    const tokenUser = request.cookies.get('tokenUser')
+    // const tokenUser = request.cookies.get('tokenUser')
     const { pathname } = request.nextUrl
     if ( pathname.includes('/admin') ) {
-        let user
+        const user = await fetchwhoAmI()
+        // console.log('respuesta del fetchwhoAmI desde el middleware:', user)
+        // if (!tokenUser) return NextResponse.error()
+        if (!user) return NextResponse.error()
         try {
-            user = await fetchwhoAmI(tokenUser)
-            // if (user.message) return NextResponse.error()
-            // if ( user.RoleId > 2 ) return NextResponse.error()
+            // const { payload } = await jwtVerify(tokenUser.value, new TextEncoder().encode('secret'))
+            // if ( payload.RoleId > 2 ) return NextResponse.error()
+            if ( user.RoleId > 2 ) return NextResponse.error()
+                return NextResponse.next()
         } catch(error) {
+            console.log(error.message)
             return NextResponse.redirect(new URL('/pizzas', request.url))
-        }
-        if ( !user || user.message || user.RoleId > 2 ) {
-            // return notFound()
-            // return NextResponse.redirect(new URL('/not-found', request.url))
-            await whatHappen({user, message: user.message, role: user.RoleId})
         }
     }
 
