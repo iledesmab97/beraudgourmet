72ca9659c1907a93c5637859c426e30c91151193
Troy Pernía <troy00pernia@gmail.com>
2024-06-20 17:07:47 -0400

    change components/OrderRewards: add property position to Container component (line 37), add minWith property (line 42-46). add id to Box component (line 77), wrap Store and TotalPriceSection between Box component (line 94-98, 117). Wrap ButtonPay component between Box component (line 119,127) add SliceProgressBar component (line 116, 126)

--- components/OrderRewards/OrderRewards.jsx
+++ components/OrderRewards/OrderRewards.jsx
@@ -1,12 +1,5 @@
 'use client'
 
-import useGetModal from '@/hooks/useGetModal'
-import UserSection from './UserSection'
-import OrderSection from './OrderSection'
-import StoreSection from './StoreSection'
-import TotalPriceSection from './TotalPriceSection'
-import ButtonPay from '@/components/ButtonPay/ButtonPay'
-
 import Image from 'next/image'
 import Grid from '@mui/material/Grid'
 import Container from '@mui/material/Container'
@@ -15,6 +8,16 @@ import FormControl from '@mui/material/FormControl'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+
+import UserSection from './UserSection'
+import OrderSection from './OrderSection'
+import StoreSection from './StoreSection'
+import TotalPriceSection from './TotalPriceSection'
+import ButtonPay from '@/components/ButtonPay/ButtonPay'
+import SliceProgressBar from '@/components/SliceProgressBar/SliceProgressBar'
+
+import useGetModal from '@/hooks/useGetModal'
 
 import style from './OrderRewards.module.css'
 import logoBeraud from '@/public/images/homeimg/homeimgberaud/logoBeraud.png'
@@ -27,19 +30,27 @@ function OrderRewards () {
     <Grid
       item
       xs={12}
+      // xs={12}
       md={4}
     >
       <Container
         sx={{
+          position: 'relative',
           height: {
             xs: '100%',
             md: 'auto'
+          },
+          minWidth: {
+            xs: '300px',
+            sm: '315px',
+            md: '300px'
           },
           bgcolor: '#EAEDF2',
           pb: 2,
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
+          // alignItems: 'center',
+          alignItems: 'flex-end',
           justifyContent: 'flex-start'
         }}
       >
@@ -64,48 +75,58 @@ function OrderRewards () {
           />
         </Box>
         <Box
+          id='OrderRewards-form-container'
           component='form'
           noValidate
           autoComplete='off'
           sx={{
-            '& > :not(style)': { m: 1,
-            width: '25ch'
-          }
+            position: 'relative',
+            '& > :not(style)': {
+              m: 1,
+              width: '25ch'
+            }
           }}
         >
-          <UserSection />
 
-          <StoreSection />
+          <UserSection />
 
           <OrderSection />
 
-          <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-            <TextField
-                id="cupon"
-                label="Cupon"
-                type='text'
-                size='small'
-                margin='dense'
-                fullWidth
-                helperText=''
-                error={false}/>
+          <Box
+            sx={{
+              position: 'relative'
+            }}
+          >
+
+            <StoreSection />
+
+            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+              <TextField
+                  id="cupon"
+                  label="Cupon"
+                  type='text'
+                  size='small'
+                  margin='dense'
+                  fullWidth
+                  helperText=''
+                  error={false}/>
+            </Box>
+
+            <TotalPriceSection />
+
+            <SliceProgressBar section={'store'} />
           </Box>
 
-          <TotalPriceSection />
 
-          <Box>
-            {/* <Button
-              variant='contained'
-              color='secondary'
-              sx={{ my:1 }}
-              fullWidth
-              onClick={() => {handleOpenModal('pay')}}
-            >
-              Siguiente paso
-            </Button> */}
+          <Box
+            id='ButtonPay-container'
+            sx={{
+              position: 'relative'
+            }}
+          >
             <ButtonPay />
+            <SliceProgressBar section={'pay'} />
           </Box>
-          
         </Box>
       </Container>
     </Grid>
