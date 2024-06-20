7ff5dccbd9ecb6f1f3d3153fb37c862413a8b407
Troy Pernía <troy00pernia@gmail.com>
2024-06-20 16:56:37 -0400

    change stores/index: add stepsReducer (line 13,28)

--- stores/index.js
+++ stores/index.js
@@ -10,6 +10,7 @@ import extraIngredientsReducer from './extraIngredients/slice'
 import alertMessageReducer from './alertMessage/slice'
 import alertDialogMessageReducer from './alertDialogMessage/slice'
 import orderListReducer from './orderList/slice'
+import stepsReducer from './steps/slice'
 
 export const store = configureStore({
     reducer: {
@@ -23,6 +24,7 @@ export const store = configureStore({
         extraIngredients: extraIngredientsReducer,
         alertMessage: alertMessageReducer,
         alertDialogMessage: alertDialogMessageReducer,
-        orderList: orderListReducer
+        orderList: orderListReducer,
+        steps: stepsReducer
     }
 })
