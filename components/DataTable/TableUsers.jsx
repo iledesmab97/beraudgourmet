f5fda368c8755ac430cb3a4481de88d72a26ece6
Troy Pernía <troy00pernia@gmail.com>
2024-06-28 12:11:42 -0400

    change components/DataTable/TableUsers: add style to Paper component (line 132-135)

--- components/DataTable/TableUsers.jsx
+++ components/DataTable/TableUsers.jsx
@@ -129,6 +129,10 @@ function TableUsers() {
             sx={{
                 position: 'relative',
                 // overflowY: 'hidden',
+                mt: {
+                    xs: '70px',
+                    sm: '0px'
+                }
             }}
         >
             <TableContainer
