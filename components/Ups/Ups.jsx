3763aeaddf7e75b6e45d2d82e42da692f7d6b1da
Troy Pernía <troy00pernia@gmail.com>
2024-07-02 14:53:18 -0400

    add components/Ups and components/ErrorBoundary

---
+++ components/Ups/Ups.jsx
@@ -0,0 +1,10 @@
+function Ups({text}) {
+    return (
+        <>
+            <h1>Ups</h1>
+            <p>{text ? text : 'Algo salió mal pero no se que fué'}</p>        
+        </>
+    )
+}
+
+export default Ups
