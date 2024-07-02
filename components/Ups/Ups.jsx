27c00938c96cb038bff55a070cb0fe15c73b0096
Troy Pernía <troy00pernia@gmail.com>
2024-07-02 14:59:44 -0400

    change components/Ups: change styles of component (line 6-18)

--- components/Ups/Ups.jsx
+++ components/Ups/Ups.jsx
@@ -1,9 +1,21 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
 function Ups({text}) {
     return (
-        <>
-            <h1>Ups</h1>
-            <p>{text ? text : 'Algo salió mal pero no se que fué'}</p>        
-        </>
+        <Box
+            sx={{
+                width: '100%',
+                position: 'absolute',
+                top: '30%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}
+        >
+            <Typography component={'h1'} variant='encabezado' >Ups</Typography>
+            <Typography component={'p'} variant='p' >{text ? text : 'Algo salió mal pero no se que fué'}</Typography>        
+        </Box>
     )
 }
 
