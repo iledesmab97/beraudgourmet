71840e5075d964dce7cf16f321ab4fa234b14d49
Troy Pernía <troy00pernia@gmail.com>
2024-07-03 14:01:21 -0400

    add components/ModalPizzaExtraIngredientsDetails

---
+++ components/ModalPizzaExtraIngredientDetails/ModalPizzaExtraIngredientDetails.jsx
@@ -0,0 +1,113 @@
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+
+import { useState } from 'react'
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: {
+        xs: '324px',
+        sm: '500px',
+        md: '750px'
+    },
+    height: 'auto',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    borderRadius: 5,
+    p: {
+        xs: 2,
+        sm: 4,
+        md: 5
+    },
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 2,
+}
+
+function ModalPizzaExtraIngredientDetails({ openExtraIngredientDetails, handleOpenExtraIngredientDetails, extraIngredientSelected }) {
+
+    const [extraIngredient, setExtraIngredient] = useState(extraIngredientSelected)
+
+    return (
+        <Modal
+            open={openExtraIngredientDetails}
+            onClose={() => {handleOpenExtraIngredientDetails(false)}}
+        >
+            <Box
+                sx={style}
+            >
+                <Grid
+                    container
+                    spacing={3}
+                >
+                    <Grid
+                        item
+                        xs={12}
+                        sx={{
+                            height: 'fit-content',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                    >
+                        <Typography component={'h1'} variant='encabezado' sx={{ fontSize: '2.0rem' }}>
+                            {extraIngredient.name} Nº{extraIngredient.id}
+                        </Typography>
+                    </Grid>
+                    
+                    
+                    <Grid item xs={4}>
+                        <Typography>Nombre:</Typography>
+                    </Grid>
+                    <Grid item xs>
+                        <Typography>{extraIngredient.name}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider />
+                    </Grid>
+                    
+                    
+                    <Grid item xs={4}>
+                        <Typography>Precio:</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography>{extraIngredient.price}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider />
+                    </Grid>
+
+                    
+                    <Grid item xs={4}>
+                        <Typography>Precio al público:</Typography>
+                    </Grid>
+                    <Grid item xs>
+                        <Typography>${extraIngredient.totalPrice}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider />
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <Typography>Cantidad en inventario:</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography>{'Infinito'}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider />
+                    </Grid>
+                </Grid>
+            </Box>
+        </Modal>
+    )
+}
+
+export default ModalPizzaExtraIngredientDetails
