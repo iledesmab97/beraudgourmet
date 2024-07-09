45693c86475296d35827fc28cbb923a603a8d016
Troy Pernía <troy00pernia@gmail.com>
2024-07-09 11:24:36 -0400

    change components/ModalSaladDetails/CostSection: add validator function (line 17-22), add states (line 26-30), add handleChange (line 32-35). Add function handleEdit (line 37-49). Add updatePrice function (line 51-83). add condition to show InputUpdate component (line 101-102,147-148). Add TextField component (line 119-146)

--- components/ModalSaladDetails/CostSection.jsx
+++ components/ModalSaladDetails/CostSection.jsx
@@ -4,10 +4,84 @@ import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
 import TextField from '@mui/material/TextField'
 import InputAdornment from '@mui/material/InputAdornment'
-
 import InputUpdate from '@/components/InputUpdate/InputUpdate'
 
-function CostSection({ salad, saladNew, errors, handleChangeInput, handleInputsChecked }) {
+import IconButton from '@mui/material/IconButton'
+
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check'
+
+import { useState } from 'react'
+import useGetAlertMessage from '@/hooks/useGetAlertMessage'
+
+function validator(inputCost) {
+    let error = ''
+    if (!inputCost) error = 'este campo no puede estar vacio'
+    if (/[^0-9.]/.test(inputCost)) error = 'Solo números o punto (.)'
+    return error
+}
+
+function CostSection({ salad, saladNew, errors, handleChangeInput, handleInputsChecked, updateProperty, updateState }) {
+
+    const [price, setPrice] = useState(salad.price)
+    const [edit, setEdit] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+    const { handleUpdateAlertMessage } = useGetAlertMessage()
+
+    function handleChange(value) {
+        setPrice(value)
+        setError(validator(value))
+    }
+
+    async function handleEdit() {
+        if (edit) {
+            if (error) return
+            if (price != salad.price) {
+                const response = await updatePrice()
+                if (response !== 'Información guardada con exito') {
+                    return console.log(response)
+                }
+                console.log(response)
+            }
+        }
+        setEdit(prevState => !prevState)
+    }
+
+    async function updatePrice() {
+        console.log('Actualizando...')
+        setLoading(true)
+        const response = await updateProperty( salad.id, { property: 'cost', value: price })
+        let text, status
+        if (response.message) {
+            text = response.message
+            status = 'error'
+        } else {
+            text = 'Ingredientes actualizados exitosamente'
+            status = 'success'
+        }
+        handleUpdateAlertMessage({
+            checked: true,
+            text,
+            status
+        })
+        if (!response.message) {
+            updateState({
+                id: response.id,
+                properties: {
+                    price: response.cost,
+                    totalPrice: response.costIVAStripe
+                }
+            })
+
+            setLoading(false)
+            return 'Información guardada con exito'
+        } else {
+            setLoading(false)
+            return 'No se ha guardado la información exitosamente'
+        }
+    }
+
     return (
         <Grid
             container
@@ -24,21 +98,54 @@ function CostSection({ salad, saladNew, errors, handleChangeInput, handleInputsC
                 <Typography>Precio:</Typography>
             </Grid>
             <Grid item xs sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-                <InputUpdate
-                    value={salad.price}
-                    // updateProperty={updateExtraIngredientDB}
-                    // updateState={updateExtraIngredientFront}
-                    properties={{ id: salad.id, property: 'cost' }}
-                    // validateError={validationPrice}
-                    pizzaNew={saladNew}
-                    errors={errors.cost}
-                    handleChangeInput={handleChangeInput}
-                    handleInputsChecked={handleInputsChecked}
-                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                    sx={{
-                        width: '160px'
-                    }}
-                />
+                {
+                    saladNew ? (
+                        <InputUpdate
+                            value={salad.price}
+                            // updateProperty={updateProperty}
+                            // updateState={updateState}
+                            properties={{ id: salad.id, property: 'cost' }}
+                            // validateError={validationPrice}
+                            pizzaNew={saladNew}
+                            errors={errors.cost}
+                            handleChangeInput={handleChangeInput}
+                            handleInputsChecked={handleInputsChecked}
+                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                            sx={{
+                                width: '160px'
+                            }}
+                        />
+                    ) : (
+                        <TextField
+                            value={price}
+                            onChange={(event) => {handleChange(event.target.value)}}
+                            disabled={!edit}
+                            error={Boolean(error)}
+                            helperText={error}
+                            InputProps={{
+                                endAdornment: (
+                                    <IconButton
+                                        onClick={handleEdit}
+                                        disabled={loading}
+                                        // sx={errorStyles(errors)}
+                                    >
+                                        {
+                                            edit ? (
+                                                <CheckIcon />
+                                            ) : (
+                                                <EditIcon />
+                                            )
+                                        }
+                                    </IconButton>
+                                ),
+                                startAdornment: <InputAdornment position="start">$</InputAdornment>
+                            }}
+                            sx={{
+                                width: '160px'
+                            }}
+                        />
+                    )
+                }
             </Grid>
             <Grid item xs={12}>
                 <Divider />
