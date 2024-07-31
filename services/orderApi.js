c2a82220d9dca63f6dab49da659e99f4c8c313c4
Troy Pernía <troy00pernia@gmail.com>
2024-07-31 09:00:33 -0400

    change services/orderApi: change format

--- services/orderApi.js
+++ services/orderApi.js
@@ -1,117 +1,119 @@
-import { requestSettings } from '@/utils/preparingData'
+import { requestSettings } from "@/utils/preparingData";
 
-const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK
+const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK;
 
 export function getAllOrders(queries) {
-
-    let lastPath
+    let lastPath;
     if (queries) {
-        lastPath = Object.keys(queries).map(query => `${query}=${queries[query]}`).join('&&')
+        lastPath = Object.keys(queries)
+            .map((query) => `${query}=${queries[query]}`)
+            .join("&&");
     }
 
-    return fetch(`${PATH_BACK}/orders${lastPath ? '?' + lastPath : ''}`, {
+    return fetch(`${PATH_BACK}/orders${lastPath ? "?" + lastPath : ""}`, {
         ...requestSettings(),
-        cache: 'no-store'
+        cache: "no-store",
     })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            return data
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            return data;
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
 
 export function getAllOrdersOfUser(userId) {
-
     return fetch(`${PATH_BACK}/orders/user/${userId}`, {
         ...requestSettings(),
-        cache: 'no-store'
+        cache: "no-store",
     })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            return data
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            return data;
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
 
 export function getOneOrder(orderId) {
     return fetch(`${PATH_BACK}/orders/${orderId}`, {
         ...requestSettings(),
-        cache: 'no-store'
+        cache: "no-store",
     })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            return data[0]
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            return data[0];
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
 
 export function updateOrder(id, body) {
     return fetch(`${PATH_BACK}/orders/${id}`, {
-        ...requestSettings('PUT'),
+        ...requestSettings("PUT"),
         body: JSON.stringify(body),
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            return data
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            return data;
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
 
 export async function sendImage(id, formData) {
     return fetch(`${PATH_BACK}/orders/image/${id}`, {
-        ...requestSettings('POST', null, 'image'),
+        ...requestSettings("POST", null, "image"),
         body: formData,
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            return data
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            return data;
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
 
 export async function registerOrder(data) {
     return fetch(`${PATH_BACK}/orders`, {
-        ...requestSettings('POST'),
-        body: JSON.stringify(data)
+        ...requestSettings("POST"),
+        body: JSON.stringify(data),
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            console.log('La orden fue creada exitosamente')
-            return data
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            console.log("La orden fue creada exitosamente");
+            return data;
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
 
 export function requestRemovalOrder(id) {
-    if (!id) return {message: 'id can not be undefined'}
+    if (!id) return { message: "id can not be undefined" };
     return fetch(`${PATH_BACK}/orders/${id}`, {
-        ...requestSettings('DELETE')
+        ...requestSettings("DELETE"),
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            console.log('La orden fue removida exitosamente')
-            return data
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            console.log("La orden fue removida exitosamente");
+            return data;
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
 
 export function changeOrderItems(data) {
     return fetch(`${PATH_BACK}/orders/changeItems`, {
-        ...requestSettings('PUT'),
-        body: JSON.stringify(data)
+        ...requestSettings("PUT"),
+        body: JSON.stringify(data),
     })
-        .then(res => res.json())
-        .then(data => {
-            if (data.message) throw new Error(data.message)
-            console.log('Los items de la orden fueron actualizados exitosamente')
-            return data
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.message) throw new Error(data.message);
+            console.log(
+                "Los items de la orden fueron actualizados exitosamente"
+            );
+            return data;
         })
-        .catch(error => ({message: error.message}))
+        .catch((error) => ({ message: error.message }));
 }
