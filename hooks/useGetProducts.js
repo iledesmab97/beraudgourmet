63d94256e20e105ff9a2a5eca2d6fc06e46d6d54
Troy Pernía <troy00pernia@gmail.com>
2024-08-02 15:38:26 -0400

    change hooks/useGetProducts: change format

--- hooks/useGetProducts.js
+++ hooks/useGetProducts.js
@@ -1,67 +1,74 @@
-import { useAppSelector, useAppDispatch } from '@/hooks/store'
-import { addProductsList, updateProductsList } from '@/stores/products/slice'
+import { useAppSelector, useAppDispatch } from "@/hooks/store";
+import { addProductsList, updateProductsList } from "@/stores/products/slice";
 
-export default function useGetProducts ({type}) {
+export default function useGetProducts({ type }) {
+    const products = useAppSelector((state) => state.products[type]);
+    const totalProducts = useAppSelector((state) => state.products);
+    const dispatch = useAppDispatch();
 
-    const products = useAppSelector(state => state.products[type])
-    const totalProducts = useAppSelector(state => state.products)
-    const dispatch = useAppDispatch()
-
-    function handleAddProductsList({type, products}) {
-        dispatch(addProductsList({type, products}))
+    function handleAddProductsList({ type, products }) {
+        dispatch(addProductsList({ type, products }));
     }
 
     function handleUpdateProduct(newProduct) {
-        const { type, id, property, value } = newProduct
-        let index
+        const { type, id, property, value } = newProduct;
+        let index;
         const [productToUpdate] = products.filter((element, i) => {
-            if (element.id !== id) return false
-            index = i
-            return true
-        })
-        const productUpdated = {...productToUpdate}
-        productUpdated[property] = value
-        const newProductList = [...products]
-        newProductList[index] = productUpdated
-        dispatch(updateProductsList({type, newProductList}))
+            if (element.id !== id) return false;
+            index = i;
+            return true;
+        });
+        const productUpdated = { ...productToUpdate };
+        productUpdated[property] = value;
+        const newProductList = [...products];
+        newProductList[index] = productUpdated;
+        dispatch(updateProductsList({ type, newProductList }));
     }
 
     function handleDeleteProduct(newProduct) {
-        const { type, id } = newProduct
-        const newProductList = products.filter(element => {
-            if (element.id !== id) return true
-            return false
-        })
-        dispatch(updateProductsList({type, newProductList}))
+        const { type, id } = newProduct;
+        const newProductList = products.filter((element) => {
+            if (element.id !== id) return true;
+            return false;
+        });
+        dispatch(updateProductsList({ type, newProductList }));
     }
 
     function handleAddProduct({ type, newProduct }) {
         const newProductToAdd = {
             ...newProduct,
             price: newProduct.cost,
-            totalPrice: newProduct.costIVAStripe
-        }
-        delete newProductToAdd.cost
-        delete newProductToAdd.costIVA
-        delete newProductToAdd.costIVAStripe
-        const newProductList = [...products]
-        newProductList.push(newProductToAdd)
-        dispatch(updateProductsList({ type, newProductList }))
+            totalPrice: newProduct.costIVAStripe,
+        };
+        delete newProductToAdd.cost;
+        delete newProductToAdd.costIVA;
+        delete newProductToAdd.costIVAStripe;
+        const newProductList = [...products];
+        newProductList.push(newProductToAdd);
+        dispatch(updateProductsList({ type, newProductList }));
     }
 
     function handleUpdateManyPropertiesProduct(newProduct) {
-        const { type, id, properties } = newProduct
-        let index
+        const { type, id, properties } = newProduct;
+        let index;
         const productToUpdate = products.find((element, i) => {
-            if (element.id !== id) return false
-            index = i
-            return true
-        })
-        const productUpdated = {...productToUpdate, ...properties}
-        const newProductList = [...products]
-        newProductList[index] = productUpdated
-        dispatch(updateProductsList({type, newProductList}))
+            if (element.id !== id) return false;
+            index = i;
+            return true;
+        });
+        const productUpdated = { ...productToUpdate, ...properties };
+        const newProductList = [...products];
+        newProductList[index] = productUpdated;
+        dispatch(updateProductsList({ type, newProductList }));
     }
-    
-    return { products, totalProducts, handleAddProductsList, handleUpdateProduct, handleDeleteProduct, handleAddProduct, handleUpdateManyPropertiesProduct }
+
+    return {
+        products,
+        totalProducts,
+        handleAddProductsList,
+        handleUpdateProduct,
+        handleDeleteProduct,
+        handleAddProduct,
+        handleUpdateManyPropertiesProduct,
+    };
 }
