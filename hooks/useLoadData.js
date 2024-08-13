72bf5b7c835756b62c24f9bd5602bea27b783b22
Troy Pernía <troy00pernia@gmail.com>
2024-08-13 17:12:14 -0400

    change hooks/useLoadData: the states place, stores, status, error and firstTime were added (line 21-27). A new useEffect was added (line 56-88)

--- hooks/useLoadData.js
+++ hooks/useLoadData.js
@@ -1,21 +1,30 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 import useGetExtraIngredients from "@/hooks/useGetExtraIngredients";
 import useGetOrderList from "@/hooks/useGetOrderList";
 import useLogedUser from "@/hooks/useLogedUser";
-import { lookingForUserLoged } from "@/services/userApi";
-
 import useGetUser from "@/hooks/useGetUser";
+import useGetPlace from "@/hooks/useGetPlace";
 
+import { lookingForUserLoged } from "@/services/userApi";
 import { getExtraIngredients } from "@/services/productApi";
 import { getAllOrders } from "@/services/orderApi";
 import useLocalData from "./useLocalData";
+import { deepEqual, validPlaceLocal } from "@/utils/preparingData";
 
 function useLoadData() {
     const { handleAddExtraIngredinetsList } = useGetExtraIngredients();
     const { orderList, handleAddOrderList } = useGetOrderList();
     const { gerUserLoged } = useLogedUser();
-    const { getLocalData, saveLocalData } = useLocalData();
+    const { getLocalData, saveLocalData, removeLocalData } = useLocalData();
     const { handleAddUser } = useGetUser();
+    const { place, handleAddPlace } = useGetPlace();
+    const {
+        stores: storeListArray,
+        status,
+        error,
+    } = useSelector((state) => state.storeList);
+    const firstTime = useRef(true);
 
     const loadData = useCallback(async (rol) => {
         // Cargar usuario
@@ -43,6 +52,40 @@ function useLoadData() {
             else handleAddOrderList(newOrderList);
         }
     }, []);
+
+    // update data from place to localStorage
+    useEffect(() => {
+        if (status !== "succeeded") return;
+        const placeLocal = getLocalData("place");
+        // load data from localStorage
+        if (firstTime.current && placeLocal) {
+            const isPlaceValid = validPlaceLocal(placeLocal);
+            if (!isPlaceValid) {
+                return removeLocalData("place");
+            }
+            firstTime.current = false;
+            const closerStore = storeListArray.find(
+                (store) => store.id === placeLocal.closerStore
+            );
+            if (!Object.keys(place).length && placeLocal && closerStore) {
+                handleAddPlace({
+                    ...placeLocal,
+                    closerStore,
+                });
+            }
+        } else {
+            // update every time the place is modified
+            firstTime.current = false;
+            if (!Object.keys(place).length) return;
+            const placeToCompare = {
+                ...place,
+                closerStore: place.closerStore.id,
+            };
+            if (!deepEqual(placeLocal, placeToCompare)) {
+                saveLocalData("place", placeToCompare);
+            }
+        }
+    }, [status, place]);
 
     return { loadData };
 }
