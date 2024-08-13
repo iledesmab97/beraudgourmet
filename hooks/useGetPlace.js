916acdacd867f2a3d00b3d33af76c0bbdec430b7
Troy Pernía <troy00pernia@gmail.com>
2024-08-13 11:16:41 -0400

    change hooks/useGetPlace: the function handleMakePlace was added (line 15-18) and exported (line 41). format file was modified

--- hooks/useGetPlace.js
+++ hooks/useGetPlace.js
@@ -1,30 +1,48 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { addPlace, removePlace, addDeadLine, addTypeDelivery, updatePlaceToInitialState } from '@/stores/place/slice'
+import { useSelector, useDispatch } from "react-redux";
+import {
+    makePlace,
+    addPlace,
+    removePlace,
+    addDeadLine,
+    addTypeDelivery,
+    updatePlaceToInitialState,
+} from "@/stores/place/slice";
 
-export default function useGetPlace () {
+export default function useGetPlace() {
+    const place = useSelector((state) => state.place);
+    const dispatch = useDispatch();
 
-    const place = useSelector(state => state.place)
-    const dispatch = useDispatch()
-    
+    function handleMakePlace(newPlace) {
+        dispatch(makePlace(newPlace));
+    }
+
     function handleAddPlace(newPlace) {
-        dispatch(addPlace(newPlace))
+        dispatch(addPlace(newPlace));
     }
 
     function handleRemovePlace(placeToRemove) {
-        dispatch(removePlace(placeToRemove))
+        dispatch(removePlace(placeToRemove));
     }
 
     function handleDeadLine(time) {
-        dispatch(addDeadLine(time))
+        dispatch(addDeadLine(time));
     }
 
     function handleTypeDelivery(date) {
-        dispatch(addTypeDelivery(date))
+        dispatch(addTypeDelivery(date));
     }
 
     function handleUpdatePlaceToInitialState() {
-        dispatch(updatePlaceToInitialState())
+        dispatch(updatePlaceToInitialState());
     }
 
-    return {place, handleAddPlace, handleRemovePlace, handleDeadLine, handleTypeDelivery, handleUpdatePlaceToInitialState}
+    return {
+        place,
+        handleMakePlace,
+        handleAddPlace,
+        handleRemovePlace,
+        handleDeadLine,
+        handleTypeDelivery,
+        handleUpdatePlaceToInitialState,
+    };
 }
