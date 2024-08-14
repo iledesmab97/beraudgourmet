dc432e9103f1e78a976b55a210178e1acc274a03
Troy Pernía <troy00pernia@gmail.com>
2024-08-14 18:03:15 -0400

    change components/UberComponents/QuoteComponente: the useEffect and states were removed. the text to render in Typography component about timer was modified (line 31, 54)

--- components/UberComponents/QuoteComponent.jsx
+++ components/UberComponents/QuoteComponent.jsx
@@ -1,57 +1,13 @@
-import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 
 import CenteredSpinner from "../LoadingSpinner/CenteredSpinner";
 
-import { useState, useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-    fetchDeliveryQuote,
-    createNewDeliveryOrder,
-    fetchDeliveryTracking,
-    cancelExistingDelivery,
-} from "@/stores/actions/uberDirect";
-import { timeOutCalculator, howMuchLeftTime } from '@/utils/hours'
+import { useSelector } from "react-redux";
 
 const QuoteComponent = () => {
-    const { quote, loading, error } = useSelector((state) => state.uberQuote);
-    const { inputsHome, closerStore } = useSelector(state => state.place)
-    const [timeOut, setTimeOut] = useState(timeOutCalculator(0))
-    const [currentTimer, setCurrentTimer] = useState(() => {
-        return howMuchLeftTime(timeOut)
-    })
-    const dispatch = useDispatch();
-    
-    // Refresh timeOut
-    useEffect(() => {
-        if (loading || error) return
-        setTimeOut(timeOutCalculator(15))
-    }, [loading])
-
-    // fetch delivery quote
-    useEffect(() => {
-        if (!closerStore || currentTimer.sec > 0) return
-        dispatch(
-            fetchDeliveryQuote({
-                pickup_address: closerStore.place,
-                dropoff_address: inputsHome.inputAddress,
-            })
-        );
-    }, [closerStore, currentTimer]);
-
-    // turn timeOut on and off
-    useEffect(() => {
-        function activeClock() {
-            const newCurrentTimer = howMuchLeftTime(timeOut)
-            if (newCurrentTimer.sec >= 0) return setCurrentTimer(newCurrentTimer)
-            else clearInterval(intervalId)
-        }   
-        const intervalId = setInterval(activeClock, 1000)
-        return () => {
-            clearInterval(intervalId)
-        }
-    }, [timeOut])
+    const { quote, loading, error, timeExpiration } = useSelector((state) => state.uberQuote);
+    const { currentTimer } = timeExpiration
 
     return (
         <Box
@@ -72,7 +28,7 @@ const QuoteComponent = () => {
                         variant="body1"
                         fontWeight="bold"
                     >
-                        Precio de envío estimado ({currentTimer.min}):
+                        Precio de envío estimado ({currentTimer ? currentTimer.min.slice(0,2): '00'} min):
                     </Typography>
                     <CenteredSpinner
                         width={20}
@@ -95,7 +51,7 @@ const QuoteComponent = () => {
                     mt={4}
                 >
                     <Typography variant="body1" fontWeight="bold">
-                        Precio de envío estimado ({currentTimer.min}):
+                        Precio de envío estimado ({currentTimer ? currentTimer.min.slice(0,2): '00:00'} min):
                     </Typography>
                     <Typography variant="body1" color="primary">
                         {quote.fee / 100} MXN
