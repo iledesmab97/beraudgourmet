eda25cc32a88315edd667e409c345c31a830e1e3
iledesmab97 <iledesmab97@gmail.com>
2024-08-28 09:38:41 -0600

    shadow

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,8 @@ const ServiceCard = styled(Card)(({ theme }) => ({
     transition: "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out",
     "&:hover": {
         transform: "translateY(-5px)",
-        boxShadow: theme.shadows[4],
+        boxShadow:
+            "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
     },
 }));
 
