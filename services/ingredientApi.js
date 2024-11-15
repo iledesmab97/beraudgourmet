2c27a151123e06329f4e41282a377cc753d41360
Troy Pernía <troy00pernia@gmail.com>
2024-11-15 14:17:34 -0400

    changed file services/ingredientApi: the function getExtraIngredients was added (line 6-23). The format file was changed

--- services/ingredientApi.js
+++ services/ingredientApi.js
@@ -1,54 +1,73 @@
-import { requestSettings } from '@/utils/preparingData'
+import { requestSettings } from "@/utils/preparingData";
+import { generateURLQueries } from "@/utils/preparingData";
 
-const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK
+const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK;
+
+export async function getExtraIngredients(queries) {
+    const queriesString = generateURLQueries(queries);
+    const res = await fetch(`${PATH_BACK}/ingredients${queriesString}`);
+    const data = await res.json();
+    if (data.message) throw new Error(data.message);
+    const extraIngredinetList = {};
+    data.forEach((extraIngredient) => {
+        const { id, name, cost, costIVAStripe, available } = extraIngredient;
+        extraIngredinetList[name] = {
+            id,
+            name,
+            price: cost,
+            totalPrice: costIVAStripe ? costIVAStripe : "0",
+            available,
+        };
+    });
+    return extraIngredinetList;
+}
 
 export function addIngredient(name) {
     return fetch(`${PATH_BACK}/ingredients`, {
-        ...requestSettings('POST'),
-        body: JSON.stringify({name})
+        ...requestSettings("POST"),
+        body: JSON.stringify({ name }),
     })
-        .then(response => {
-            return response.json()
+        .then((response) => {
+            return response.json();
         })
-        .then(response => {
-            if (response.message) throw new Error(response.message)
-            return response
+        .then((response) => {
+            if (response.message) throw new Error(response.message);
+            return response;
         })
-        .catch(error => {
-            return {message: error.message}
-        })
+        .catch((error) => {
+            return { message: error.message };
+        });
 }
 
-export function removeIngredient({id, name}) {
+export function removeIngredient({ id, name }) {
     if (id) {
         return fetch(`${PATH_BACK}/ingredients/${id}`, {
-            ...requestSettings('DELETE')
+            ...requestSettings("DELETE"),
         })
-            .then(response => {
-                return response.json()
+            .then((response) => {
+                return response.json();
             })
-            .then(response => {
-                if (response.message) throw new Error(response.message)
-                return response
+            .then((response) => {
+                if (response.message) throw new Error(response.message);
+                return response;
             })
-            .catch(error => {
-                return {message: error.message}
-            })
-    }
-    else if (name) {
+            .catch((error) => {
+                return { message: error.message };
+            });
+    } else if (name) {
         return fetch(`${PATH_BACK}/ingredients`, {
-            ...requestSettings('DELETE'),
-            body: JSON.stringify({name})
+            ...requestSettings("DELETE"),
+            body: JSON.stringify({ name }),
         })
-            .then(response => {
-                return response.json()
+            .then((response) => {
+                return response.json();
             })
-            .then(response => {
-                if (response.message) throw new Error(response.message)
-                return response
+            .then((response) => {
+                if (response.message) throw new Error(response.message);
+                return response;
             })
-            .catch(error => {
-                return {message: error.message}
-            })
+            .catch((error) => {
+                return { message: error.message };
+            });
     }
 }
