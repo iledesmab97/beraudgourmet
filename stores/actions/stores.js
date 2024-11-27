86c70add3be2a31127185a94c60fd04c87d12b7d
Troy Pernía <troy00pernia@gmail.com>
2024-11-27 09:45:10 -0400

    changed file stores/actions/stores: the delay function was removed (line 3, 10)

--- stores/actions/stores.js
+++ stores/actions/stores.js
@@ -1,13 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getAllStores } from "../../services/storeApi"; // Ensure the correct path
-import { delay } from "@/utils/wait";
 
 export const fetchStoreListThunk = createAsyncThunk(
     "storeList/fetchStoreList",
     async (queries, { rejectWithValue }) => {
         try {
             const storeList = await getAllStores(queries);
-            await delay(3000);
             return storeList;
         } catch (error) {
             return rejectWithValue(error.message);
