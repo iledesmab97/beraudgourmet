c3d5a6825488a1fd3b5262c2a8520c9f79136896
Troy Pernía <troy00pernia@gmail.com>
2024-12-02 17:00:42 -0400

    changed file components/ModalStoreDelivery/StorePickup: the function selectStore was modified adding new removeLocalData (line 75)

--- components/ModalStoreDelivery/StorePickup.jsx
+++ components/ModalStoreDelivery/StorePickup.jsx
@@ -72,6 +72,7 @@ export default function StorePickup({
         });
         removeLocalData("countdownTimer");
         removeLocalData("expirationDate");
+        removeLocalData("quote");
         handleCloseModal("place");
         nextStep("store");
     }
