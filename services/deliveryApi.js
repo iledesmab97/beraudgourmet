2f85cc3eb72944687f88f80ba1f2f22d5a9b6f33
Troy Pernía <troy00pernia@gmail.com>
2025-01-08 16:25:19 -0400

    added file services/deliveryApi

---
+++ services/deliveryApi.js
@@ -0,0 +1,15 @@
+import { requestSettings } from "@/utils/preparingData";
+
+const PATH_BACK = process.env.NEXT_PUBLIC_PATH_BACK;
+
+export async function createDelivery(body) {
+    console.log("body:", body);
+    const res = await fetch(`${PATH_BACK}/deliveries`, {
+        ...requestSettings("POST"),
+        body: JSON.stringify(body),
+    });
+    const data = await res.json();
+    if (data.message)
+        throw new Error(`Error creating delivery data: ${data.message}`);
+    return data;
+}
