927b5fa3fe4c514505ecfc56029035a27230c649
Troy Pernía <troy00pernia@gmail.com>
2025-01-30 14:27:57 -0400

    changed file components/ModalUserOrders/OrdersList: the function getFormatOrderDate was added (line 21-24). the List was wrapped for condition (line 37-117). the TablePagination component was modified adding new styles (line 139-145)

--- components/ModalUserOrders/OrdersList.jsx
+++ components/ModalUserOrders/OrdersList.jsx
@@ -13,9 +13,15 @@ import CenteredSpinner from '../LoadingComponets/CenteredSpinner';
 import TablePaginationActions from "@/components/TablePaginationActions/TablePaginationActions"
 
 import { extractIngredientsOut } from '@/utils/preparingData'
+import { dateStringToDate } from '@/utils/hours';
 
 function OrdersList({ orders, loading, pagination }) {
     const { count, rowsPerPage, page, handleChangePage, handleChangeRowsPerPage } = pagination
+
+    function getFormatOrderDate(dateString) {
+        const date = dateStringToDate({ dateString }) 
+        return date.format("DD/MM/YYYY - HH:mm")
+    }
 
     return (
         <Grid
@@ -28,76 +34,6 @@ function OrdersList({ orders, loading, pagination }) {
                 justifyContent: 'space-between'
             }}
         >
-            <List
-                sx={{
-                    overflowY: 'auto'
-                }}
-            >
-                {
-                    orders.map(order => (
-                        <>
-                            <ListItem alignItems={'flex-start'}>
-                                <ListItemText
-                                    primary={
-                                        <>
-                                            <Typography component={'span'} sx={{fontWeight: 'bold'}}>Estatus: </Typography>
-                                            <Typography
-                                                component={'span'}
-                                                sx={ order.closed ? {color: 'green'} : {color: 'red'} }
-                                            >
-                                                {order.closed ? 'Entregado' : 'Pendiente'}
-                                            </Typography>
-                                        </>
-                                    }
-                                    secondary={
-                                        <>
-                                            <ul
-                                                style={{
-                                                    listStyleType: 'disc',
-                                                    paddingLeft: '16px'
-                                                }}
-                                            >
-                                                <li>
-                                                    <Typography component={'span'} sx={{fontWeight: 'bold'}}>Pedido: </Typography>
-                                                    <Typography
-                                                        dangerouslySetInnerHTML={{
-                                                        __html: order.itemsxOrder.map(item => {
-                                                            const ingredinetsOut = extractIngredientsOut(item.description)
-                                                            if (!ingredinetsOut.length) return item.description
-                                                            const index = item.description.indexOf(', ~')
-                                                            return (
-                                                                item.description.slice(0, index) + ingredinetsOut.map( ingredient => `, <span style="text-decoration: line-through">${ingredient}</span>` ).join('')
-                                                            )
-                                                        }).join('; ')
-                                                    }}
-                                                        sx={{
-                                                            display: 'inline'
-                                                        }}
-                                                    />
-                                                </li>
-                                                <li>
-                                                    <Typography component={'span'} sx={{fontWeight: 'bold'}}>Emición: </Typography>
-                                                    <Typography sx={{display: 'inline'}}>{order.applicationDate}</Typography>
-                                                </li>
-                                                <li>
-                                                    <Typography component={'span'} sx={{fontWeight: 'bold'}}>Entrega: </Typography>
-                                                    <Typography sx={{display: 'inline'}}>{order.deliveryDate}</Typography>
-                                                </li>
-                                                <li>
-                                                    <Typography component={'span'} sx={{fontWeight: 'bold'}}>Precio($): </Typography>
-                                                    <Typography sx={{display: 'inline'}}>{order.totalCost}</Typography>
-                                                </li>
-                                            </ul>
-                                            <br />
-                                        </>
-                                    }
-                                />
-                            </ListItem>
-                            <Divider />
-                        </>
-                    ))
-                }
-            </List>
             {
                 loading ? (
                     <Box
@@ -107,7 +43,78 @@ function OrdersList({ orders, loading, pagination }) {
                     >
                         <CenteredSpinner />
                     </Box>
-                ) : null
+                ) : (
+                    <List
+                        sx={{
+                            overflowY: 'auto'
+                        }}
+                    >
+                        {
+                            orders.map(order => (
+                                <>
+                                    <ListItem alignItems={'flex-start'}>
+                                        <ListItemText
+                                            primary={
+                                                <>
+                                                    <Typography component={'span'} sx={{fontWeight: 'bold'}}>Estatus: </Typography>
+                                                    <Typography
+                                                        component={'span'}
+                                                        sx={ order.closed ? {color: 'green'} : {color: 'red'} }
+                                                    >
+                                                        {order.closed ? 'Entregado' : 'Pendiente'}
+                                                    </Typography>
+                                                </>
+                                            }
+                                            secondary={
+                                                <>
+                                                    <ul
+                                                        style={{
+                                                            listStyleType: 'disc',
+                                                            paddingLeft: '16px'
+                                                        }}
+                                                    >
+                                                        <li>
+                                                            <Typography component={'span'} sx={{fontWeight: 'bold'}}>Pedido: </Typography>
+                                                            <Typography
+                                                                dangerouslySetInnerHTML={{
+                                                                __html: order.itemsxOrder.map(item => {
+                                                                    const ingredinetsOut = extractIngredientsOut(item.description)
+                                                                    if (!ingredinetsOut.length) return item.description
+                                                                    const index = item.description.indexOf(', ~')
+                                                                    return (
+                                                                        item.description.slice(0, index) + ingredinetsOut.map( ingredient => `, <span style="text-decoration: line-through">${ingredient}</span>` ).join('')
+                                                                    )
+                                                                }).join('; ')
+                                                            }}
+                                                                sx={{
+                                                                    display: 'inline'
+                                                                }}
+                                                            />
+                                                        </li>
+                                                        <li>
+                                                            <Typography component={'span'} sx={{fontWeight: 'bold'}}>Emición: </Typography>
+                                                            <Typography sx={{display: 'inline'}}>{getFormatOrderDate( order.applicationDate )}</Typography>
+                                                        </li>
+                                                        <li>
+                                                            <Typography component={'span'} sx={{fontWeight: 'bold'}}>Entrega: </Typography>
+                                                            <Typography sx={{display: 'inline'}}>{getFormatOrderDate( order.deliveryDate )}</Typography>
+                                                        </li>
+                                                        <li>
+                                                            <Typography component={'span'} sx={{fontWeight: 'bold'}}>Precio($): </Typography>
+                                                            <Typography sx={{display: 'inline'}}>{order.totalCost}</Typography>
+                                                        </li>
+                                                    </ul>
+                                                    <br />
+                                                </>
+                                            }
+                                        />
+                                    </ListItem>
+                                    <Divider />
+                                </>
+                            ))
+                        }
+                    </List>
+                )
             }
             <Box
                 sx={{
@@ -124,13 +131,18 @@ function OrdersList({ orders, loading, pagination }) {
                         handleChangePage(newPage);
                     }}
                     onRowsPerPageChange={handleChangeRowsPerPage}
-                    // labelRowsPerPage={"Filas por página"}
                     labelRowsPerPage={""}
                     ActionsComponent={TablePaginationActions}
                     sx={{
-                        width: "100%",
                         "& .MuiTablePagination-toolbar": {
                             p: 0
+                        },
+                        "& .MuiTablePagination-input": {
+                            ml: "0px",
+                            mr: "8px"
+                        },
+                        "& .MuiBox-root": {
+                            ml: "0px"
                         }
                     }}
                 />
