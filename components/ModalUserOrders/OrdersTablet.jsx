d6dc5d188500fd0850ab0273e64cd39d275bc7f8
Troy Pernía <troy00pernia@gmail.com>
2025-01-31 15:55:56 -0400

    changed file components/ModalUserOrders/OrdersTablet: some styles were modified for Grid component (line 33-39), Paper component (line 43-46)

--- components/ModalUserOrders/OrdersTablet.jsx
+++ components/ModalUserOrders/OrdersTablet.jsx
@@ -1,3 +1,4 @@
+import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
@@ -10,6 +11,7 @@ import TableRow from '@mui/material/TableRow';
 import TablePagination from "@mui/material/TablePagination";
 
 import TablePaginationActions from "@/components/TablePaginationActions/TablePaginationActions"
+import EmptyOrdersContainer from "@/components/ModalUserOrders/EmptyOrdersContainer"
 
 import CenteredSpinner from '../LoadingComponets/CenteredSpinner';
 
@@ -28,136 +30,180 @@ function OrdersTablet({ orders, ubers, loading, pagination }) {
         <Grid
             item
             sx={{
+                p: 1,
                 flexGrow: 1,
-                overflowY: 'auto',
                 display: 'flex',
                 flexDirection: 'column',
-                justifyContent: 'space-between'
+                justifyContent: 'flex-start',
+                alignItems: "center",
+                overflow: "hidden"
             }}
         >
-            <TableContainer
+            <Paper
+                elevation={3}
                 sx={{
-                    flexGrow: 1,
-                    maxHeight: "calc(100% - 52px)",
+                    width: "100%",
+                    height: "100%",
                     display: "flex",
                     flexDirection: "column"
                 }}
             >
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align='center'>ID</TableCell>
-                            <TableCell >Pedido</TableCell>
-                            <TableCell align='center'>Emición</TableCell>
-                            <TableCell align='center'>Entrega</TableCell>
-                            <TableCell
-                                align='center'
-                            >
-                                Precio($)
-                            </TableCell>
-                            <TableCell
-                                align='center'
-                            >
-                                Estatus
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            loading ? null : (
-                                orders.map( order => (
-                                    <>
-                                        <TableRow
-                                            key={order.id}
-                                            sx={ ubers[order.id] && {
-                                                '& > *': {
-                                                    borderBottom: "none"
-                                                }
+                <TableContainer
+                    sx={{
+                        flexGrow: 1,
+                    }}
+                >
+                    <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align='center'>ID</TableCell>
+                                <TableCell >Pedido</TableCell>
+                                <TableCell align='center'>Emición</TableCell>
+                                <TableCell align='center'>Entrega</TableCell>
+                                <TableCell
+                                    align='center'
+                                >
+                                    Precio($)
+                                </TableCell>
+                                <TableCell
+                                    align='center'
+                                >
+                                    Estatus
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                loading ? (
+                                    <TableRow>
+                                        <TableCell
+                                            colSpan={6}
+                                            sx={{
+                                                p: 0
                                             }}
                                         >
-                                            <TableCell align='center'>{order.id}</TableCell>
-                                            <TableCell
-                                                dangerouslySetInnerHTML={{
-                                                    __html: order.itemsxOrder.map(item => {
-                                                        const ingredinetsOut = extractIngredientsOut(item.description)
-                                                        if (!ingredinetsOut.length) return item.description
-                                                        const index = item.description.indexOf(', ~')
-                                                        return (
-                                                            item.description.slice(0, index) + ingredinetsOut.map( ingredient => `, <span style="text-decoration: line-through">${ingredient}</span>` ).join('')
-                                                        )
-                                                    }).join('; ')
-                                                }}/>
-                                            <TableCell align='center'>{getFormatOrderDate(order.applicationDate)}</TableCell>
-                                            <TableCell align='center'>{getFormatOrderDate(order.deliveryDate)}</TableCell>
-                                            <TableCell align='center'>{order.totalCost}</TableCell>
-                                            <TableCell
-                                                align='center'
-                                                sx={ order.closed ? {color: 'green'} : {color: 'red'} }
+                                            <Box
+                                                sx={{
+                                                    height: "441px"
+                                                }}
                                             >
-                                                {order.closed ? 'Entregado' : 'Pendiente'}
-                                            </TableCell>
-                                        </TableRow>
-                                        {
-                                            ubers[order.id] && !order.closed ? (
-                                                <TableRow>
-                                                    <TableCell
-                                                        colSpan={6}
-                                                        sx={{
-                                                            pt: "0px"
-                                                        }}
-                                                    >
-                                                        <Box
+                                                <CenteredSpinner />
+                                            </Box>
+                                        </TableCell>
+                                    </TableRow>
+                                ) : !orders.length ? (
+                                    <TableRow>
+                                        <TableCell
+                                            colSpan={6}
+                                            sx={{
+                                                p: 0
+                                            }}
+                                        >
+                                            <Box
+                                                sx={{
+                                                    height: "441px",
+                                                    display: "flex",
+                                                    justifyContent: "center",
+                                                    alignItems: "center"
+                                                }}
+                                            >
+                                                <EmptyOrdersContainer/> 
+                                            </Box>
+                                        </TableCell>
+                                    </TableRow>
+                                ) : (
+                                    orders.map( order => (
+                                        <>
+                                            <TableRow
+                                                key={order.id}
+                                                sx={ ubers[order.id] && {
+                                                    '& > *': {
+                                                        borderBottom: "none"
+                                                    }
+                                                }}
+                                            >
+                                                <TableCell align='center'>{order.id}</TableCell>
+                                                <TableCell
+                                                    dangerouslySetInnerHTML={{
+                                                        __html: order.itemsxOrder.map(item => {
+                                                            const ingredinetsOut = extractIngredientsOut(item.description)
+                                                            if (!ingredinetsOut.length) return item.description
+                                                            const index = item.description.indexOf(', ~')
+                                                            return (
+                                                                item.description.slice(0, index) + ingredinetsOut.map( ingredient => `, <span style="text-decoration: line-through">${ingredient}</span>` ).join('')
+                                                            )
+                                                        }).join('; ')
+                                                    }}/>
+                                                <TableCell align='center'>{getFormatOrderDate(order.applicationDate)}</TableCell>
+                                                <TableCell align='center'>{getFormatOrderDate(order.deliveryDate)}</TableCell>
+                                                <TableCell align='center'>{order.totalCost}</TableCell>
+                                                <TableCell
+                                                    align='center'
+                                                    sx={ order.closed ? {color: 'green'} : {color: 'red'} }
+                                                >
+                                                    {order.closed ? 'Entregado' : 'Pendiente'}
+                                                </TableCell>
+                                            </TableRow>
+                                            {
+                                                ubers[order.id] && !order.closed ? (
+                                                    <TableRow>
+                                                        <TableCell
+                                                            colSpan={6}
                                                             sx={{
-                                                                display: "flex",
-                                                                justifyContent: "flex-end",
-                                                                alignItems: "center"
+                                                                pt: "0px"
                                                             }}
                                                         >
-                                                            <Button
-                                                                component="a"
-                                                                variant="contained"
-                                                                color='primary'
-                                                                href={ubers[order.id]}
-                                                                target='_blank'
+                                                            <Box
+                                                                sx={{
+                                                                    display: "flex",
+                                                                    justifyContent: "flex-end",
+                                                                    alignItems: "center"
+                                                                }}
                                                             >
-                                                                Seguimiento
-                                                            </Button>
-                                                        </Box>
-                                                    </TableCell>
-                                                </TableRow>
-                                            ) : null
-                                        }
-                                    </>
-                                ))
-                            ) 
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            {
-                loading ? (
-                    <Box
-                        sx={{
-                            height: 'calc(100% - 56.5px - 52px)',
+                                                                <Button
+                                                                    component="a"
+                                                                    variant="contained"
+                                                                    color='primary'
+                                                                    href={ubers[order.id]}
+                                                                    target='_blank'
+                                                                >
+                                                                    Seguimiento
+                                                                </Button>
+                                                            </Box>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ) : null
+                                            }
+                                        </>
+                                    ))
+                                ) 
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <Box
+                    sx={{
+                        width: "100%",
+                        height: "auto",
+                        display: "flex",
+                        justifyContent: "flex-end",
+                    }}
+                >
+                    <TablePagination
+                        rowsPerPageOptions={[10, 15, 25]}
+                        component="div"
+                        count={count}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={(event, newPage) => {
+                            handleChangePage(newPage);
                         }}
-                    >
-                        <CenteredSpinner />
-                    </Box>
-                ) : null
-            }
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={count}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={(event, newPage) => {
-                    handleChangePage(newPage);
-                }}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                labelRowsPerPage={"Filas por página"}
-                ActionsComponent={TablePaginationActions}
-            />
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                        labelRowsPerPage={"Filas por página"}
+                        ActionsComponent={TablePaginationActions}
+                    />
+                </Box>
+            </Paper>
         </Grid>
     )
 }
