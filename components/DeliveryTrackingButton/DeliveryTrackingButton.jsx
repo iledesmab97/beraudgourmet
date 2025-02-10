2edeb0d4a62fd207eecff3172d0c7c62f41f61bc
Troy Pernía <troy00pernia@gmail.com>
2025-02-10 13:47:46 -0400

    changed file components/DeliveryTrackingButton: the ubers state was added (line 20). the function getNextOrdertoBeDelivered was modified (line 48). some styles of Fab component were modified (line 64-66). a Box component was added (line 87-113)

--- components/DeliveryTrackingButton/DeliveryTrackingButton.jsx
+++ components/DeliveryTrackingButton/DeliveryTrackingButton.jsx
@@ -1,9 +1,12 @@
 "use client";
 
 import Fab from "@mui/material/Fab";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 
 import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
+import CircleIcon from '@mui/icons-material/Circle';
 
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
@@ -14,6 +17,7 @@ import { getUberDeliveries } from "@/services/uberDirectApi";
 function DeliveryTrackingButton() {
     const [ nextUber, setNextUber ] = useState(null)
     const { user } = useSelector(state => state.user)
+    const [ubers, setUbers] = useState(0)
 
     useEffect(() => {
         if (!user) {
@@ -41,6 +45,7 @@ function DeliveryTrackingButton() {
 
             if (!uberDeliveries[0]) return
             setNextUber(uberDeliveries[0])
+            setUbers(uberDeliveries.length)
 
         } catch(error) {
             alert(error.message)
@@ -55,8 +60,10 @@ function DeliveryTrackingButton() {
                         position: "fixed",
                         bottom: "90px",
                         right: "20px",
-                        // backgroundColor: "#25D366",
                         backgroundColor: "#295386",
+                        "&:hover svg": {
+                            color: "#295386"
+                        }
                     }}
                 >
                     <Link
@@ -74,12 +81,36 @@ function DeliveryTrackingButton() {
                             sx={{
                                 scale: "1.5",
                                 color: "white",
-                                "&:hover": {
-                                    color: "#295386"
-                                }
                             }}
                         />
                     </Link>
+                    <Box
+                        sx={{
+                            width: "15px",
+                            height: "15px",
+                            position: "absolute",
+                            top: "0px",
+                            left: "40px",
+                            color: "white",
+                            backgroundColor: "red",
+                            borderRadius: "50%",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            scale: "1.3"
+                        }}
+
+                    >
+                        <Typography
+                            align="center"
+                            sx={{
+                                fontSize: "0.6rem",
+                                fontWeight: "700"
+                            }}
+                        >
+                            {ubers}
+                        </Typography>
+                    </Box>
                 </Fab>
             )}
         </>
